bba9e6a17b8c0cdbfc3ca111b117605ee33d39c5
drberry <>
2011-06-10 14:51:46 +0000

    Changed to do observed limits only

--- Macros/sub.sh
+++ Macros/sub.sh
@@ -1,21 +1,16 @@
 #!/bin/bash
-#1 Number of Toys
-#2 The number of parallel jobs
-NUMJOBS=0
-if [ -n $2 ]; then
-	NUMJOBS=$2
-fi 
-if [ $NUMJOBS -gt 0 ]
-then
-	for i in `seq 1 $NUMJOBS`; do 
-		for M in {105,110,115,120,125,130,135,140}; do
-			echo "Submitting job number $i with mass $M and $1 Toys"
-			bsub -q 1nh run_ProfileLikelihood.sh $PWD $M $1
-		done
-	done
-else
-	for M in `seq 105 0.5 140`; do
-		echo "Submitting mass $M"
+#Method (ProfileLikelihood, MarkovChainMC, HybridNew)
+for M in `seq 105 0.5 140`; do
+	if [ $1="ProfileLikelihood" ]; then
+		echo "Submitting mass $M for $1 method"
 		bsub -q 1nh run_OBSProfileLikelihood.sh $PWD $M
-	done
-fi
+	fi
+	if [ $1="MarkovChainMC" ]; then
+		echo "Submitting mass $M for $1 method"
+		bsub -q 1nh run_OBSMarkovChainMC.sh $PWD $M
+	fi
+	if [ $1="HybridNew" ]; then
+		echo "Submitting mass $M for $1 method"
+		bsub -q 1nh run_OBSHybridNew.sh $PWD $M
+	fi
+done
