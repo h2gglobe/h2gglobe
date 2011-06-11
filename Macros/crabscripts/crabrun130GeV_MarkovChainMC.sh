0e9cad82b324cf652bb86a96ecfab025edb919c0
drberry <>
2011-06-11 23:30:24 +0000

    Updated number of iterations and tries

--- Macros/crabscripts/crabrun130GeV_MarkovChainMC.sh
+++ Macros/crabscripts/crabrun130GeV_MarkovChainMC.sh
@@ -3,7 +3,7 @@
 mkdir outputToy
 echo "max events from CRAB: $MaxEvents"
 n="$MaxEvents"
-./combine model.root -M MarkovChainMC -D data_mass -m 130 -t $n -s -1 --generateBinnedWorkaround -S 1 -H ProfileLikelihood --rMin=0 --rMax=40 --iteration 50000 --tries 100 --hintStatOnly
+./combine model.root -M MarkovChainMC -D data_mass -m 130 -t $n -s -1 --generateBinnedWorkaround -S 1 -H ProfileLikelihood --rMin=0 --rMax=40 --iteration 50000 --tries 25 --hintStatOnly
 rm CMS-HGG.root
 rm model.root
 mv *.root outputToy/
