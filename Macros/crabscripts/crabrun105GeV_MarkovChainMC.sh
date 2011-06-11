5a42ef2f46a3a9afe161e6fea816e550dbcee2cf
drberry <>
2011-06-11 12:05:24 +0000

    First Commit

---
+++ Macros/crabscripts/crabrun105GeV_MarkovChainMC.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir outputToy
+echo "max events from CRAB: $MaxEvents"
+n="$MaxEvents"
+./combine model.root -M MarkovChainMC -D data_mass -m 105 -t $n -s -1 --generateBinnedWorkaround -S 1 -H ProfileLikelihood --rMin=0 --rMax=40 --iteration 50000 --tries 100 --hintStatOnly
+rm CMS-HGG.root
+rm model.root
+mv *.root outputToy/
+tar cvfz outputToy.tgz outputToy/
---
+++ Macros/crabscripts/crabrun115GeV_HybridNew.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mkdir outputToy
+echo "max events from CRAB: $MaxEvents"
+n="$MaxEvents"
+./combine model.root -M HybridNew -T $n  --generateNuis=0 --generateExt=1 --fitNuisances=1 --testStat Atlas -D data_mass -m 115 -s -1 -t 1 --generateBinnedWorkaround -S 1 -H ProfileLikelihood --hintStatOnly  --saveHybridResult --saveToys --toysFile HybridToys_mH115.root
+rm CMS-HGG.root
+rm model.root
+mv *.root outputToy/
+tar cvfz outputToy.tgz outputToy/
