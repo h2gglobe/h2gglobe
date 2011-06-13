957856247b34bf1ce42cfa1c663a6ac2ae8101ac
nancy <>
2011-06-13 14:51:57 +0000

    Put results in a directory with the method name

--- Macros/run_OBSHybridNew.sh
+++ Macros/run_OBSHybridNew.sh
@@ -2,12 +2,12 @@
 #1 Working Directory
 #2 Mass
 cd $1
-ODIR=$1/$2GeV
+ODIR=$1/HybridNew
 if [ ! -d $ODIR ]; then
 	mkdir $ODIR
 fi
 eval `scramv1 runtime -sh`
-combine cms-hgg-datacard_parBKG.txt -M HybridNew --generateNuis=0 --generateExt=1 --fitNuisances=1 --testStat Atlas -D data_mass -m $2 -s -1 --generateBinnedWorkaround -S 1 -H ProfileLikelihood --hintStatOnly  --saveHybridResult --saveToys --toysFile HybridToys_mH$2.root  | tee mH$2.HybridNew.$$.log
+combine cms-hgg-datacard_parBKG.txt -M HybridNew  -T 500 --generateNuis=0 --generateExt=1 --fitNuisances=1 --testStat Atlas -D data_mass -m $2 -s -1 --generateBinnedWorkaround -S 1 -n OBSERVED -H ProfileLikelihood --hintStatOnly  --saveHybridResult --saveToys --toysFile HybridToys_mH$2.root  | tee mH$2.HybridNew.$$.log
 SEED=`cat mH$2.HybridNew.$$.log | grep '>>> Used OpenSSL to get a really random seed' | awk '{print$10}'`
-mv mH$2.HybridNew.$$.log $ODIR/higgsCombineTest.OBSHybridNew.mH$2.$SEED.log
-mv higgsCombineTest.HybridNew.mH$2.$SEED.root $ODIR/higgsCombineTest.OBSHybridNew.mH$2.$SEED.root
+mv mH$2.HybridNew.$$.log $ODIR/higgsCombineOBSERVED.HybridNew.mH$2.$SEED.log
+mv higgsCombineOBSERVED.HybridNew.mH$2.$SEED.root $ODIR/higgsCombineOBSERVED.HybridNew.mH$2.root
