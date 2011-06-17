cbf5f5335ec2bad2a9816590545bc5ea1299d52b
drberry <>
2011-06-17 12:41:01 +0000

    Creates Unique Subdirectory

--- Macros/sub.sh
+++ Macros/sub.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 #1 method: ProfileLikelihood or MarkovChainMC or HybridNew  
 METHOD=$1
-	for M in `seq 105 0.5 140`; do
-		echo "Submitting mass $M for $1 method"
-		bsub -q 1nh run_OBS$METHOD.sh $PWD $M 
-	done
-
+DATE=`date +%F_%R_%S | tr -s ' ' '_' | tr -s ':' '_'`
+WORKINGDIR=${METHOD}_$DATE
+if [ ! -d $WORKINGDIR ]; then
+	mkdir $WORKINGDIR
+fi
+cp run_OBS$METHOD.sh $WORKINGDIR
+for M in `seq 105 0.5 140`; do
+	echo "Submitting mass $M for $1 method. Results in $WORKINGDIR"
+	bsub -q 8nm $PWD/$WORKINGDIR/run_OBS$METHOD.sh $PWD/$WORKINGDIR $M
+done
