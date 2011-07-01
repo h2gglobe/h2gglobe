093fc0525238615df68470ab480d498b3c3e4885
drberry <>
2011-07-01 16:23:59 +0000

    First Commit

---
+++ Macros/sub_Frequentist.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+DATE=`date +%F_%R_%S | tr -s ' ' '_' | tr -s ':' '_'`
+WORKINGDIR="Frequentist"_$DATE
+if [ ! -d $WORKINGDIR ]; then
+	mkdir $WORKINGDIR
+fi
+cp run_OBSFrequentist.sh $WORKINGDIR
+cp run_EXPFrequentist.sh $WORKINGDIR
+for M in `seq 105 0.5 140`; do
+	echo "Submitting mass $M for Frequentist method. Results in $WORKINGDIR"
+	bsub -q 8nm -o $WORKINGDIR/OBSFrequentist.mH$M.log $PWD/$WORKINGDIR/run_OBSFrequentist.sh $PWD/$WORKINGDIR $M
+	bsub -q 8nm -o $WORKINGDIR/EXPFrequentist.mH$M.0.027.log $PWD/$WORKINGDIR/run_EXPFrequentist.sh $PWD/$WORKINGDIR $M 0.0275
+	bsub -q 8nm -o $WORKINGDIR/EXPFrequentist.mH$M.0.160.log $PWD/$WORKINGDIR/run_EXPFrequentist.sh $PWD/$WORKINGDIR $M 0.160
+	bsub -q 8nm -o $WORKINGDIR/EXPFrequentist.mH$M.0.500.log $PWD/$WORKINGDIR/run_EXPFrequentist.sh $PWD/$WORKINGDIR $M 0.500
+	bsub -q 8nm -o $WORKINGDIR/EXPFrequentist.mH$M.0.840.log $PWD/$WORKINGDIR/run_EXPFrequentist.sh $PWD/$WORKINGDIR $M 0.840
+	bsub -q 8nm -o $WORKINGDIR/EXPFrequentist.mH$M.0.975.log $PWD/$WORKINGDIR/run_EXPFrequentist.sh $PWD/$WORKINGDIR $M 0.975
+done
