a20d1b5b98838baa9fbaf801d1e4833c750e2533
drberry <>
2011-07-01 12:44:52 +0000

    Updated Input Parameters

--- Macros/run_OBSMarkovChainMC.sh
+++ Macros/run_OBSMarkovChainMC.sh
@@ -8,7 +8,7 @@ if [ ! -d $ODIR ]; then
 	mkdir $ODIR
 fi
 eval `scramv1 runtime -sh`
-combine cms-hgg-datacard_parBKG.txt -M MarkovChainMC -D data_mass -m $2 -s -1 -S 1 --generateBinnedWorkaround  -H ProfileLikelihood -n OBSERVED --rMin=0 --rMax=40 --truncatedMeanFraction 0.05  --iteration 70000 --tries 200 --hintStatOnly | tee mH$2.Markov.$$.log
+combine -d cms-hgg-datacard_parBKG.txt -M MarkovChainMC -D data_mass -m $2 -s -1 -S 1 -n OBSERVED --rMin=0.0 --rMax=60.0 -b 3000 -i 50000 --tries=100 --optimizeSim=1 | tee mH$2.Markov.$$.log
 SEED=`cat mH$2.Markov.$$.log | grep '>>> Used OpenSSL to get a really random seed' | awk '{print$10}'`
 mv mH$2.Markov.$$.log $ODIR/higgsCombineOBSERVED.MarkovChainMC.mH$2.$SEED.log
 mv higgsCombineOBSERVED.MarkovChainMC.mH$2.$SEED.root $ODIR/higgsCombineOBSERVED.MarkovChainMC.mH$2.root
