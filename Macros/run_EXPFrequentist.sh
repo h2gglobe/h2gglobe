8aaa8abc558f6b26bb0524ab6f50e95dec244e44
drberry <>
2011-07-11 20:55:25 +0000

    Now uses CASTOR

--- Macros/run_EXPFrequentist.sh
+++ Macros/run_EXPFrequentist.sh
@@ -2,14 +2,16 @@
 #1 Working Directory
 #2 Mass
 #3 Expected Limit (0.0275, 0.16, 0.5, 0.84, 0.975)
-cd $1
-ODIR=$1
-cd ..
-if [ ! -d $ODIR ]; then
-	mkdir $ODIR
+REMOTEDIR=$PWD
+rfcp /castor/cern.ch/user/d/drberry/FrequentistGrid.root .
+if [ ! -d $1 ]; then
+	mkdir $1
 fi
+cd $1
+ln -s ../CMS-HGG.root CMS-HGG.root
+ln -s $REMOTEDIR/FrequentistGrid.root FrequentistGrid$$.root
 eval `scramv1 runtime -sh`
-combine RooModel/${2}GeVmodel.root -m ${2} -D data_mass -M HybridNew --freq --rAbsAcc=0.001 --grid=FrequentistGrid.root --expectedFromGrid $3
-set limit = `echo "$3" | cut -c1-5`
-limit = `perl -e "printf('%.03f', $limit)"`
-mv higgsCombineTest.HybridNew.mH$2.quant${limit}.root $ODIR/expectedFrequentist.mH$2.quant${limit}.root
+combine ../RooModel/${2}GeVmodel.root -m ${2} -D data_mass -M HybridNew --freq --rAbsAcc=0.001 --grid=FrequentistGrid$$.root --expectedFromGrid $3
+limit=`echo "$3" | cut -c 1-5`
+limit=`perl -e "printf('%.03f', $limit)"`
+mv higgsCombineTest.HybridNew.mH$2.quant${limit}.root expectedFrequentist.mH$2.quant${limit}.root
--- Macros/run_OBSFrequentist.sh
+++ Macros/run_OBSFrequentist.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 #1 Working Directory
 #2 Mass
-cd $1
-ODIR=$1
-cd ..
-if [ ! -d $ODIR ]; then
-	mkdir $ODIR
+REMOTEDIR=$PWD
+rfcp /castor/cern.ch/user/d/drberry/FrequentistGrid.root .
+if [ ! -d $1 ]; then
+	mkdir $1
 fi
+cd $1
+ln -s ../CMS-HGG.root CMS-HGG.root
+ln -s $REMOTEDIR/FrequentistGrid.root FrequentistGrid$$.root
 eval `scramv1 runtime -sh`
-combine RooModel/${2}GeVmodel.root -m ${2} -D data_mass -M HybridNew --freq --rAbsAcc=0.001 --grid=FrequentistGrid.root
-mv higgsCombineTest.HybridNew.mH$2.root $ODIR/higgsCombine.Frequentist.mH$2.root
+combine ../RooModel/${2}GeVmodel.root -m ${2} -D data_mass -M HybridNew --freq --rAbsAcc=0.001 --grid=FrequentistGrid$$.root
+mv higgsCombineTest.HybridNew.mH$2.root higgsCombineOBSERVED.Frequentist.mH$2.root
