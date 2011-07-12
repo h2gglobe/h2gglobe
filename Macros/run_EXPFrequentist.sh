8c7b1b7799b0b86680ae24b2336dd7162c2352c8
drberry <>
2011-07-12 12:16:58 +0000

    Uses Random Seed

--- Macros/run_EXPFrequentist.sh
+++ Macros/run_EXPFrequentist.sh
@@ -3,15 +3,20 @@
 #2 Mass
 #3 Expected Limit (0.0275, 0.16, 0.5, 0.84, 0.975)
 REMOTEDIR=$PWD
+SUBNUMBER=1
 rfcp /castor/cern.ch/user/d/drberry/FrequentistGrid.root .
 if [ ! -d $1 ]; then
 	mkdir $1
 fi
 cd $1
 ln -s ../CMS-HGG.root CMS-HGG.root
-ln -s $REMOTEDIR/FrequentistGrid.root FrequentistGrid$$.root
+while [ -e FrequentistGrid$SUBNUMBER.root ]; do
+	SUBNUMBER=$(($SUBNUMBER+1))
+done
+ln -s $REMOTEDIR/FrequentistGrid.root FrequentistGrid$SUBNUMBER.root
 eval `scramv1 runtime -sh`
-combine ../RooModel/${2}GeVmodel.root -m ${2} -D data_mass -M HybridNew --freq --rAbsAcc=0.001 --grid=FrequentistGrid$$.root --expectedFromGrid $3
+combine ../RooModel/${2}GeVmodel.root -m ${2} -D data_mass -M HybridNew -s -1 --freq --rAbsAcc=0.001 --grid=FrequentistGrid$SUBNUMBER.root --expectedFromGrid $3
 limit=`echo "$3" | cut -c 1-5`
 limit=`perl -e "printf('%.03f', $limit)"`
-mv higgsCombineTest.HybridNew.mH$2.quant${limit}.root expectedFrequentist.mH$2.quant${limit}.root
+filename=`/bin/ls higgsCombineTest.HybridNew.mH$2.[-0-9]*.quant${limit}.root`
+mv $filename expectedFrequentist.mH$2.quant${limit}.root
--- Macros/run_OBSFrequentist.sh
+++ Macros/run_OBSFrequentist.sh
@@ -2,13 +2,18 @@
 #1 Working Directory
 #2 Mass
 REMOTEDIR=$PWD
+SUBNUMBER=1
 rfcp /castor/cern.ch/user/d/drberry/FrequentistGrid.root .
 if [ ! -d $1 ]; then
 	mkdir $1
 fi
 cd $1
 ln -s ../CMS-HGG.root CMS-HGG.root
-ln -s $REMOTEDIR/FrequentistGrid.root FrequentistGrid$$.root
+while [ -e FrequentistGrid$SUBNUMBER.root ]; do
+	SUBNUMBER=$(($SUBNUMBER+1))
+done
+ln -s $REMOTEDIR/FrequentistGrid.root FrequentistGrid$SUBNUMBER.root
 eval `scramv1 runtime -sh`
-combine ../RooModel/${2}GeVmodel.root -m ${2} -D data_mass -M HybridNew --freq --rAbsAcc=0.001 --grid=FrequentistGrid$$.root
+combine ../RooModel/${2}GeVmodel.root -m ${2} -D data_mass -M HybridNew --freq --rAbsAcc=0.001 --grid=FrequentistGrid$SUBNUMBER.root
+/bin/ls
 mv higgsCombineTest.HybridNew.mH$2.root higgsCombineOBSERVED.Frequentist.mH$2.root
