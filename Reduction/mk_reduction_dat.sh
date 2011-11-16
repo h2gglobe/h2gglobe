b7f669f0d660b985af37a0f2b84c46397ff0bb5b
musella <>
2011-11-16 13:56:07 +0000

    *** empty log message ***

--- Reduction/mk_reduction_dat.sh
+++ Reduction/mk_reduction_dat.sh
@@ -3,6 +3,9 @@
 . setup.sh
 
 rm data/*.dat
-./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/h2g_V10_00/Data ${storedir}/Data data.txt
+./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V11_04/Data ${storedir}/Data data.txt
+
+rm mc/*.dat
+./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V11_04/mc ${storedir}/mc mc.txt
 
 tar zcf ${version}.tgz  PhotonAnalysis_scripts/*.dat
