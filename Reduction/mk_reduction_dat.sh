d67eab6de43ecbc34161b4d100a20b26deff1af5
futyand <>
2011-12-07 18:46:37 +0000

    update to Dec07 reduction

--- Reduction/mk_reduction_dat.sh
+++ Reduction/mk_reduction_dat.sh
@@ -3,9 +3,9 @@
 . setup.sh
 
 rm data/*.dat
-./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V11_04/Data ${storedir}/Data data.txt
+./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V11_04_05/Data ${storedir}/Data data.txt
 
-rm mc/*.dat
-./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V11_04/mc ${storedir}/mc mc.txt
+rm  mc_sig_fall11_s6/*.dat
+./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V11_04_05/Data ${storedir}/MC_Sig_Fall11_S6 mc_sig_fall11_s6.txt
 
 tar zcf ${version}.tgz  PhotonAnalysis_scripts/*.dat
