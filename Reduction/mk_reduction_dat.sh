e45a23c21285503f618d31ebb9236dacdae787b9
futyand <>
2012-01-26 17:03:28 +0000

    add background MC

--- Reduction/mk_reduction_dat.sh
+++ Reduction/mk_reduction_dat.sh
@@ -8,4 +8,7 @@ rm data/*.dat
 rm  mc_sig_fall11_s6/*.dat
 ./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V11_04_05/Data ${storedir}/MC_Sig_Fall11_S6 mc_sig_fall11_s6.txt
 
+rm  mc_bkg_fall11_s6/*.dat
+./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V11_04_05/Data ${storedir}/MC_Bkg_Fall11_S6 mc_bkg_fall11_s6.txt
+
 tar zcf ${version}.tgz  PhotonAnalysis_scripts/*.dat
