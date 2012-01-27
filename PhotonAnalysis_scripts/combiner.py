282c0315897944f7d0b580dd88b3ca841982b2ab
nckw <>
2012-01-27 15:24:14 +0000

    combiner now takes job splitting

--- PhotonAnalysis_scripts/combiner.py
+++ PhotonAnalysis_scripts/combiner.py
@@ -7,6 +7,8 @@ from python.lumi import *
 import sys
 from python.runOptions import *
 (options,args)=parser.parse_args()
+if (int(options.nJobs) > 0) and (int(options.jobId) >= int(options.nJobs)):
+  sys.exit("Job id's must run from 0 -> %d when splitting into %d jobs"%(int(options.nJobs)-1,int(options.nJobs)))
 
 ROOT.gSystem.Load("libRooFit.so")
 ROOT.gSystem.Load("libPhysics.so");
@@ -22,7 +24,8 @@ if options.inputDat:
     config_file = options.inputDat
 
 ut = ROOT.LoopAll();
-cfg = configProducer(ut,config_file,0,-1,0)
+cfg = configProducer(ut,config_file,0,int(options.nJobs),int(options.jobId))
+#cfg = configProducer(ut,config_file,0,-1,0)
 
 if not options.dryRun:
   ut.MergeContainers()
