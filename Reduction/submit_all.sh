b21c44f7b321d16f5f683256bbf15fa4e674a398
futyand <>
2012-02-17 18:20:00 +0000

    script to submit all jobs

---
+++ Reduction/submit_all.sh
@@ -0,0 +1,89 @@
+#!/bin/bash
+
+./submit_reduction.sh data PhotonRun2011A_Clean 50
+./submit_reduction.sh data PhotonRun2011B_Clean 50
+
+./submit_reduction.sh mc_sig_fall11_s6 GluGluToHToGG_M-100_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 GluGluToHToGG_M-105_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 GluGluToHToGG_M-110_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 GluGluToHToGG_M-115_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 GluGluToHToGG_M-120_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 GluGluToHToGG_M-121_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 GluGluToHToGG_M-123_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 GluGluToHToGG_M-125_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 GluGluToHToGG_M-130_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 GluGluToHToGG_M-135_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 GluGluToHToGG_M-140_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 GluGluToHToGG_M-145_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 GluGluToHToGG_M-150_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 GluGluToHToGG_M-155_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 GluGluToHToGG_M-90_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 GluGluToHToGG_M-95_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 TTH_HToGG_M-100_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 TTH_HToGG_M-105_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 TTH_HToGG_M-110_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 TTH_HToGG_M-115_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 TTH_HToGG_M-120_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 TTH_HToGG_M-121_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 TTH_HToGG_M-123_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 TTH_HToGG_M-125_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 TTH_HToGG_M-130_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 TTH_HToGG_M-135_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 TTH_HToGG_M-140_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 TTH_HToGG_M-145_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 TTH_HToGG_M-150_7TeV_Backup
+./submit_reduction.sh mc_sig_fall11_s6 TTH_HToGG_M-155_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 TTH_HToGG_M-90_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 TTH_HToGG_M-95_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 VBF_HToGG_M-100_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 VBF_HToGG_M-105_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 VBF_HToGG_M-110_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 VBF_HToGG_M-115_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 VBF_HToGG_M-120_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 VBF_HToGG_M-121_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 VBF_HToGG_M-123_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 VBF_HToGG_M-125_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 VBF_HToGG_M-130_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 VBF_HToGG_M-135_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 VBF_HToGG_M-140_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 VBF_HToGG_M-145_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 VBF_HToGG_M-150_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 VBF_HToGG_M-155_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 VBF_HToGG_M-160_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 VBF_HToGG_M-90_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 VBF_HToGG_M-95_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 WH_ZH_HToGG_M-100_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 WH_ZH_HToGG_M-105_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 WH_ZH_HToGG_M-110_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 WH_ZH_HToGG_M-115_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 WH_ZH_HToGG_M-120_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 WH_ZH_HToGG_M-121_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 WH_ZH_HToGG_M-123_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 WH_ZH_HToGG_M-125_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 WH_ZH_HToGG_M-130_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 WH_ZH_HToGG_M-135_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 WH_ZH_HToGG_M-140_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 WH_ZH_HToGG_M-145_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 WH_ZH_HToGG_M-150_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 WH_ZH_HToGG_M-155_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 WH_ZH_HToGG_M-160_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 WH_ZH_HToGG_M-90_7TeV
+./submit_reduction.sh mc_sig_fall11_s6 WH_ZH_HToGG_M-95_7TeV
+
+#./submit_reduction.sh mc_bkg_fall11_s6 BoxPt10to25 5
+./submit_reduction.sh mc_bkg_fall11_s6 BoxPt25to250 5
+#./submit_reduction.sh mc_bkg_fall11_s6 BoxPt250 5
+./submit_reduction.sh mc_bkg_fall11_s6 DiPhotonJets 10
+./submit_reduction.sh mc_bkg_fall11_s6 DYJetsToLL_M50 50
+
+./submit_reduction.sh mc_bkg_fall11_s6 GJet_Pt-20_ff 10
+./submit_reduction.sh mc_bkg_fall11_s6 GJet_Pt-20_pf 10
+./submit_reduction.sh mc_bkg_fall11_s6 GJet_Pt-20_pp 10
+
+./submit_reduction.sh mc_bkg_fall11_s6 QCDPt30to40_ff 5
+./submit_reduction.sh mc_bkg_fall11_s6 QCDPt30to40_pf 5
+#./submit_reduction.sh mc_bkg_fall11_s6 QCDPt30to40_pp 5
+
+./submit_reduction.sh mc_bkg_fall11_s6 QCDPt40_ff 10
+./submit_reduction.sh mc_bkg_fall11_s6 QCDPt40_pf 10
+#./submit_reduction.sh mc_bkg_fall11_s6 QCDPt40_pp 10
