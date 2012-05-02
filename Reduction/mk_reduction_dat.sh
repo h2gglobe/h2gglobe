e450da05aa4d184135a11d686c51d3e6d10e0fdb
capalmer <>
2012-05-02 23:01:17 +0000

    first reduction of V13 data/mc

--- Reduction/mk_reduction_dat.sh
+++ Reduction/mk_reduction_dat.sh
@@ -3,12 +3,12 @@
 . setup.sh
 
 rm data/*.dat
-./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V11_04_05/Data ${storedir}/Data data.txt
+./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V13_01_01/data ${storedir}/Data data.txt
 
-rm  mc_sig_fall11_s6/*.dat
-./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V11_04_05/Data ${storedir}/MC_Sig_Fall11_S6 mc_sig_fall11_s6.txt
+rm  mc_sig_summer12_s7/*.dat
+./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V13_01_02/mc/Summer12_S7 ${storedir}/MC_Sig_Summer12_S7 mc_sig_summer12_s7.txt
 
-rm  mc_bkg_fall11_s6/*.dat
-./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V11_04_05/Data ${storedir}/MC_Bkg_Fall11_S6 mc_bkg_fall11_s6.txt
+rm  mc_bkg_summer12_s7/*.dat
+./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V13_01_02/mc/Summer12_S7 ${storedir}/MC_Bkg_Summer12_S6 mc_bkg_summer12_s7.txt
 
 tar zcf ${version}.tgz  PhotonAnalysis_scripts/*.dat
