b9b95b8e50066b4414992a96e6536cdc905105f5
capalmer <>
2012-05-02 23:02:20 +0000

    text edit

--- Reduction/mk_reduction_dat.sh
+++ Reduction/mk_reduction_dat.sh
@@ -9,6 +9,6 @@ rm  mc_sig_summer12_s7/*.dat
 ./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V13_01_02/mc/Summer12_S7 ${storedir}/MC_Sig_Summer12_S7 mc_sig_summer12_s7.txt
 
 rm  mc_bkg_summer12_s7/*.dat
-./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V13_01_02/mc/Summer12_S7 ${storedir}/MC_Bkg_Summer12_S6 mc_bkg_summer12_s7.txt
+./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V13_01_02/mc/Summer12_S7 ${storedir}/MC_Bkg_Summer12_S7 mc_bkg_summer12_s7.txt
 
 tar zcf ${version}.tgz  PhotonAnalysis_scripts/*.dat
