9954e59999d8ab7a4ee799c09557433c9628d42f
capalmer <>
2012-05-06 20:58:05 +0000

    *** empty log message ***

--- Reduction/submit_all.sh
+++ Reduction/submit_all.sh
@@ -2,9 +2,12 @@
 
 ./submit_reduction.sh data  PhotonPromptReco2012A_30-04-12  25
 
-#./submit_reduction.sh mc_sig_summer12_s7 GluGluToHToGG_M-120 
-#./submit_reduction.sh mc_sig_summer12_s7 GluGluToHToGG_M-125 
+./submit_reduction.sh mc_sig_summer12_s7 GluGluToHToGG_M-120 
+./submit_reduction.sh mc_sig_summer12_s7 GluGluToHToGG_M-125 
 
-#./submit_reduction.sh mc_bkg_summer12_s7 DYJets 50
-#./submit_reduction.sh QCD_bkg_summer12_s7 QCD_Pt_30_80 5
+./submit_reduction.sh mc_bkg_summer12_s7_V13_01_02 DYJets 10
+./submit_reduction.sh mc_bkg_summer12_s7_V13_01_02 QCD_Pt_30_80 10
 
+./submit_reduction.sh mc_bkg_summer12_s7_V13_01_03 GJet_Pt-20_ff 10
+./submit_reduction.sh mc_bkg_summer12_s7_V13_01_03 GJet_Pt-20_pf 10
+./submit_reduction.sh mc_bkg_summer12_s7_V13_01_03 GJet_Pt-20_pp 10
