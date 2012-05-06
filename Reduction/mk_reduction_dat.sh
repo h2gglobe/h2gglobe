3b026e1d595ff8bb28890b76c1df59fe0b0a18a5
capalmer <>
2012-05-06 20:35:09 +0000

    *** empty log message ***

--- Reduction/mk_reduction_dat.sh
+++ Reduction/mk_reduction_dat.sh
@@ -8,7 +8,11 @@ rm data/*.dat
 rm  mc_sig_summer12_s7/*.dat
 ./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V13_01_02/mc/Summer12_S7 ${storedir}/MC_Sig_Summer12_S7 mc_sig_summer12_s7.txt
 
-rm  mc_bkg_summer12_s7/*.dat
-./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V13_01_02/mc/Summer12_S7 ${storedir}/MC_Bkg_Summer12_S7 mc_bkg_summer12_s7.txt
+rm  mc_bkg_summer12_s7_V13_01_02/*.dat
+./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V13_01_02/mc/Summer12_S7 ${storedir}/MC_Bkg_Summer12_S7 mc_bkg_summer12_s7_V13_01_02.txt
+
+rm  mc_bkg_summer12_s7_V13_01_03/*.dat
+./PhotonAnalysis_scripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V13_01_03/mc/Summer12_S7 ${storedir}/MC_Bkg_Summer12_S7 mc_bkg_summer12_s7_V13_01_03.txt
+
 
 tar zcf ${version}.tgz  PhotonAnalysis_scripts/*.dat
