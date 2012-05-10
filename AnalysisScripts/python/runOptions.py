1fd19a7d177582da0b2438aea6aa677abcc074bd
musella <>
2012-05-10 11:17:51 +0000

    Committing restructured dat files directory

---
+++ AnalysisScripts/python/runOptions.py
@@ -0,0 +1,10 @@
+from optparse import OptionParser
+parser = OptionParser()
+parser.add_option("","--dryRun",action="store_true",dest="dryRun",default=False)
+parser.add_option("-i","--inputDat",dest="inputDat")
+parser.add_option("-n","--nJobs",dest="nJobs",default=-1)
+parser.add_option("-j","--jobId",dest="jobId",default=0)
+parser.add_option("-t","--typeRun",type="int",dest="typeRun",default=-1)
+parser.add_option("-s","--searchPath",type="string",dest="searchPath",default="common:reduction:baseline:massfac_mva_binned:full_mva_binned")
+parser.add_option("-a","--appendSearchPath",dest="postSearchPath",action="append",default=[])
+parser.add_option("-p","--prependSearchPath",dest="preSearchPath",action="append",default=[])
