1542937676f154cdcae447e36318d4c308ed22ad
mkenzie <>
2012-05-22 12:04:52 +0000

    Allows grepping

--- Macros/histoCheck.py
+++ Macros/histoCheck.py
@@ -3,8 +3,12 @@ import sys
 
 filename = sys.argv[1]
 tfile = ROOT.TFile(filename)
+grepBit="_"
+if (len(sys.argv)>2):
+	if (sys.argv[2]=="-grep"):
+		grepBit=sys.argv[3]
 
 for key in tfile.GetListOfKeys():
-  if 'th1f' in key.GetName() and ('BDT' in key.GetName() or 'VBF' in key.GetName()):
+  if 'th1f' in key.GetName() and 'BDT' in key.GetName() and grepBit in key.GetName():
     th1f = tfile.Get(key.GetName())
     print "%60s %10d %4.4f"%(th1f.GetName(),th1f.GetEntries(),th1f.Integral())
