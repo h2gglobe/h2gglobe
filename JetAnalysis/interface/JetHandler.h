4ac2991bcfe926a1f9a912b521ab795b37977c7d
musella <>
2012-05-23 00:21:03 +0000

    on the fly re-computation of betas set-up JEC

--- JetAnalysis/interface/JetHandler.h
+++ JetAnalysis/interface/JetHandler.h
@@ -2,6 +2,9 @@
 #define __JetHandler__
 
 #include "CMGTools/External/interface/PileupJetIdAlgo.h"
+#include "CondFormats/JetMETObjects/interface/JetCorrectorParameters.h"
+#include "CondFormats/JetMETObjects/interface/FactorizedJetCorrector.h"
+
 #include <string>
 
 class LoopAll;
@@ -11,9 +14,9 @@ class JetHandler
 {
 public:
     
-    JetHandler(const std::string & cfg, const std::string & pset, LoopAll &l);
+    JetHandler(const std::string & cfg, LoopAll &l);
     
-    void compute_beta(int ijet, int ivx=-1);
+    void compute_betas(int ijet, int ivx=-1);
     void compute_mva(int ijet, int ivtx=-1);
     void  compute_wp(int ijet, int ivtx=-1);
     
@@ -24,6 +27,10 @@ public:
 private:
     LoopAll & l_;
     PileupJetIdAlgo * cutbased, * simple, * full;
+
+    FactorizedJetCorrector *jecCorData_, *jecCorMc_;
+    std::vector<JetCorrectorParameters> jetCorParsData_, jetCorParsMc_;
+    
     
 };
 
--- JetAnalysis/src/JetHandler.cc
+++ JetAnalysis/src/JetHandler.cc
@@ -6,14 +6,37 @@
 #include "LoopAll.h"
 
 // ---------------------------------------------------------------------------------------------------------------
-JetHandler::JetHandler(const std::string & cfg, const std::string & pset, LoopAll & l):
+JetHandler::JetHandler(const std::string & cfg, LoopAll & l):
     l_(l)
 {
-    const edm::ParameterSet & myPset = edm::readPSetsFrom(cfg)->getParameter<edm::ParameterSet>(pset);
+    boost::shared_ptr<edm::ParameterSet> myPset = edm::readConfig(cfg);
     
-    cutbased = new PileupJetIdAlgo(myPset.getParameter<edm::ParameterSet>("cutbased"));
-    simple   = new PileupJetIdAlgo(myPset.getParameter<edm::ParameterSet>("simple"));
-    full     = new PileupJetIdAlgo(myPset.getParameter<edm::ParameterSet>("full"));
+    cutbased = new PileupJetIdAlgo(myPset->getParameter<edm::ParameterSet>("cutbased"));
+    simple   = new PileupJetIdAlgo(myPset->getParameter<edm::ParameterSet>("simple"));
+    full     = new PileupJetIdAlgo(myPset->getParameter<edm::ParameterSet>("full"));
+    
+    
+    jecCorData_ = 0;
+    jecCorMc_   = 0;
+    std::vector<std::string> dataJEC = myPset->getParameter<std::vector<std::string> >("dataJEC");
+    std::vector<std::string> mcJEC   = myPset->getParameter<std::vector<std::string> >("mcJEC");
+    
+    for(std::vector<std::string>::iterator corr = dataJEC.begin(); corr!=dataJEC.end(); ++corr) {
+	jetCorParsData_.push_back( JetCorrectorParameters(*corr) );
+    }
+
+    for(std::vector<std::string>::iterator corr = mcJEC.begin(); corr!=mcJEC.end(); ++corr) {
+	jetCorParsMc_.push_back( JetCorrectorParameters(*corr) );
+    }
+    
+    if( ! jetCorParsData_.empty() ) {
+	jecCorData_ = new FactorizedJetCorrector(jetCorParsData_);
+    }
+
+    if( ! jetCorParsMc_.empty() ) {
+	jecCorMc_ = new FactorizedJetCorrector(jetCorParsMc_);
+    }
+    
 }
 
 // ---------------------------------------------------------------------------------------------------------------
@@ -25,12 +48,68 @@ JetHandler::~JetHandler()
 }
 
 // ---------------------------------------------------------------------------------------------------------------
-void compute_beta(int ijet, int ivx)
+float dz(TLorentzVector * tkp4, TVector3 * tkpos, TVector3 * vtxpos)
 {
-    //////////// TVector3 * tkpos= (TVector3 *) tk_vtx_pos->At(itk);
-    //////////// /// double deltaz = fabs(vtxpos->Z() - tkpos->Z()); 
-    //////////// double deltaz = fabs( (tkpos->Z()-vtxpos->Z()) - ( (tkpos->X()-vtxpos->X())*tkp4->Px() + (tkpos->Y()-vtxpos->Y())*tkp4->Py() )/tkp4->Pt() * tkp4->Pz()/tkp4->Pt() );
+    return fabs( (tkpos->Z()-vtxpos->Z()) - 
+		 ( (tkpos->X()-vtxpos->X())*tkp4->Px() + (tkpos->Y()-vtxpos->Y())*tkp4->Py() )/tkp4->Pt() * tkp4->Pz()/tkp4->Pt() );
+}
 
+// ---------------------------------------------------------------------------------------------------------------
+void JetHandler::compute_betas(int ijet, int vtx)
+{
+    float sumTkPt = 0.;
+    float & beta = (*l_.jet_algoPF1_beta_ext)[ijet][vtx];
+    float & betaStar = (*l_.jet_algoPF1_betaStar_ext)[ijet][vtx];
+    float & betaStarClassic = (*l_.jet_algoPF1_betaStarClassic_ext)[ijet][vtx];
+    beta = 0., betaStar = 0., betaStarClassic = 0.;
+
+    const std::vector<unsigned short> & vtx_tracks = (*l_.vtx_std_tkind)[vtx];
+    TVector3 * vtxpos = (TVector3*)l_.vtx_std_xyz->At(vtx);
+    
+    std::cout << l_.jet_algoPF1_tkind << " " << l_.jet_algoPF1_tkind->size() << std::endl;
+    std::cout << l_.jet_algoPF2_tkind << " " << l_.jet_algoPF2_tkind->size() << std::endl;
+    std::cout << l_.jet_algoPF3_tkind << " " << l_.jet_algoPF3_tkind->size() << std::endl;
+    const std::vector<unsigned short> & jet_tracks = (*l_.jet_algoPF1_tkind)[ijet];
+    for(std::vector<unsigned short>::const_iterator itrack=jet_tracks.begin(); itrack!=jet_tracks.end(); ++itrack ) {
+	bool inVtx0 = find( vtx_tracks.begin(),  vtx_tracks.end(), *itrack ) != vtx_tracks.end();
+	bool inAnyOther = false;
+	
+	TVector3 * tkpos= (TVector3 *) l_.tk_vtx_pos->At(*itrack);
+	TLorentzVector * tkp4= (TLorentzVector *) l_.tk_p4->At(*itrack);
+	float tkpt = tkp4->Pt();
+	
+	float dZ0 = dz(tkp4, tkpos, vtxpos);
+	float dZ = dZ0;
+	
+	for(int ivtx=0; ivtx<l_.vtx_std_n; ++ivtx) {
+	    bool isVtx0 = (ivtx == vtx);
+	    if( ! isVtx0 && ! inAnyOther ) {
+		const std::vector<unsigned short> & ivtx_tracks = (*l_.vtx_std_tkind)[ivtx];
+		inAnyOther = find( ivtx_tracks.begin(),  ivtx_tracks.end(), *itrack ) != ivtx_tracks.end();
+	    }
+	    TVector3 * ivtxpos = (TVector3*)l_.vtx_std_xyz->At(ivtx);
+	    dZ = std::min(dZ, dz(tkp4, tkpos, ivtxpos));
+	}
+	
+	sumTkPt += tkpt;
+	if( ! inVtx0 && inAnyOther ) {
+	    betaStarClassic += tkpt; 
+	}
+	if( dZ0 < 0.2 ) {
+	    beta += tkpt;
+	} else if( dZ < 0.2 ) {
+	    betaStar += tkpt;
+	}
+    }
+    
+    if( sumTkPt != 0. ) {
+	beta     /= sumTkPt;
+	betaStar /= sumTkPt;
+	betaStarClassic /= sumTkPt;
+    } else {
+	assert( beta == 0. && betaStar == 0. && betaStarClassic == 0. );
+    }
+    
 }
 
 // ---------------------------------------------------------------------------------------------------------------
