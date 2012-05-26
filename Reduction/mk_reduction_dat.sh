ad162ad69af6cc9729108cb60e5ab5e9ce8b0eb2
capalmer <>
2012-05-26 08:43:59 +0000

    V13_03 reduction

--- Reduction/mk_reduction_dat.sh
+++ Reduction/mk_reduction_dat.sh
@@ -2,25 +2,13 @@
 
 . setup.sh
 
-### rm data/*.dat
-### ./AnalysisScripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V13_01_01/data ${storedir}/Data data.txt
-### 
-### rm  mc_sig_summer12_s7/*.dat
-### ./AnalysisScripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V13_01_02/mc/Summer12_S7 ${storedir}/MC_Sig_Summer12_S7 mc_sig_summer12_s7.txt
-### 
-### rm  mc_bkg_summer12_s7_V13_01_02/*.dat
-### ./AnalysisScripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V13_01_02/mc/Summer12_S7 ${storedir}/MC_Bkg_Summer12_S7 mc_bkg_summer12_s7_V13_01_02.txt
-### 
-### rm  mc_bkg_summer12_s7_V13_01_03/*.dat
-### ./AnalysisScripts/mk_reduction_dat.py /castor/cern.ch/user/c/cmshgg/processed/V13_01_03/mc/Summer12_S7 ${storedir}/MC_Bkg_Summer12_S7 mc_bkg_summer12_s7_V13_01_03.txt
-
 rm data_2012/*.dat
-./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_02_00/data ${storedir}/data data_2012.txt
+./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_00/data ${storedir}/data data_2012.txt
 
 rm mc_sig_summer12_s7/*.dat
-./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_02_00/mc/Summer12_S7_8TeV ${storedir}/Summer12_S7_8TeV mc_sig_summer12_s7.txt
+./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_00/mc/Summer12_S7_8TeV ${storedir}/Summer12_S7_8TeV mc_sig_summer12_s7.txt
 
 rm mc_bkg_summer12_s7/*.dat
-./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_02_00/mc/Summer12_S7_8TeV ${storedir}/Summer12_S7_8TeV mc_bkg_summer12_s7.txt
+./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_00/mc/Summer12_S7_8TeV ${storedir}/Summer12_S7_8TeV mc_bkg_summer12_s7.txt
 
 tar zcf ${version}.tgz  AnalysisScripts/*.dat
--- Reduction/submit_all.sh
+++ Reduction/submit_all.sh
@@ -1,20 +1,19 @@
 #!/bin/bash
 
-./submit_reduction.sh data_2012  PhotonPromptReco2012A\*  100 & 
+./submit_reduction.sh data_2012  PhotonPromptReco\*  50 & 
 
-./submit_reduction.sh mc_sig_summer12_s7 \* 20 & 
+./submit_reduction.sh mc_sig_summer12_s7 \* 5 & 
 
 ./submit_reduction.sh mc_bkg_summer12_s7 DiPhoton\*_Pt-10To25\*   20 & 
-./submit_reduction.sh mc_bkg_summer12_s7 DiPhotonBorn_Pt-25To250\*  10 & 
-./submit_reduction.sh mc_bkg_summer12_s7 DiPhotonBox_Pt-25To250\*  30 & 
-./submit_reduction.sh mc_bkg_summer12_s7 DiPhoton\*_Pt-250ToInf\* 45 & 
+./submit_reduction.sh mc_bkg_summer12_s7 DiPhoton\*_Pt-25To250\*  30 & 
+./submit_reduction.sh mc_bkg_summer12_s7 DiPhoton\*_Pt-250ToInf\* 20 & 
 
 ./submit_reduction.sh mc_bkg_summer12_s7 DYJetsToLL_M-50 50 & 
-./submit_reduction.sh mc_bkg_summer12_s7 DYJetsToLL_M-50_52_1e6 3 & 
+./submit_reduction.sh mc_bkg_summer12_s7 DYJetsToLL_M-50_52_1e6 5 & 
 
-./submit_reduction.sh mc_bkg_summer12_s7 QCD_Pt-30to40\* 50 & 
+./submit_reduction.sh mc_bkg_summer12_s7 QCD_Pt\* 40 & 
 
-./submit_reduction.sh mc_bkg_summer12_s7 GJet\* 50 &
+./submit_reduction.sh mc_bkg_summer12_s7 GJet\* 40 &
 
 wait
 
