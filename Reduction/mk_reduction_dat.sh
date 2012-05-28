75e0741ed20232eb33c567eca1c328564dbff4b3
musella <>
2012-05-28 00:43:09 +0000

    may31freezing_reduction_v1

--- Reduction/mk_reduction_dat.sh
+++ Reduction/mk_reduction_dat.sh
@@ -3,12 +3,12 @@
 . setup.sh
 
 rm data_2012/*.dat
-./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_00/data ${storedir}/data data_2012.txt
+./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_03/data ${storedir}/data data_2012.txt
 
 rm mc_sig_summer12_s7/*.dat
-./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_00/mc/Summer12_S7_8TeV ${storedir}/Summer12_S7_8TeV mc_sig_summer12_s7.txt
+./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_03/mc/Summer12_S7_8TeV ${storedir}/Summer12_S7_8TeV mc_sig_summer12_s7.txt
 
 rm mc_bkg_summer12_s7/*.dat
-./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_00/mc/Summer12_S7_8TeV ${storedir}/Summer12_S7_8TeV mc_bkg_summer12_s7.txt
+./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_03/mc/Summer12_S7_8TeV ${storedir}/Summer12_S7_8TeV mc_bkg_summer12_s7.txt
 
 tar zcf ${version}.tgz  AnalysisScripts/*.dat
--- Reduction/submit_all.sh
+++ Reduction/submit_all.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-./submit_reduction.sh data_2012  PhotonPromptReco\*  50 & 
+./submit_reduction.sh data_2012  PhotonPromptReco2012A  50 & 
+## ./submit_reduction.sh data_2012  PhotonPromptReco2012A_p3  1 & 
+./submit_reduction.sh data_2012  DiPhotonPromptReco2012B  50 & 
 
 ./submit_reduction.sh mc_sig_summer12_s7 \* 5 & 
 
@@ -8,11 +10,9 @@
 ./submit_reduction.sh mc_bkg_summer12_s7 DiPhoton\*_Pt-25To250\*  30 & 
 ./submit_reduction.sh mc_bkg_summer12_s7 DiPhoton\*_Pt-250ToInf\* 20 & 
 
-./submit_reduction.sh mc_bkg_summer12_s7 DYJetsToLL_M-50 50 & 
-./submit_reduction.sh mc_bkg_summer12_s7 DYJetsToLL_M-50_52_1e6 5 & 
+./submit_reduction.sh mc_bkg_summer12_s7 DYJetsToLL\* 70 & 
 
 ./submit_reduction.sh mc_bkg_summer12_s7 QCD_Pt\* 40 & 
-
 ./submit_reduction.sh mc_bkg_summer12_s7 GJet\* 40 &
 
 wait
