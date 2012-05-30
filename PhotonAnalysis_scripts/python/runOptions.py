ed49916aa2283ac03ede94f1873f9763a53aabc8
musella <>
2012-05-30 07:40:11 +0000

    Removed PhotonAnalysis_scripts directory

--- PhotonAnalysis_scripts/python/runOptions.py
+++
@@ -1,6 +0,0 @@
-from optparse import OptionParser
-parser = OptionParser()
-parser.add_option("","--dryRun",action="store_true",dest="dryRun",default=False)
-parser.add_option("-i","--inputDat",dest="inputDat")
-parser.add_option("-n","--nJobs",dest="nJobs",default=-1)
-parser.add_option("-j","--jobId",dest="jobId",default=0)
