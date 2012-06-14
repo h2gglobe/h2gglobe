f026d045c697aaa19facc4bd38f2480320591ac9
drberry <>
2012-06-14 19:23:48 +0000

    Rollback to version 1.15

--- Macros/InterpolateMass.C
+++ Macros/InterpolateMass.C
@@ -13,14 +13,13 @@
 #include <string>
 
 #include "th1fmorph.C"
-#include "Normalization_7TeV.C"
-//#include "Normalization_8TeV.h"
+#include "Normalization_8TeV.h"
 //#include "Normalization_ff.C"
 
 using namespace std;
 using namespace RooFit;
 
-//Normalization_8TeV *normalizer = new Normalization_8TeV();
+Normalization_8TeV *normalizer = new Normalization_8TeV();
 
 string dtoa(double value) {
     stringstream sstr;
@@ -77,8 +76,7 @@ void dofit(double fitmass, vector <TString> InterpolationList, TFile* SourceFile
     TH1F* UpperHist = (TH1F*) SourceFile->Get(UpperHistName.Data());
     if (debug>=1) cout << "Calculating mass point at " << fitmass << "GeV with histograms " << LowerHistName << " and " << UpperHistName << endl;
 
-    //double Normalization = normalizer->GetNorm(lowerbound, LowerHist, upperbound, UpperHist, fitmass);
-    double Normalization = GetNorm(lowerbound, LowerHist, upperbound, UpperHist, fitmass);
+    double Normalization = normalizer->GetNorm(lowerbound, LowerHist, upperbound, UpperHist, fitmass);
 
     TH1F* MCHist = (TH1F*) SourceFile->Get(HistName.Data());
     TH1F* InterpolatedHist = (TH1F*) th1fmorph((Char_t*) HistName.Data(),(Char_t*) HistTitle.Data(),LowerHist,UpperHist,lowerbound,upperbound,fitmass,Normalization,0);
