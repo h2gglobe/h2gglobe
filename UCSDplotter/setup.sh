55fa27212cc30cac0c6b74968d3caf302b250548
aholz <>
2012-06-28 08:37:06 +0000

    - initial commit: file to be sourced to initialize environment variables

---
+++ UCSDplotter/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+cd $(scram list -c CMSSW CMSSW_5_2_0 | awk '{ print $3; }') ; cmsenv ; cd -
+
+# set environment for Boost
+cd $CMSSW_BASE
+export $(scram tool info boost | grep BOOST_BASE)
