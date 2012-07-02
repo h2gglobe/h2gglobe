d4edd8e83cb615ebb0855fba5344e81d4db01be5
aholz <>
2012-07-02 12:20:42 +0000

    - simplified the internal organization of the histograms to   just one container of TH1 instead of having three containers   for TH1F, TH2F and TProfile.
    - added methods:
    
        getItypes(..)
        makeHistoSum(..)
        makeSumName(..)
        hasCategory(..)
        createHistogram(..)
    
    - added an enum HistoType
    
    - added an internal class HistoDef

--- UCSDplotter/HistoContainer.cc
+++ UCSDplotter/HistoContainer.cc
@@ -4,6 +4,10 @@
 #include <cassert>
 
 #include <boost/foreach.hpp>
+#include <boost/lexical_cast.hpp>
+
+
+#include <stdexcept>
 
 #define HCDEBUG 1
 
@@ -50,20 +54,28 @@ void HistoContainer::setScale(float scale){
 
 //----------------------------------------------------------------------
 
-std::string HistoContainer::ModifiedName(const char* name, int itype, int cat) 
+std::string HistoContainer::fullName(const string &name, int itype, int cat)
 {
-  char* modName= new char[500];
-  sprintf(modName,"%s_cat%d_itype%d%s",name, cat, itype, histNam.c_str());
-  return modName;
+  string suffix = "itype" + boost::lexical_cast<string>(itype);
+  return makeSumName(name, suffix, cat);
 }
 
 //----------------------------------------------------------------------
-TH1F *
-HistoContainer::Get1Dhisto(const std::string &name, int itype, int category)
-{
-  map<string, vector< map<int, TH1F *> > >::iterator it = h1.find(name);
 
-  if (it == h1.end()) 
+std::string
+HistoContainer::makeSumName(const std::string &name, const std::string &suffix, int cat)
+{
+  return name + "_" + "cat" + boost::lexical_cast<string>(cat) +
+      "_" + suffix + histNam;
+}
+
+//----------------------------------------------------------------------
+map<int, TH1 *> *
+HistoContainer::getMapForNameAndCategory(const std::string &name, int category)
+{
+  map<string, vector< map<int, TH1 *> > >::iterator it = histos.find(name);
+
+  if (it == histos.end())
     {    // a plot with this name was not booked
        cout << "NAME NOT FOUND" << endl;
     return NULL;
@@ -74,31 +86,101 @@ HistoContainer::Get1Dhisto(const std::string &name, int itype, int category)
       cout << "CATEGORY OUT OF RANGE" << endl;
     return NULL;
     }
-  map<int, TH1F *> &histoMap = it->second[category];
+
+  return &it->second[category];
+}
+
+//----------------------------------------------------------------------
+TH1 *
+HistoContainer::createHistogram(const std::string &name, const HistoDef &histoDef)
+{
+  switch (histoDef.type)
+  {
+  case HISTO_1D:
+    {
+      TH1F *histo_temp = new TH1F(name.c_str(), name.c_str(), histoDef.xbins, histoDef.xmin, histoDef.xmax);
+      histo_temp->Sumw2();
+      histo_temp->GetXaxis()->SetTitle(histoDef.xtitle.c_str());
+      histo_temp->GetYaxis()->SetTitle(histoDef.ytitle.c_str());
+      histo_temp->SetDirectory(0);
+
+      return histo_temp;
+    }
+
+  case HISTO_2D:
+  {
+    TH2F *histo_temp = new TH2F(name.c_str(), name.c_str(),
+                                histoDef.xbins,
+                                histoDef.xmin,
+                                histoDef.xmax,
+
+                                histoDef.ybins,
+                                histoDef.ymin,
+                                histoDef.ymax);
+
+    histo_temp->Sumw2();
+    histo_temp->GetXaxis()->SetTitle(histoDef.xtitle.c_str());
+    histo_temp->GetYaxis()->SetTitle(histoDef.ytitle.c_str());
+    histo_temp->SetDirectory(0);
+
+    return histo_temp;
+  }
+
+  case HISTO_PROFILE:
+  {
+    TProfile *histo_temp = new TProfile(name.c_str(), name.c_str(),
+      histoDef.xbins,
+      histoDef.xmin,
+      histoDef.xmax,
+
+      // histoDef.ybins,
+      histoDef.ymin,
+      histoDef.ymax);
+
+      histo_temp->Sumw2();
+      histo_temp->GetXaxis()->SetTitle(histoDef.xtitle.c_str());
+      histo_temp->GetYaxis()->SetTitle(histoDef.ytitle.c_str());
+      histo_temp->SetDirectory(0);
+
+      return histo_temp;
+  }
+
+  default:
+    cerr << "internal error on line " << __LINE__ << " in file " << __FILE__ << endl;
+    exit(1);
+  }
+}
+
+//----------------------------------------------------------------------
+
+TH1F*
+HistoContainer::Get1Dhisto(const std::string &name, int itype, int category)
+{
+  map<int, TH1 *> *histoMap = getMapForNameAndCategory(name, category);
+  if (histoMap == NULL)
+    // something was wrong
+    return NULL;
 
   // try to find itype
-  map<int, TH1F *>::iterator it2 = histoMap.find(itype);
+  map<int, TH1 *>::iterator it2 = histoMap->find(itype);
 
-  if (it2 != histoMap.end())
-    return it2->second;
+  if (it2 != histoMap->end())
+  {
+    assert(it2->second != NULL);
+    TH1F *retval = dynamic_cast<TH1F*>(it2->second);
+    assert(retval != NULL);
+    return retval;
+  }
 
   // book a new histogram for this itype
-  std::string modName = ModifiedName(name.c_str(), itype, category);
+  std::string modName = fullName(name.c_str(), itype, category);
 
   // get parameters for histogram booking 
   HistoDef histoDef = histoDefs[name];
 
-  TH1F *histo_temp = new TH1F(modName.c_str(), modName.c_str(), 
-                              histoDef.xbins, 
-                              histoDef.xmin, 
-                              histoDef.xmax);
-
-  histoMap[itype] = histo_temp;
-
-  histo_temp->Sumw2();
-  histo_temp->GetXaxis()->SetTitle(histoDef.xtitle.c_str());
-  histo_temp->GetYaxis()->SetTitle(histoDef.ytitle.c_str());
-  histo_temp->SetDirectory(0);
+  TH1F *histo_temp = dynamic_cast<TH1F*>(createHistogram(modName, histoDef));
+  assert(histo_temp != NULL);
+  (*histoMap)[itype] = histo_temp;
 
   return histo_temp;
 }
@@ -108,46 +190,31 @@ HistoContainer::Get1Dhisto(const std::string &name, int itype, int category)
 TH2F *
 HistoContainer::Get2Dhisto(const std::string &name, int itype, int category)
 {
-  map<string, vector< map<int, TH2F *> > >::iterator it = h2.find(name);
-
-  if (it == h2.end()) 
-    // a plot with this name was not booked 
+  map<int, TH1 *> *histoMap = getMapForNameAndCategory(name, category);
+  if (histoMap == NULL)
+    // something was wrong
     return NULL;
-
-  // get category
-  if (category < 0 || category >= it->second.size())
-    // category out of range
-    return NULL;
-
-  map<int, TH2F *> &histoMap = it->second[category];
 
   // try to find itype
-  map<int, TH2F *>::iterator it2 = histoMap.find(itype);
+  map<int, TH1 *>::iterator it2 = histoMap->find(itype);
 
-  if (it2 != histoMap.end())
-    return it2->second;
+  if (it2 != histoMap->end())
+  {
+    assert(it2->second != NULL);
+    TH2F *retval = dynamic_cast<TH2F*>(it2->second);
+    assert(retval != NULL);
+    return retval;
+  }
 
   // book a new histogram for this itype
-  std::string modName = ModifiedName(name.c_str(), itype, category);
+  std::string modName = fullName(name.c_str(), itype, category);
 
   // get parameters for histogram booking 
   HistoDef histoDef = histoDefs[name];
 
-  TH2F *histo_temp = new TH2F(modName.c_str(), modName.c_str(), 
-                              histoDef.xbins, 
-                              histoDef.xmin, 
-                              histoDef.xmax,
-
-                              histoDef.ybins, 
-                              histoDef.ymin, 
-                              histoDef.ymax);
-
-  histoMap[itype] = histo_temp;
-
-  histo_temp->Sumw2();
-  histo_temp->GetXaxis()->SetTitle(histoDef.xtitle.c_str());
-  histo_temp->GetYaxis()->SetTitle(histoDef.ytitle.c_str());
-  histo_temp->SetDirectory(0);
+  TH2F *histo_temp = dynamic_cast<TH2F*>(createHistogram(modName, histoDef));
+  assert(histo_temp != NULL);
+  (*histoMap)[itype] = histo_temp;
 
   return histo_temp;
 }
@@ -156,68 +223,62 @@ HistoContainer::Get2Dhisto(const std::string &name, int itype, int category)
 TProfile *
 HistoContainer::GetProfileHisto(const std::string &name, int itype, int category)
 {
-  map<string, vector< map<int, TProfile *> > >::iterator it = hp.find(name);
-
-  if (it == hp.end()) 
-    // a plot with this name was not booked 
+  map<int, TH1 *> *histoMap = getMapForNameAndCategory(name, category);
+  if (histoMap == NULL)
+    // something was wrong
     return NULL;
-
-  // get category
-  if (category < 0 || category >= it->second.size())
-    // category out of range
-    return NULL;
-
-  map<int, TProfile *> &histoMap = it->second[category];
 
   // try to find itype
-  map<int, TProfile *>::iterator it2 = histoMap.find(itype);
+  map<int, TH1 *>::iterator it2 = histoMap->find(itype);
 
-  if (it2 != histoMap.end())
-    return it2->second;
+  if (it2 != histoMap->end())
+  {
+    assert(it2->second != NULL);
+    TProfile *retval = dynamic_cast<TProfile*>(it2->second);
+    assert(retval != NULL);
+    return retval;
+  }
 
   // book a new histogram for this itype
-  std::string modName = ModifiedName(name.c_str(), itype, category);
+  std::string modName = fullName(name.c_str(), itype, category);
 
   // get parameters for histogram booking 
   HistoDef histoDef = histoDefs[name];
 
-  TProfile *histo_temp = new TProfile(modName.c_str(), modName.c_str(), 
-                              histoDef.xbins, 
-                              histoDef.xmin, 
-                              histoDef.xmax,
-
-                              // histoDef.ybins, 
-                              histoDef.ymin, 
-                              histoDef.ymax);
-
-  histoMap[itype] = histo_temp;
-
-  histo_temp->Sumw2();
-  histo_temp->GetXaxis()->SetTitle(histoDef.xtitle.c_str());
-  histo_temp->GetYaxis()->SetTitle(histoDef.ytitle.c_str());
-  histo_temp->SetDirectory(0);
+  TProfile *histo_temp = dynamic_cast<TProfile*>(createHistogram(modName, histoDef));
+  assert(histo_temp != NULL);
+  (*histoMap)[itype] = histo_temp;
 
   return histo_temp;
 }
 
 //----------------------------------------------------------------------
-
-void HistoContainer::Add(const std::string &name, const std::string &xaxis, const std::string &yaxis, int categories,int bins, float xmin, float xmax) 
+void
+HistoContainer::Add(const std::string &name, const HistoDef &histoDef, int categories)
 {
   // ensure there is not already a histogram with this name
-  assert(h1[name].size() == 0);
+  assert(histos[name].size() == 0);
+  names.push_back(name);
 
   for (int i=0; i<categories; i++) 
   {
     // add empty containers to remember the number of categories
-    h1[name].push_back(map<int, TH1F*>());
+    histos[name].push_back(map<int, TH1*>());
   }
 
-  names.push_back(name);
-  // h1[std::string(name)] = temp;
+  histoDefs[name] = histoDef;
 
+}
+
+//----------------------------------------------------------------------
+
+void HistoContainer::Add(const std::string &name, const std::string &xaxis, const std::string &yaxis, int categories,int bins, float xmin, float xmax)
+{
   // store parameters
   HistoDef histoDef;
+
+  histoDef.type = HISTO_1D;
+
   histoDef.xtitle = xaxis;
   histoDef.ytitle = yaxis;
   histoDef.xbins = bins;
@@ -226,7 +287,7 @@ void HistoContainer::Add(const std::string &name, const std::string &xaxis, cons
 
   // leave the y axis parameters unset
 
-  histoDefs[name] = histoDef;
+  Add(name, histoDef, categories);
 }
 
 //----------------------------------------------------------------------
@@ -234,21 +295,11 @@ void HistoContainer::Add(const std::string &name, const std::string &xaxis, cons
 void HistoContainer::Add(const std::string &name, const std::string &xaxis, const std::string &yaxis, int categories, int binsx, float xmin, float xmax,
 			 int binsy, float ymin, float ymax) 
 {
-
-  // ensure there is not already a histogram with this name
-  assert(h2[name].size() == 0);
-
-  for (int i=0; i<categories; i++) 
-  {
-    // add empty containers to remember the number of categories
-    h2[name].push_back(map<int, TH2F*>());
-  }
-
-  names.push_back(name);
-  // h1[std::string(name)] = temp;
-
   // store parameters
   HistoDef histoDef;
+
+  histoDef.type = HISTO_2D;
+
   histoDef.xtitle = xaxis;
   histoDef.ytitle = yaxis;
   histoDef.xbins = binsx;
@@ -258,8 +309,8 @@ void HistoContainer::Add(const std::string &name, const std::string &xaxis, cons
   histoDef.ybins = binsy;
   histoDef.ymin = ymin;
   histoDef.ymax = ymax;
-  
-  histoDefs[name] = histoDef;
+
+  Add(name, histoDef, categories);
 
 }
 
@@ -267,24 +318,13 @@ void HistoContainer::Add(const std::string &name, const std::string &xaxis, cons
 
 void HistoContainer::Add(char* name, char* xaxis, char* yaxis, int categories, int binsx, 
 			 float xmin, float xmax,
-			 float ymin, float ymax) {
-
-
-
-  // ensure there is not already a histogram with this name
-  assert(hp[name].size() == 0);
-
-  for (int i=0; i<categories; i++) 
-  {
-    // add empty containers to remember the number of categories
-    hp[name].push_back(map<int, TProfile*>());
-  }
-
-  names.push_back(name);
-  // h1[std::string(name)] = temp;
-
+			 float ymin, float ymax)
+{
   // store parameters
   HistoDef histoDef;
+
+  histoDef.type = HISTO_PROFILE;
+
   histoDef.xtitle = xaxis;
   histoDef.ytitle = yaxis;
   histoDef.xbins = binsx;
@@ -295,8 +335,7 @@ void HistoContainer::Add(char* name, char* xaxis, char* yaxis, int categories, i
   histoDef.ymin = ymin;
   histoDef.ymax = ymax;
   
-  histoDefs[name] = histoDef;
-
+  Add(name, histoDef, categories);
 } 
 
 //----------------------------------------------------------------------
@@ -361,53 +400,18 @@ void HistoContainer::Fill2D(const std::string &name, int itype, int category, fl
 //----------------------------------------------------------------------
 
 void HistoContainer::Save() {
-  // TH1
+  map<string, vector<map<int, TH1 *> > >::iterator it;
+  for (it = histos.begin(); it != histos.end(); ++it)
   {
-    map<string, vector<map<int, TH1F *> > >::iterator it;
-    for (it = h1.begin(); it != h1.end(); ++it) 
+    for (unsigned int i = 0; i < (it->second).size(); i++)
+    {
+      map<int, TH1*> &tmp = (it->second)[i];
+      for (map<int, TH1 *>::iterator it2 = tmp.begin(); it2 != tmp.end(); ++it2)
       {
-        for (unsigned int i=0; i<(it->second).size(); i++) 
-          {
-            map<int, TH1F* > &tmp = (it->second)[i];
-            for (map<int, TH1F *>::iterator it2 = tmp.begin(); it2 != tmp.end(); ++it2)
-              {
-                it2->second->Write();
-              } // loop over itypes
-          } // loop over categories
-      } // loop over plot names
-  }
-
-  // TH2
-  {
-    map<string, vector<map<int, TH2F *> > >::iterator it;
-    for (it = h2.begin(); it != h2.end(); ++it) 
-      {
-        for (unsigned int i=0; i<(it->second).size(); i++) 
-          {
-            map<int, TH2F* > &tmp = (it->second)[i];
-            for (map<int, TH2F *>::iterator it2 = tmp.begin(); it2 != tmp.end(); ++it2)
-              {
-                it2->second->Write();
-              } // loop over itypes
-          } // loop over categories
-      } // loop over plot names
-  }
-
-  // TProfile
-  {
-    map<string, vector<map<int, TProfile *> > >::iterator it;
-    for (it = hp.begin(); it != hp.end(); ++it) 
-      {
-        for (unsigned int i=0; i<(it->second).size(); i++) 
-          {
-            map<int, TProfile* > &tmp = (it->second)[i];
-            for (map<int, TProfile *>::iterator it2 = tmp.begin(); it2 != tmp.end(); ++it2)
-              {
-                it2->second->Write();
-              } // loop over itypes
-          } // loop over categories
-      } // loop over plot names
-  }
+        it2->second->Write();
+      } // loop over itypes
+    } // loop over categories
+  } // loop over plot names
 
 
 }
@@ -433,17 +437,9 @@ void HistoContainer::Save() {
 
 int HistoContainer::getNumCategories(const std::string &name)
 {
-   map<string, vector<map<int, TH1F *> > >::iterator it = h1.find(name);
-   if (it != h1.end())
+   map<string, vector<map<int, TH1 *> > >::iterator it = histos.find(name);
+   if (it != histos.end())
      return it->second.size();
-
-   map<string, vector<map<int, TH2F *> > >::iterator it2 = h2.find(name);
-   if (it2 != h2.end())
-     return it2->second.size();
-
-   map<string, vector<map<int, TProfile *> > >::iterator itp = hp.find(name);
-   if (itp != hp.end())
-     return itp->second.size();
 
    return -1;
  }
@@ -571,5 +567,75 @@ int HistoContainer::getNumCategories(const std::string &name)
 // 
 //   return "";
 // }
+
+//----------------------------------------------------------------------
+
+std::vector<int>
+HistoContainer::getItypes(const std::string &plotName, int cat)
+{
+  assert(cat >= 0);
+  {
+    map<string, vector<map<int, TH1 *> > >::iterator it1 = histos.find(plotName);
+    if (it1 != histos.end())
+    {
+      vector<map<int, TH1 *> > &tmp = it1->second;
+      assert(cat < tmp.size());
+
+      vector<int> retval;
+      for (map<int, TH1*>::const_iterator it = tmp[cat].begin(); it != tmp[cat].end(); ++it)
+        retval.push_back(it->first);
+
+      return retval;
+    }
+  }
+
+  throw std::runtime_error("could not find plot '" + plotName + "' cat " + boost::lexical_cast<string>(cat));
+
+}
+//----------------------------------------------------------------------
+
+TH1*
+HistoContainer::makeHistoSum(const std::string &name, const std::string &sumName, const std::vector<int> &itypes, int cat)
+{
+  string fullName = this->makeSumName(name, sumName, cat);
+
+  // check what type of histogram the histograms to be summed are
+  // (take the type from the first histogram)
+  //
+  // we also have use cases where we e.g. only have data but no MC
+  //assert(itypes.size() > 0);
+
+  assert(this->histoDefs.count(name) > 0);
+
+  // book the sum histogram
+  TH1 *retval = this->createHistogram(fullName, histoDefs[name]);
+
+  BOOST_FOREACH(int itype, itypes)
+  {
+    map<int, TH1*>::iterator it = histos[name][cat].find(itype);
+    if (it == histos[name][cat].end())
+      // itype never appeared for this category / plot
+      continue;
+
+    retval->Add(it->second);
+
+  } // loop over all itypes
+
+  // should have found at least one histogram
+  return retval;
+}
+
+//----------------------------------------------------------------------
+
+bool
+HistoContainer::hasCategory(const std::string &plotName, int cat)
+{
+  if (cat < 0 || cat >= histos[plotName].size())
+    // out of range anyway
+    return false;
+
+  // check whether something was created/filled for this category
+  return histos[plotName][cat].size() != 0;
+}
 
 //----------------------------------------------------------------------
--- UCSDplotter/HistoContainer.h
+++ UCSDplotter/HistoContainer.h
@@ -7,6 +7,13 @@
 #include <map>
 #include <string>
 
+/** class keeping track of the histograms needed for plots
+ *  (split by category and by processes).
+ *
+ *
+ *  (technical note: we should use TH1 where possible because TH1F/TH1D as well as
+ *   TH2F/TH2D and TProfile inherit from it)
+ */
 class HistoContainer {
 
  public:
@@ -14,6 +21,12 @@ class HistoContainer {
   HistoContainer(int,std::string);
   ~HistoContainer();
     
+  enum HistoType
+  {
+    HISTO_1D = 0,
+    HISTO_2D = 1,
+    HISTO_PROFILE = 2,
+  };
 
   /** books a set of 1D histograms for a plot */
   void Add(const std::string &name, const std::string &xaxisTitle,const std::string &yaxisTitle, 
@@ -43,6 +56,9 @@ class HistoContainer {
   /** @return -1 if the plot name is not found */
   int getNumCategories(const std::string &catname);
 
+  /** get all itypes for a given name and category */
+  std::vector<int> getItypes(const std::string &plotName, int cat);
+
   /** number of different plots ('names') booked */
   int size() { return (int)names.size(); }
 
@@ -67,15 +83,51 @@ class HistoContainer {
 
   void setScale(float);
 
-  /** builds the full histogram name from the given name,
-      the category etc. */
-  std::string ModifiedName(const char* name, int itype, int cat);
+  /** produces a sum of the histograms found for the given name and category
+   *  and given itypes.
+   *
+   *  @param sumName should be something distinguishing this histogram
+   *   from the existing histograms with the same name and category.
+   *   Typical examples are "data", "bg", "sig".
+   *   */
+  TH1* makeHistoSum(const std::string &name, const std::string &sumName, const std::vector<int> &itypes, int cat);
+
+  /** @return true iff this category was encountered for this plot */
+  bool hasCategory(const std::string &plotName, int cat);
 
   /** overall scaling factor (with which
       the given weights are multiplied) */
   float total_scale;
 
  private:
+  //----------
+
+  /** class with histogram definition parameters (except the name) */
+    struct HistoDef
+    {
+      HistoType type;
+      std::string xtitle;
+      std::string ytitle;
+
+      unsigned xbins;
+
+      float xmin, xmax;
+
+      unsigned ybins;
+      float ymin, ymax;
+
+    };
+
+    /** we must keep these here in order to book the histograms
+        per process on demand */
+    std::map<std::string, HistoDef> histoDefs;
+    //----------
+
+  /** helper for the other Add(..) methods */
+  void Add(const std::string &name, const HistoDef &histoDef, int categories);
+
+  std::map<int, TH1 *> *getMapForNameAndCategory(const std::string &name, int category);
+
   /** returns an 1D histogram from the list of histograms
       or creates it if the corresponding process type
       was never seen before */
@@ -85,43 +137,36 @@ class HistoContainer {
 
   TProfile *GetProfileHisto(const std::string &name, int itype, int category);
 
-  //----------
-  /** class with histogram definition parameters (except the name) */
-  struct HistoDef
-  {
-    std::string xtitle;
-    std::string ytitle;
-
-    unsigned xbins;
-
-    float xmin, xmax;
-
-    unsigned ybins;
-    float ymin, ymax;
-    
-  };
-
-  /** we must keep these here in order to book the histograms
-      per process on demand */
-  std::map<std::string, HistoDef> histoDefs;
-  //----------
 
   int histVal;
   std::string histNam;
   std::vector<std::string> names;
   
-  /** list of 1D histograms. 
+  /** list of all 1D, 2D and profile histograms.
       First index is the 'plot name',
       the second index is the category number 
       the third index is the process number
   */
-  std::map<std::string, std::vector< std::map<int, TH1F *> > > h1;
+  std::map<std::string, std::vector< std::map<int, TH1 *> > > histos;
 
-  /** similar to h1 but for 2d histograms */
-  std::map<std::string, std::vector< std::map<int, TH2F *> > > h2;
+//  /** similar to h1 but for 2d histograms */
+//  std::map<std::string, std::vector< std::map<int, TH2F *> > > h2;
+//
+//  /** similar to h1 but for profile histograms */
+//  std::map<std::string, std::vector< std::map<int, TProfile *> > > hp;
 
-  /** similar to h1 but for profile histograms */
-  std::map<std::string, std::vector< std::map<int, TProfile *> > > hp;
+  /** builds the full histogram name from the given name,
+      the category etc. */
+  std::string fullName(const std::string &name, int itype, int cat);
+
+  /** similar to fullName but uses 'suffix' instead of itype */
+  std::string makeSumName(const std::string &name, const std::string &suffix, int cat);
+
+  /** creates a histogram with the specified binning and axis labels etc.
+   *  without adding it to any container.
+   */
+  TH1 *createHistogram(const std::string &name, const HistoDef &histoDef);
+
 };
 
 #endif
