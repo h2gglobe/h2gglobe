ec4de8edaa76b985fdac1cac43b249ba390f14a9
nckw <>
2012-07-17 15:22:27 +0000

    option for doing a list of masses

--- Macros/FullMvaToolkit/python/limit.py
+++ Macros/FullMvaToolkit/python/limit.py
@@ -1,6 +1,11 @@
 import sys,numpy,os
 from optparse import OptionParser
 
+mymasses = []
+def cback(option,opt_str,value,parser):
+	value = value.split(",")
+	for v in value: mymasses.append(float(v))
+
 # UserInput
 parser=OptionParser()
 parser.add_option("-l","--mHMin",default=110, type='float')
@@ -11,10 +16,12 @@ parser.add_option("","--noas",default=False, action="store_true")
 parser.add_option("","--nopl",default=False, action="store_true")
 parser.add_option("","--nomf",default=False, action="store_true")
 parser.add_option("","--noexppl",default=False, action="store_true")
+parser.add_option("","--masses",dest="jobs",action="callback",callback=cback,type='string')
 
 (options,args)=parser.parse_args()
 
 masses = numpy.arange(options.mHMin,options.mHMax+options.mHStep,options.mHStep)
+if len(mymasses)>0: masses = mymasses
 
 Methods = []
 if not options.noas: Methods.append("Asymptotic")
@@ -27,13 +34,16 @@ for m in masses:
   print " -------------------------------"
   print " running mass", m
   print " -------------------------------"
-  if not options.noas: os.system("combine %s/mva-datacard_grad_%3.1f.txt -M Asymptotic --newExpected -m %3.1f --rAbsAcc 0.00001 --rRelAcc 0.00001 --minosAlgo=stepping --noFitAsimov"%(options.runDirectory,m,m))
-  if not options.nopl: os.system("combine %s/mva-datacard_grad_%3.1f.txt -M ProfileLikelihood --signif --pvalue -m %3.1f" %(options.runDirectory,m,m))
-  if not options.nomf: os.system("combine %s/mva-datacard_grad_%3.1f.txt -M MaxLikelihoodFit --rMin -10 --rMax 10  -m %3.1f" %(options.runDirectory,m,m))
-  for M in Methods: os.system("mv higgsCombineTest.%s.* %s/%s"%(M,options.runDirectory,M))
+  if not options.noas: 
+	os.system("combine %s/mva-datacard_grad_%3.1f.txt -M Asymptotic --newExpected -m %3.1f --rAbsAcc 0.00001 --rRelAcc 0.00001 --minosAlgo=stepping --noFitAsimov"%(options.runDirectory,m,m))
+  if not options.nopl: 
+	os.system("combine %s/mva-datacard_grad_%3.1f.txt -M ProfileLikelihood --signif --pvalue -m %3.1f" %(options.runDirectory,m,m))
+  if not options.nomf: 
+	os.system("combine %s/mva-datacard_grad_%3.1f.txt -M MaxLikelihoodFit --rMin -10 --rMax 10  -m %3.1f" %(options.runDirectory,m,m))
+  for M in Methods: os.system("mv -v higgsCombineTest.%s.* %s/%s"%(M,options.runDirectory,M))
   if not options.noexppl: 
 	os.system("combine %s/mva-datacard_grad_%3.1f.txt -M ProfileLikelihood --signif --pvalue -m %3.1f -t -1 --expectSignal 1" %(options.runDirectory,m,m))
-  	os.system("mv higgsCombineTest.ProfileLikelihood.* %s/ExpProfileLikelihood"%options.runDirectory)
+  	os.system("mv -v higgsCombineTest.ProfileLikelihood.* %s/ExpProfileLikelihood"%options.runDirectory)
 
 print "Finished Limits"
 
