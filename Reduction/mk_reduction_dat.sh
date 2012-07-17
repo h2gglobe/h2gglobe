e5fccedcb997c2dfeaa4abe6e345238e091db69d
itopsisg <>
2012-07-17 14:28:23 +0000

    mk_reduction 2012

--- Reduction/mk_reduction_dat.sh
+++ Reduction/mk_reduction_dat.sh
@@ -3,10 +3,10 @@
 . setup.sh
 
 rm data2012_prompt/*.dat
-./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_08/data ${storedir}/data data_2012_prompt.txt
+./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_08/data ${storedir}/data data2012_prompt.txt
 
 rm data2012_rereco/*.dat
-./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_08/data ${storedir}/data data_2012_rereco.txt
+./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_08/data ${storedir}/data data2012_rereco.txt
 
 rm mc_bkg_summer12_s7/*.dat
 ./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_08/mc/Summer12_S7_8TeV ${storedir}/Summer12_S7_8TeV mc_bkg_summer12_s7.txt
