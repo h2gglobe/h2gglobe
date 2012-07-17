af151c18ac871fc5f05bf7e02aea78038d0b9630
itopsisg <>
2012-07-17 13:56:13 +0000

    prompt+rereco+MC 2012

--- Reduction/mk_reduction_dat.sh
+++ Reduction/mk_reduction_dat.sh
@@ -2,13 +2,17 @@
 
 . setup.sh
 
-rm data_2012/*.dat
-./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_06/dataichep2012 ${storedir}/data data_2012.txt
+rm data2012_prompt/*.dat
+./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_08/data ${storedir}/data data_2012_prompt.txt
+
+rm data2012_rereco/*.dat
+./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_08/data ${storedir}/data data_2012_rereco.txt
 
 rm mc_bkg_summer12_s7/*.dat
-./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_06/mc/Summer12_S7_8TeV ${storedir}/Summer12_S7_8TeV mc_bkg_summer12_s7.txt
+./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_08/mc/Summer12_S7_8TeV ${storedir}/Summer12_S7_8TeV mc_bkg_summer12_s7.txt
 
-rm mc_sig_summer12_s7_03_06/*.dat
-./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_06/mc/Summer12_S7_8TeV ${storedir}/Summer12_S7_8TeV mc_sig_summer12_s7_03_06.txt
+rm mc_sig_summer12_s7/*.dat
+./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_08/mc/Summer12_S7_8TeV ${storedir}/Summer12_S7_8TeV mc_sig_summer12_s7.txt
+
 
 tar zcf ${version}.tgz  AnalysisScripts/{common,reduction,aux,*.py}
