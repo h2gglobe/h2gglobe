6dcfa4fc62cefc3913f353576390062012eab6f3
itopsisg <>
2012-07-17 15:53:06 +0000

    submit data2012 prompt

--- Reduction/submit_all_prompt.sh
+++ Reduction/submit_all_prompt.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 ## submit data prompt
-./submit_reduction.sh   data_2012_prompt   DoublePhoton_Run2012B-PromptReco-v1_sub1  400
-./submit_reduction.sh   data_2012_prompt   DoublePhoton_Run2012B-PromptReco-v1_sub2  20
-./submit_reduction.sh   data_2012_prompt   Photon_Run2012A-08Jun2012-v2_sub1  30
-./submit_reduction.sh   data_2012_prompt   Photon_Run2012A-08Jun2012-v2_sub2  1
-./submit_reduction.sh   data_2012_prompt   Photon_Run2012A-08Jun2012-v2_sub3  1
-./submit_reduction.sh   data_2012_prompt   Photon_Run2012A-23May2012-v2_sub1  10
-./submit_reduction.sh   data_2012_prompt   Photon_Run2012A-23May2012-v2_sub2  1
-./submit_reduction.sh   data_2012_prompt   Photon_Run2012A-PromptReco-v1_sub1  120
-./submit_reduction.sh   data_2012_prompt   Photon_Run2012A-PromptReco-v1_sub2  5
+./submit_reduction.sh   data2012_prompt   DoublePhoton_Run2012B-PromptReco-v1_sub1  400
+./submit_reduction.sh   data2012_prompt   DoublePhoton_Run2012B-PromptReco-v1_sub2  20
+./submit_reduction.sh   data2012_prompt   Photon_Run2012A-08Jun2012-v2_sub1  30
+./submit_reduction.sh   data2012_prompt   Photon_Run2012A-08Jun2012-v2_sub2  1
+./submit_reduction.sh   data2012_prompt   Photon_Run2012A-08Jun2012-v2_sub3  1
+./submit_reduction.sh   data2012_prompt   Photon_Run2012A-23May2012-v2_sub1  10
+./submit_reduction.sh   data2012_prompt   Photon_Run2012A-23May2012-v2_sub2  1
+./submit_reduction.sh   data2012_prompt   Photon_Run2012A-PromptReco-v1_sub1  120
+./submit_reduction.sh   data2012_prompt   Photon_Run2012A-PromptReco-v1_sub2  5
 
 
-wait
+#wait
 
