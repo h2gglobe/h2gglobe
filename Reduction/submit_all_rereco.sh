fe18c64560c819ae60af0f022862ec2bfd1330e9
itopsisg <>
2012-07-17 15:53:26 +0000

    submit data2012 rereco

--- Reduction/submit_all_rereco.sh
+++ Reduction/submit_all_rereco.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 ## submit data rereco
-./submit_reduction.sh   data_2012_rereco   DoublePhoton_Run2012B-29Jun2012-v1_sub1  350
-./submit_reduction.sh   data_2012_rereco   DoublePhoton_Run2012B-29Jun2012-v1_sub2  10
-./submit_reduction.sh   data_2012_rereco   Photon_Run2012A-29Jun2012-v1_sub1  130
-./submit_reduction.sh   data_2012_rereco   Photon_Run2012A-29Jun2012-v1_sub2  1
-./submit_reduction.sh   data_2012_rereco   Photon_Run2012A-recover_29Jun2012-v1_sub1  15
-./submit_reduction.sh   data_2012_rereco   Photon_Run2012A-recover_29Jun2012-v1_sub2  1
+./submit_reduction.sh   data2012_rereco   DoublePhoton_Run2012B-29Jun2012-v1_sub1  350
+./submit_reduction.sh   data2012_rereco   DoublePhoton_Run2012B-29Jun2012-v1_sub2  10
+./submit_reduction.sh   data2012_rereco   Photon_Run2012A-29Jun2012-v1_sub1  130
+./submit_reduction.sh   data2012_rereco   Photon_Run2012A-29Jun2012-v1_sub2  1
+./submit_reduction.sh   data2012_rereco   Photon_Run2012A-recover_29Jun2012-v1_sub1  15
+./submit_reduction.sh   data2012_rereco   Photon_Run2012A-recover_29Jun2012-v1_sub2  1
 
 
 wait
