a286d890e85f8690fe6588ae8d102af95146beef
musella <>
2012-08-01 09:23:39 +0000

    Importing multi-dimentional fit macros.

--- Macros/ResultScripts/multiDimFit.py
+++ Macros/ResultScripts/multiDimFit.py
@@ -36,40 +36,56 @@ def extract_par_limits(pars, model_name, mass, cl=0.05):
     tin.Close()
     
     return par_limits
+
+def system(cmd):
+    print cmd
+    os.system(cmd)
     
 def main(options, args):
 
 
     model_args  = { "ggHqqH" : "-P HiggsAnalysis.CombinedLimit.PhysicsModel:floatingXSHiggs --PO modes=ggH,qqH",
-                    "cVcF"   : "-P HiggsAnalysis.CombinedLimit.HiggsCouplings:cVcF",
-                    "rVrF"   : "-P HiggsAnalysis.CombinedLimit.PhysicsModel:rVrFXSHiggs"
+                    "cVcF"   : "-P HiggsAnalysis.CombinedLimit.HiggsCouplingsLOSM:cVcF",
+                    "rVrF"   : "-P HiggsAnalysis.CombinedLimit.PhysicsModel:rVrFXSHiggs",
+                    "rV"     : "-P HiggsAnalysis.CombinedLimit.PhysicsModel:rVrFXSHiggs --PO RV",
                     }
+
+    model_combine_args  = { "ggHqqH" : "",
+                            "cVcF"   : "",
+                            "rVrF"   : "",
+                            "rV"     : "--floatOtherPOIs=0 -P RV",
+        }
     
     model_pars  = { "ggHqqH" : ["r_ggH","r_qqH"],
                     "cVcF"   : ["CV","CF"],
                     "rVrF"   : ["RV","RF"],
+                    "rV"     : ["RV"],
                     
                     }
     pars_ranges = { "r_ggH"  : (0.,999999.,False),
                     "r_qqH"  : (0.,999999.,False),
                     "CV"     : (-5.,5.,True),
                     "CF"     : (-5.,5.,True),
-                    "RV"     : (0.,999999.,False),
-                    "RF"     : (0.,999999.,False),
+                    "RV"     : (-10.,999999.,False),
+                    "RF"     : (-10.,999999.,False),
                     }
-    
+
     os.chdir( options.workdir )
+
+    combine = "combine %s " % model_combine_args[options.model]
+    if options.expected > 0.:
+        combine += " -t -1 --expectSignal=%f" % options.expected
 
     ## generate the model
     mass = "%1.4g" % options.mH
     model_name = "%s%1.4g" % ( options.model, options.mH )
     model = "%s.root" % model_name
-    if not os.path.isfile(model):
-        os.system("text2workspace.py %s -o %s -m %1.4g %s" % ( options.datacard, model, options.mH, model_args[options.model] ) )
+    if not os.path.isfile(model) or options.forceRedoWorkspace:
+        system("text2workspace.py %s -o %s -m %1.4g %s" % ( options.datacard, model, options.mH, model_args[options.model] ) )
 
     ## best fit
-    os.system("combine -M MultiDimFit %s --algo=singles -v2 -n %s_single -m %s | tee combine_%s_single.log"  % (
-        model, model_name, mass, model_name ) )
+    system("%s -M MultiDimFit %s --algo=singles -v2 -n %s_single -m %s | tee combine_%s_single.log"  % (
+        combine, model, model_name, mass, model_name ) )
 
     par_limits = extract_par_limits(model_pars[options.model],model_name, mass)
 
@@ -93,57 +109,73 @@ def main(options, args):
     ws.writeToFile("%s_grid_test.root" % model_name)
     fmod.Close()
 
+    if options.singleOnly:
+        return
+
     ## run the NLL scan
     jobs=""
     step = options.npoints / 16 
     for ip in range(options.npoints/step+1):
         jobs="%s %d %d " % ( jobs, ip*step, (ip+1)*step-1 )
     print jobs
-    os.system( "parallel -N2 --eta 'combine -M MultiDimFit %s_grid_test.root -m %s -v2 -n %s_grid{1} --algo=grid --points=%d --firstPoint={1} --lastPoint={2} | tee combine_%s_grid{1}.log' ::: %s " % ( model_name, mass, model_name, options.npoints, model_name, jobs ) )
+    system( "parallel -N2 --eta '%s -M MultiDimFit %s_grid_test.root -m %s -v2 -n %s_grid{1} --algo=grid --points=%d --firstPoint={1} --lastPoint={2} | tee combine_%s_grid{1}.log' ::: %s " % ( combine, model_name, mass, model_name, options.npoints, model_name, jobs ) )
 
     
-sys.argv.append("-b")
-import ROOT
-sys.argv.pop(-1)
-
 if __name__ == "__main__":
     parser = OptionParser(option_list=[
-        
         make_option("-w", "--workdir",
                     action="store", type="string", dest="workdir",
                     default="",
-                    help="", metavar="WORKING_DIR"
+                    help="default : [%default]", metavar="WORKING_DIR"
+                    ),
+        make_option("-f", "--forceRedoWorkspace",
+                    action="store_true",  dest="forceRedoWorkspace",
+                    default=False,
+                    help="default : [%default]",
                     ),
         make_option("-d", "--datacard",
                     action="store", type="string", dest="datacard",
                     default="datacard_fine.txt",
-                    help="", metavar="DATACARD"
+                    help="default : [%default]", metavar="DATACARD"
                     ),
         make_option("-r", "--scanrange",
                     action="store", type="float", dest="scanrange",
                     default=3.,
-                    help="", metavar="SCANRANGE"
+                    help="default : [%default]", metavar="SCANRANGE"
                     ),
         make_option("-m", "--mass",
                     action="store", type="float", dest="mH",
                     default=125.,
-                    help="", metavar=""
+                    help="default : [%default]", metavar=""
                     ),
         make_option("-n", "--npoints",
                     action="store", type="int", dest="npoints",
                     default=10000,
-                    help="", metavar="NPOINTS"
+                    help="default : [%default]", metavar="NPOINTS"
                     ),
         make_option("-M", "--model",
                     action="store", type="string", dest="model",
                     default="ggHqqH",
-                    help="", metavar=""
+                    help="default : [%default]", metavar=""
+                    ),
+        make_option("-s", "--singleOnly",
+                    action="store_true", dest="singleOnly",
+                    default=False,
+                    help="default : [%default]", metavar=""
+                    ),
+        make_option("-e", "--expected",
+                    action="store", type="float", dest="expected",
+                    default=0.,
+                    help="default : [%default]", metavar=""
                     ),
         ])
     
     (options, args) = parser.parse_args()
     if options.workdir == "":
         options.workdir = args.pop(0)
-    
+
+    sys.argv.append("-b")
+    import ROOT
+
     main( options, args )
 
