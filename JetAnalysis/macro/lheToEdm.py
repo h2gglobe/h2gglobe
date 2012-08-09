b00668502ae98d0cc2b58c86c68d6111012c8be6
musella <>
2012-08-09 09:52:44 +0000

    Added some comments and instuctions

--- JetAnalysis/macro/lheToEdm.py
+++ JetAnalysis/macro/lheToEdm.py
@@ -1,29 +1,82 @@
 #!/bin/env cmsRun
+
+##
+## cmsRun configuration to convert LHE files to EDM
+## 
+## Creates LHEEventProduct collection starting from LHE file.
+## It also converts LHE events to genParticles and make genJets.
+## Optionally it runs the Higgs to gamma gamma globe ntuplizer
+##
+## run as:
+## cmsRun lheToEdm.py maxEvents=-1 files=<list of input files> output=<output file>
+##
+
+##
+## For instructions to download the Hgg analyzer see:
+## https://twiki.cern.ch/twiki/bin/viewauth/CMS/Higgs2GAnalyzer#Checking_Out_H2gAnalyzer
+## 
+
+##
+## Note: the HEPMC class has an annoying printout in the constructor
+## To remove it
+## addpkg SimDataFormats/GeneratorProducts 
+## sed 's%cout << "Contructing HepMCProduct" << endl;%%' -i SimDataFormats/GeneratorProducts/src/HepMCProduct.cc
+## scramb -j 10 SimDataFormats/GeneratorProducts
+##
+
 import FWCore.ParameterSet.Config as cms
 
-## process = cms.Process("Gen")
 process = cms.Process("LHE")
 
+##
+## Options through VarParsing
+## https://twiki.cern.ch/twiki/bin/view/CMSPublic/SWGuideAboutPythonConfigFile#VarParsing_Example
+## 
+import FWCore.ParameterSet.VarParsing as VarParsing
+options = VarParsing.VarParsing ('standard')
+options.setDefault ('files', 'file:hgg_spin0_VBF.lhe')
+options.setDefault ('output','')
+options.setDefault ('maxEvents',10)
+
+options.register ('runGlobe',
+                  True, # default value
+                  VarParsing.VarParsing.multiplicity.singleton, # singleton or list
+                  VarParsing.VarParsing.varType.bool,          # string, int, or float
+                  "runGlobe")
+
+options.parseArguments()
+
+output = options.output
+firstInput = options.files[0].replace(".lhe","")
+if output == "" or output == ".root":
+    output = "%s.root" % firstInput
+elif output.startswith("_"):
+    output = "%s%s" % ( firstInput, output )
+annotation = firstInput
+
+
+##
+## LHE source
+## 
 process.source = cms.Source("LHESource",
-	fileNames = cms.untracked.vstring('file:hgg_spin0_VBF.lhe')
+	fileNames = cms.untracked.vstring(options.files)
 )
 
-process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(10))
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(options.maxEvents))
 
 process.configurationMetadata = cms.untracked.PSet(
 	version = cms.untracked.string('alpha'),
 	name = cms.untracked.string('LHEF input'),
-	annotation = cms.untracked.string('hgg spin0 VBF')
+	annotation = cms.untracked.string(annotation)
 )
 
+## Control printouts
 process.load("FWCore.MessageService.MessageLogger_cfi")
 process.MessageLogger.cerr.threshold = 'INFO'
-
-## Options and Output Report
 process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(True) )
-# reduce verbosity
 process.MessageLogger.cerr.FwkReport.reportEvery = cms.untracked.int32(1000)
 
+## Convert LHE to HepMC and then GenParticles
 from GeneratorInterface.LHEInterface.lhe2HepMCConverter_cfi import lhe2HepMCConverter
 process.generator = lhe2HepMCConverter.clone()
 
@@ -39,6 +92,7 @@ process.p0 = cms.Path(
         process.ak5GenJets
 )
 
+## Dump first events
 process.load("SimGeneral.HepPDTESSource.pythiapdt_cfi")
 
 process.printList = cms.EDAnalyzer("ParticleListDrawer",
@@ -50,14 +104,21 @@ process.p = cms.Path(
     process.printList
     )
 
+## Globe ntuplizer
+if options.runGlobe:
+    process.load("HiggsAnalysis.HiggsTo2photons.h2ganalyzer_GEN_cfi")
+    process.h2ganalyzer.RootFileName = output.replace(".root","_globe.root")
+    process.p.insert(1,process.h2ganalyzer)
+
+## Output 
 process.LHE = cms.OutputModule("PoolOutputModule",
                                outputCommands = cms.untracked.vstring('keep *_source_*_*',
                                                                       'keep *_genParticles_*_*',
                                                                       'keep *_ak5GenJets_*_*'),
                                dataset = cms.untracked.PSet(dataTier = cms.untracked.string('LHE')),
-                               fileName = cms.untracked.string('LHE.root')
+                               fileName = cms.untracked.string(output)
 )
 
+## Done
 process.outpath = cms.EndPath(process.LHE)
-
 process.schedule = cms.Schedule(process.p0, process.p, process.outpath)
