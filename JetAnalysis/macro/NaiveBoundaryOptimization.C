1f4ccfd518154f49bc2fc4669caf6798e2d68fc7
musella <>
2012-08-28 09:43:08 +0000

    Implemented 2D categories optimization

--- JetAnalysis/macro/NaiveBoundaryOptimization.C
+++ JetAnalysis/macro/NaiveBoundaryOptimization.C
@@ -1,11 +1,14 @@
 #include "TSpline.h"
 #include "TH1.h"
 #include "TF1.h"
+#include "TF2.h"
+#include "TF3.h"
 #include "RooRealVar.h"
 #include "Math/IFunction.h"
 
 #include <algorithm>
 
+// -----------------------------------------------------------------------------------------------------------------------------------------
 class  HistoToTF1 {
 public:
 	HistoToTF1( TString name, TH1 * g ) { sp_ = new TSpline3(g,name); };
@@ -15,7 +18,7 @@ public:
 	TSpline * sp_;
 };
 
-
+// -----------------------------------------------------------------------------------------------------------------------------------------
 class  HistoIntegral {
 public:
 	HistoIntegral( TString name, TH1 * g ) { 
@@ -34,6 +37,7 @@ public:
 	TF1 * tf1_;
 };
 
+// -----------------------------------------------------------------------------------------------------------------------------------------
 class PieceWiseSignif : public ROOT::Math::IBaseFunctionMultiDim
 {
 public:
@@ -69,24 +73,7 @@ public:
 			ii = jj;
 			lastb = newb;
 		}
-		
-		//// std::sort(x,x+nbound_);
-		//// for(int ii=nbound_-1; ii>=0; --ii) {
-		//// 	int jj=ii-1;
-		//// 	for(; jj>=0; --jj) {
-		//// 		if( x[ii] >= x[jj] + p[0] ) {
-		//// 			break;
-		//// 		}
-		//// 	}
-		//// 	
-		//// 	float csig = sigInt_->Integral(x[jj],x[ii]);
-		//// 	csig *= csig;
-		//// 	csig /= bkgInt_->Integral(x[jj],x[ii]);
-		//// 	nsig2 -= csig;
-		//// 	
-		//// 	ii = jj;
-		//// }
-                return nsig2;
+		return nsig2;
         };
 	
 	virtual double DoEval(const double * x) const { 
@@ -106,39 +93,108 @@ public:
 			      
 };
 
-///// class RooPieceWiseSignif : public RooAbsReal
-///// {
-///// public:
-///// 	RooPieceWiseSignif(PieceWiseSignif * pwsig, RooArgList & vars, double cutoff=2.5e-2) 
-///// 		: pwsig_(pwsig), vars_(vars), cutoff_(cutoff)
-///// 	{
-///// 		
-///// 	}
-///// 	
-///// 	double pippo() { 
-///// 		std::cout << "HERE" << std::endl;
-///// 		return evaluate();
-///// 	}
-///// 	
-///// 	virtual double evaluate() const {
-///// 		std::vector<double> x(vars_.getSize()); 
-///// 		double cutoff=cutoff_;
-///// 		std::cout << "HERE" << std::endl;
-///// 		for(int ii=0; ii<vars_.getSize(); ++ii) {
-///// 			std::cout << dynamic_cast<RooAbsReal*>(vars_.at(ii))->getVal() << std::endl;
-///// 			x[ii] = dynamic_cast<RooAbsReal*>(vars_.at(ii))->getVal();
-///// 			std::cout << x[ii] << std::endl;
-///// 		}
-///// 		return (*pwsig_)(&x[0],&cutoff);
-///// 	};
-///// 
-///// 	virtual TObject* clone(const char* newname) const { return new RooPieceWiseSignif(pwsig_,vars_,cutoff_); };
-///// 	virtual Bool_t readFromStream(std::istream& is, Bool_t compact, Bool_t verbose=kFALSE) { return false; }
-///// 	virtual void writeToStream(std::ostream& os, Bool_t compact) const { return; };
-///// 
-///// private:
-///// 	PieceWiseSignif * pwsig_;
-///// 	RooArgList & vars_;
-///// 	double cutoff_;
-///// };
 
+// -----------------------------------------------------------------------------------------------------------------------------------------
+class PieceWise2DSignif : public ROOT::Math::IBaseFunctionMultiDim
+{
+public:
+	PieceWise2DSignif(int nbound, TF2 * sigInt, TF2 * bkg1Int, TF2 * bkg2Int, TF3 * fomden, double xcutoff=2.5e-2, 
+			  double ycutoff=2.5e-2, double sigNorm=0., double bkg1Norm=0., double bkg2Norm=0.) : 
+		nbound_(nbound), sigInt_(sigInt), bkg1Int_(bkg1Int), bkg2Int_(bkg2Int), fomden_(fomden) {
+			cutoffs_[0] = xcutoff;
+			cutoffs_[1] = ycutoff;
+			double xmin =  sigInt_->GetXmin(), xmax =  sigInt_->GetXmax(), ymin =  sigInt_->GetYmin(), ymax =  sigInt_->GetYmax();
+			std::cout << "PieceWise2DSignif "
+				  << "xmin " << xmin << "\n"
+				  << "xmax " << xmax << "\n"
+				  << "ymin " << ymin << "\n" 
+				  << "ymax " << ymax << "\n" 
+				  << "xcutoff " << xcutoff << "\n" 
+				  << "ycutoff " << ycutoff
+				  << std::endl;
+			sigNorm_  =( sigNorm == 0. ? sigInt_->Integral(xmin,xmax,ymin,ymax) : sigNorm );
+			bkg1Norm_ =( bkg1Norm == 0. ? bkg1Int_->Integral(xmin,xmax,ymin,ymax) : bkg1Norm ); 
+			bkg2Norm_ =( bkg2Norm == 0. ? bkg2Int_->Integral(xmin,xmax,ymin,ymax) : bkg2Norm );
+			std::cout << "sigNorm " << sigNorm_  << "\n"
+				  << "bkg1Norm " << bkg1Norm_ << "\n"
+				  << "bkg2Norm " << bkg2Norm_
+				  << std::endl;
+
+		};
+	
+	double operator() (double *x, double *p) const {
+		
+		double nsig2 = 0.;
+		double firstxb= x[0];		
+		double firstyb= x[nbound_];
+		double lastxb= firstxb;
+		double lastyb= firstyb;
+		double lastSigInt=0., lastBkg1Int=0., lastBkg2Int=0.;
+		
+		std::ostream_iterator< double > output( cout, "," );
+		std::copy(&x[0],&x[2*nbound_-1],output); 
+		std::cout << std::endl;
+		
+		double xmin =  sigInt_->GetXmin(), xmax =  sigInt_->GetXmax(), ymin =  sigInt_->GetYmin(), ymax =  sigInt_->GetYmax();
+			
+		for(int ii=1; ii<nbound_; ++ii) {
+			int jj=ii;
+			double newxb = lastxb;
+			double newyb = lastyb;
+			
+			for(; jj<nbound_; ++jj) {
+				newxb -= x[jj];
+				newyb -= x[nbound_+jj];
+				if( lastxb > newxb + p[0] && lastyb > newyb + p[1] ) {
+					break;
+				}
+			}
+			if( newxb < xmin ) { newxb = xmin; }
+			if( newyb < ymin ) { newyb = ymin; }
+			
+			std::cout << "ibound " << jj << " (" << firstxb << "," << firstyb << ") "<< " (" << newxb << "," << newyb << ") " << nsig2;
+
+			if( lastxb > newxb + p[0] && lastyb > newyb + p[1] ) {
+				float sigInt  = sigInt_->Integral(lastxb,firstxb,lastyb,firstyb)  / sigNorm_;
+				float bkg1Int = bkg1Int_->Integral(lastxb,firstxb,lastyb,firstyb) / bkg1Norm_;
+				float bkg2Int = bkg2Int_->Integral(lastxb,firstxb,lastyb,firstyb) / bkg2Norm_;
+				
+				float sig  = sigInt  - lastSigInt;
+				float bkg1 = bkg2Int - lastBkg1Int;
+				float bkg2 = bkg2Int - lastBkg2Int;
+
+				float num = sig*sig;
+				float den = fomden_->Eval(sig,bkg1,bkg2);
+				
+				lastSigInt = sigInt;
+				lastBkg1Int = bkg1Int;
+				lastBkg2Int = bkg2Int;
+				
+				nsig2 -= num/den;
+			}
+			ii = jj;
+			std::cout << " " << nsig2 << std::endl;
+			lastxb = newxb;
+			lastyb = newyb;
+		}
+		return nsig2;
+        };
+
+	virtual double DoEval(const double * x) const { 
+		std::vector<double> xv(x,x+2*nbound_);
+		std::vector<double> pv(cutoffs_,cutoffs_+2);
+		return this->operator()(&xv[0],&pv[0]); 
+	}; 
+	
+	virtual ROOT::Math::IBaseFunctionMultiDim * Clone() const { return new PieceWise2DSignif(nbound_,sigInt_,bkg1Int_,bkg2Int_,fomden_,cutoffs_[0],cutoffs_[1]); }
+
+	virtual unsigned int NDim() const { return 2*nbound_; }; 
+
+	int nbound_;
+	TF2 * sigInt_; 
+	TF2 * bkg1Int_, * bkg2Int_;
+	TF3 * fomden_;
+	double cutoffs_[2];
+	double sigNorm_, bkg1Norm_, bkg2Norm_;
+
+};
--- JetAnalysis/macro/naiveVbfCatOptimization.py
+++ JetAnalysis/macro/naiveVbfCatOptimization.py
@@ -20,9 +20,172 @@ def readPlot(fin, name, cat=0, which=[""], samples=["diphojet_8TeV","ggh_m125_8T
             
     return ret
 
+def optmizeCats(func, ws, args):
+    
+    grS = ROOT.TGraph()
+    grS.SetName("zVsNcat")
+    summary = {}
+    for iter in range(1,7):
+        boundaries,z = func(iter,ws,*args)
+        ncat = len(boundaries)-1
+        if not ncat in summary or summary[ncat]["z"] < z: 
+            summary[ncat] =  { "z" : z, "boundaries" : boundaries, "ncat": ncat }
+    for ncat,val in summary.iteritems():
+        grS.SetPoint( grS.GetN(), ncat, val["z"] )
+
+    canv1 = ROOT.TCanvas("canv1","canv1")
+    canv1.cd()
+    grS.SetMarkerStyle(ROOT.kFullCircle)
+    grS.Draw("AP")
+
+    ws.rooImport(canv1)
+    ws.rooImport(grS)
+    
+    raw_input("aa")
+
+    sout = open("vbfmva_opt.json","w+")
+    sout.write( json.dumps(summary) )
+
+    ws.writeToFile("vbfmva_opt.root")
+
+def optimizeNcat2D(ncat, ws, sigPx, sigPy, sigIntegral, bkgIntegral1, bkgIntegral2, xmin1, xmin2, cutoff1, cutoff2,
+                   sigNorm, bkg1Norm, bkg2Norm, relNorm=0.1, syst=0.5):
+
+    nbound = ncat+1
+    quantilesX = numpy.zeros(nbound)
+    quantilesY = numpy.zeros(nbound)
+    prbs      = numpy.arange(0.,1.,1./ncat)
+    prbs[0]   = 0.01 
+    sigPx.GetQuantiles(ncat,quantilesX,prbs)
+    sigPy.GetQuantiles(ncat,quantilesY,prbs)
+    quantilesX[ncat] = 1.
+    quantilesY[ncat] = 1.
+    print quantilesX, quantilesY
+
+    fomDen = ROOT.TF3("fomden","x[1]+[1]*x[2] + 2*[0]*[1]*sqrt(x[1]+[1]*x[2])*x[2] + [0]*[0]*[1]*[1]*x[2]*x[2]",0.,1.e+3,0.,1.e+3,0.,1.e+3)
+    fomDen.SetParameter(0,syst)
+    fomDen.SetParameter(1,relNorm)
+
+    pws = ROOT.PieceWise2DSignif(nbound,sigIntegral,bkgIntegral1,bkgIntegral2,fomDen,cutoff1,cutoff2,sigNorm,bkg1Norm,bkg2Norm)
+        
+    minimizer = ROOT.TMinuitMinimizer()
+    minimizer.SetFunction(pws)
+    minimizer.SetFixedVariable(0,"fixedBoundX",1.)
+    minimizer.SetFixedVariable(nbound,"fixedBoundY",1.)
+    for ivar in range(1,nbound):
+        minimizer.SetLimitedVariable(ivar,       "deltaBoundX%d" % ivar, quantilesX[nbound-ivar]-quantilesX[nbound-ivar-1],1.e-3,cutoff1,1.)
+        minimizer.SetLimitedVariable(ivar+nbound,"deltaBoundY%d" % ivar, quantilesY[nbound-ivar]-quantilesY[nbound-ivar-1],1.e-3,cutoff2,1.)
+
+    minimizer.PrintResults()
+    minimizer.Minimize()
+    minimizer.PrintResults()
+    maxval = sqrt(-minimizer.MinValue())
+    xvar = minimizer.X()
+    boundariesX = [ xvar[0] ]
+    boundariesY = [ xvar[nbound] ]
+    for ivar in range(1,nbound):
+        boundariesX.append( boundariesX[-1] - xvar[ivar] )
+        boundariesY.append( boundariesY[-1] - xvar[ivar+nbound] )
+        
+    print "---------------------------------------------"
+    print "input  ncat: ", ncat
+    print "output ncat: ", len(boundariesX)-1, len(boundariesY)-1
+    ## print "max: %1.3g %1.3g" % ( maxval, maxval2 )
+    print "max: %1.3g" % ( maxval )
+    print "boundariesX: ",
+    for b in boundariesX:
+        print "%1.3g" % b,
+    print
+    print "boundariesY: ",
+    for b in boundariesY:
+        print "%1.3g" % b,
+    print
+    print 
+    
+    return (boundariesX,boundariesY),maxval
 
 
-def optimizeNcat(ncat, ws, mva, sig, sigIntegral, bkgIntegral, xmin, cutoff):
+
+def optimize2D(fin):
+    xmin1 = 0.
+    xmin2 = 0.
+    cutoff1 = 0.015
+    cutoff2 = 0.015
+
+    try:
+        ws = fin.Get("ws")
+        
+        sigDs = ws.data("sigDs")
+        bkg1Ds = ws.data("bkg1Ds")
+        bkg2Ds = ws.data("bkg2Ds")
+
+        sigPdf  = ws.pdf("sigPdf")
+        bkg1Pdf = ws.pdf("bkg1Pdf")
+        bkg2Pdf = ws.pdf("bkg2Pdf")
+
+        sigHisto = sigDs.createHistogram(mva1,mva2)
+        sigHistoX = sigHisto.ProjectionX()
+        sigHistoY = sigHisto.ProjectionY()
+        
+    except:
+        ws = ROOT.RooWorkspace("ws","ws")
+        ws.rooImport = getattr(ws,'import')
+        mva1 = ws.factory("mva1[1.,%f.,1.]" % xmin1 )
+        mva2 = ws.factory("mva2[1.,%f.,1.]" % xmin2 )
+        varSet = ROOT.RooArgSet(mva1,mva2)
+        varList = ROOT.RooArgList(mva1,mva2)
+
+        sig = fin.Get("sig")
+        bkg1 = fin.Get("bkg1")
+        bkg2 = fin.Get("bkg2")
+        
+        sigDs = ROOT.RooDataSet("sigDs","sigDs",sig,varSet)
+        bkg1Ds = ROOT.RooDataSet("bkg1Ds","bkg1Ds",bkg1,varSet)
+        bkg2Ds = ROOT.RooDataSet("bkg2Ds","bkg2Ds",bkg2,varSet)
+        
+        sigHisto = sigDs.createHistogram(mva1,mva2)
+        sigHistoX = sigHisto.ProjectionX()
+        sigHistoY = sigHisto.ProjectionY()
+
+        print "Fitting RooKeyPdfs: this takes a while (and they cannot be persisted.....)"
+        sigPdf = ROOT.RooNDKeysPdf("sigPdf","sigPdf",varList,sigDs,"amvv")
+        print "sigPdf done"
+        bkg1Pdf = ROOT.RooNDKeysPdf("bkg1Pdf","bkg1Pdf",varList,bkg1Ds,"amvv")
+        print "bkg1Pdf done"
+        bkg2Pdf = ROOT.RooNDKeysPdf("bkg2Pdf","bkg2Pdf",varList,bkg2Ds,"amvv")
+        print "bkg2Pdf done"
+        
+        ws.rooImport(mva1)
+        ws.rooImport(mva2)
+        ws.rooImport(sig)
+        ws.rooImport(bkg1)
+        ws.rooImport(bkg2)
+        ws.rooImport(sigPdf)
+        ws.rooImport(bkg1Pdf)
+        ws.rooImport(bkg2Pdf)
+
+        ##ws.writeToFile("vbfmva_input.root")
+
+    sigTF2 = sigPdf.asTF(varList)
+    bkg1TF2 = bkg1Pdf.asTF(varList)
+    bkg2TF2 = bkg2Pdf.asTF(varList)
+
+    canv2 = ROOT.TCanvas("canv2","canv2")
+    canv2.cd()
+    sigTF2.SetLineColor(ROOT.kRed)
+    bkg1TF2.SetLineColor(ROOT.kMagenta)
+    sigTF2.Draw("")
+    bkg1TF2.Draw("SAME")
+    bkg2TF2.Draw("SAME")
+
+
+    raw_input("aa")
+    optmizeCats( optimizeNcat2D, ws, (sigHistoX,sigHistoY,sigTF2,bkg1TF2,bkg2TF2,xmin1,xmin2,cutoff1,cutoff2,
+                                      sigDs.sumEntries(), bkg1Pdf.sumEntries(), bkg2Pdf.sumEntries() ) )
+
+    return ws
+
+def optimizeNcat(ncat, ws, sig, sigIntegral, bkgIntegral, xmin, cutoff):
 
     nbound = ncat+1
     quantiles = numpy.zeros(nbound)
@@ -32,10 +195,11 @@ def optimizeNcat(ncat, ws, mva, sig, sigIntegral, bkgIntegral, xmin, cutoff):
     quantiles[ncat] = 1.
 
     pws = ROOT.PieceWiseSignif(nbound,sigIntegral,bkgIntegral,cutoff)
-        
+    
     minimizer = ROOT.TMinuitMinimizer()
     minimizer.SetFunction(pws)
     minimizer.SetFixedVariable(0,"fixedBound",1.)
+    minimizer.PrintResults()
     for ivar in range(1,nbound):
         minimizer.SetLimitedVariable(ivar,"deltaBound%d" % ivar, quantiles[nbound-ivar]-quantiles[nbound-ivar-1],1.e-3,cutoff,1.)
 
@@ -47,30 +211,6 @@ def optimizeNcat(ncat, ws, mva, sig, sigIntegral, bkgIntegral, xmin, cutoff):
     for ivar in range(1,nbound):
         boundaries.append( boundaries[-1] - xvar[ivar] )
         
-    ### for ibound in range(nbound):
-    ###     minimizer.SetLimitedVariable(ibound,"bound%d" % ibound,quantiles[ibound],1.e-3,xmin,0.999)
-    ### minimizer.SetFixedVariable(nbound-1,"fixedBound",1.)
-
-    #### minimizer.Minimize()
-    #### maxval = sqrt(-minimizer.MinValue())
-    #### xvar = minimizer.X()
-    ### boundaries = sorted(set([ round(xvar[i]*400.)/400. for i in range(nbound) ]))
-
-    #### pwsMin = ROOT.PieceWiseSignif(len(boundaries),sigIntegral,bkgIntegral,cutoff)
-    #### minimizer.SetFunction(pwsMin)
-    #### for ibound in range(len(boundaries)):
-    ####     minimizer.SetLimitedVariable(ibound,"bound%d" % ibound,boundaries[ibound],1.e-3,xmin,0.999)
-    #### minimizer.SetFixedVariable(len(boundaries)-1,"fixedBound",1.)
-    #### 
-    #### minimizer.Minimize()
-    #### maxval2 = sqrt(-minimizer.MinValue())
-    #### xvar = minimizer.X()
-    #### boundaries2 = sorted(set([ round(xvar[i]*300.)/300. for i in range(nbound) ]))
-    #### 
-    #### if abs(1. - maxval2/maxval) > 5.e-2 and len(boundaries2) < len(boundaries):
-    ####     boundaries = boundaries2
-    ####     maxval = maxval2
-
     print "---------------------------------------------"
     print "input  ncat: ", ncat
     print "output ncat: ", len(boundaries)-1
@@ -83,15 +223,8 @@ def optimizeNcat(ncat, ws, mva, sig, sigIntegral, bkgIntegral, xmin, cutoff):
     print 
     
     return boundaries,maxval
-        
-def main(fname):
 
-    fin = ROOT.TFile.Open(fname)
-    ROOT.gSystem.SetIncludePath("-I$ROOTSYS/include -I$ROOFITSYS/include")
-    ROOT.gROOT.LoadMacro("NaiveBoundaryOptimization.C+")
-
-    ROOT.RooMsgService.instance().setGlobalKillBelow(ROOT.RooFit.ERROR)
-
+def optimize1D(fin):
     xmin = -1.
     cutoff = 0.015
     
@@ -105,7 +238,7 @@ def main(fname):
     for b in histos[2:]:
         bkg.Add(b)
 
-    ws = ROOT.RooWorkspace()
+    ws = ROOT.RooWorkspace("ws","ws")
     ws.rooImport = getattr(ws,'import')
     mva = ws.factory("mva[0.,%f.,1.]" % -1. )
     xvar = ROOT.RooArgList(mva)
@@ -119,14 +252,6 @@ def main(fname):
     hsig = sigDset.createHistogram("hsig",mva)
     hbkg = bkgDset.createHistogram("hbkg",mva)
     hbkg.Smooth(30)
-
-    ### sigHistoIntegral = ROOT.HistoIntegral("sigHistoIntegral",  hsig )
-    ### sigIntegral = ROOT.TF2("sigIntegral", sigHistoIntegral, xmin, 1., xmin, 1., 1, "HistoIntegral" )
-    ### sigIntegral.SetParameter(0,cutoff)
-    ### 
-    ### bkgHistoIntegral = ROOT.HistoIntegral("bkgHistoIntegral",  hbkg )
-    ### bkgIntegral = ROOT.TF2("bkgIntegral", bkgHistoIntegral, xmin, 1., xmin, 1., 1, "HistoIntegral" )
-    ### bkgIntegral.SetParameter(0,cutoff)
 
     sigHistoToTF1 = ROOT.HistoToTF1("sigHToTF1",hsig)
     sigTF1        = ROOT.TF1("sigTF1",sigHistoToTF1,-1.,1,0,"HistoToTF1")
@@ -143,39 +268,62 @@ def main(fname):
 
     ## raw_input("aa")
 
-    grS = ROOT.TGraph()
-    grS.SetName("zVsNcat")
-    summary = {}
-    for iter in range(1,9):
-        boundaries,z = optimizeNcat(iter,ws,mva,hsig,sigTF1,bkgTF1,xmin,cutoff)
-        z*=sqrt(6.7)
-        ncat = len(boundaries)-1
-        if not ncat in summary or summary[ncat]["z"] < z: 
-            summary[ncat] =  { "z" : z, "boundaries" : boundaries, "ncat": ncat }
-    for ncat,val in summary.iteritems():
-        grS.SetPoint( grS.GetN(), ncat, val["z"] )
-
-    canv1 = ROOT.TCanvas("canv1","canv1")
-    canv1.cd()
-    grS.SetMarkerStyle(ROOT.kFullCircle)
-    grS.Draw("AP")
+    optmizeCats( optimizeNcat, ws, (hsig,sigTF1,bkgTF1,xmin,cutoff) ) 
 
     canv2 = ROOT.TCanvas("canv2","canv2")
     canv2.cd()
     sigTF1.SetLineColor(ROOT.kRed)
     sigTF1.Draw("")
     bkgTF1.Draw("SAME")
-
-    ws.rooImport(canv1)
     ws.rooImport(canv2)
-    ws.rooImport(grS)
     
-    raw_input("aa")
+    #### grS = ROOT.TGraph()
+    #### grS.SetName("zVsNcat")
+    #### summary = {}
+    #### for iter in range(1,9):
+    ####     boundaries,z = optimizeNcat(iter,ws,mva,hsig,sigTF1,bkgTF1,xmin,cutoff)
+    ####     z*=sqrt(6.7)
+    ####     ncat = len(boundaries)-1
+    ####     if not ncat in summary or summary[ncat]["z"] < z: 
+    ####         summary[ncat] =  { "z" : z, "boundaries" : boundaries, "ncat": ncat }
+    #### for ncat,val in summary.iteritems():
+    ####     grS.SetPoint( grS.GetN(), ncat, val["z"] )
+    #### 
+    #### canv1 = ROOT.TCanvas("canv1","canv1")
+    #### canv1.cd()
+    #### grS.SetMarkerStyle(ROOT.kFullCircle)
+    #### grS.Draw("AP")
+    #### 
+    #### canv2 = ROOT.TCanvas("canv2","canv2")
+    #### canv2.cd()
+    #### sigTF1.SetLineColor(ROOT.kRed)
+    #### sigTF1.Draw("")
+    #### bkgTF1.Draw("SAME")
+    #### 
+    #### ws.rooImport(canv1)
+    #### ws.rooImport(canv2)
+    #### ws.rooImport(grS)
+    #### 
+    #### raw_input("aa")
+    #### 
+    #### sout = open("vbfmva_opt.json","w+")
+    #### sout.write( json.dumps(summary) )
+    #### 
+    #### ws.writeToFile("vbfmva_opt.root")
 
-    sout = open("vbfmva_opt.json","w+")
-    sout.write( json.dumps(summary) )
+    return ws
+        
+def main(fname):
 
-    ws.writeToFile("vbfmva_opt.root")
+    fin = ROOT.TFile.Open(fname)
+    ROOT.gSystem.SetIncludePath("-I$ROOTSYS/include -I$ROOFITSYS/include")
+    ROOT.gROOT.LoadMacro("NaiveBoundaryOptimization.C+")
+
+    ## ROOT.RooMsgService.instance().setGlobalKillBelow(ROOT.RooFit.ERROR)
+    
+    ## ws = optimize1D(fin)
+    ws = optimize2D(fin)
+
     
     return ws
     
