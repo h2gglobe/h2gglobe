209866b8f236d244bf17d879ddef91d9831fa026
capalmer <>
2012-09-10 22:06:27 +0000

    protecting rm -r after the unmount

--- AnalysisScripts/python/makeFilelist.py
+++ AnalysisScripts/python/makeFilelist.py
@@ -83,7 +83,10 @@ def unmounteos(dir):
    
    unmount = 'csh -c "eosumount %s "'%dir
    sc,flist = commands.getstatusoutput(unmount) 
-   sc,flist = commands.getstatusoutput("rm -r %s"%dir) 
+   if sc==0: #status must be completed for unmount
+      sc,flist = commands.getstatusoutput("rm -r %s"%dir) 
+   else:
+      sys.exit("Unmount",dir,"failed. Exiting.")
    
 
 def makeEosFiles(dir,njobs=-1,jobid=0,nf=[0]):
