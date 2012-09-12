53594be090f1a023fbd79a026663b378e2dae389
futyand <>
2012-09-12 18:41:24 +0000

    update ot Lisbon ntuples

--- Reduction/mk_reduction_dat.sh
+++ Reduction/mk_reduction_dat.sh
@@ -2,17 +2,10 @@
 
 . setup.sh
 
-rm data2012_prompt/*.dat
-./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_08/data ${storedir}/data data2012_prompt.txt
+rm data2012_53_v1/*.dat
+./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V14_00_02/data ${storedir}/data data2012_53_v1.txt
 
-rm data2012_rereco/*.dat
-./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_08/data ${storedir}/data data2012_rereco.txt
-
-rm mc_bkg_summer12_s7/*.dat
-./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_08/mc/Summer12_S7_8TeV ${storedir}/Summer12_S7_8TeV mc_bkg_summer12_s7.txt
-
-rm mc_sig_summer12_s7/*.dat
-./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V13_03_08/mc/Summer12_S7_8TeV ${storedir}/Summer12_S7_8TeV mc_sig_summer12_s7.txt
-
+rm mc_bkg_summer12_s10/*.dat
+./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V14_00_02/mc/Summer12_S10_8TeV ${storedir}/Summer12_S10_8TeV mc_bkg_summer12_s10.txt
 
 tar zcf ${version}.tgz  AnalysisScripts/{common,reduction,aux,*.py}
