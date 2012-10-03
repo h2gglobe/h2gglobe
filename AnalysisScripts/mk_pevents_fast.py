f436b57892ee5f40fed5e04db237505de7c404ae
drberry <>
2012-10-03 13:11:38 +0000

    Added safe handling of unexpected root files

--- AnalysisScripts/mk_pevents_fast.py
+++ AnalysisScripts/mk_pevents_fast.py
@@ -12,8 +12,8 @@ if (len(argv)<3):
 
 eos="/afs/cern.ch/project/eos/installation/0.2.5/bin/eos.select"
 
-if (len(argv)==3): filelist=popen(eos+" find -f "+argv[1]+" | grep .root | grep -v pileup").readlines()
-else: filelist=popen(eos+" find -f "+argv[1]+" | grep .root | grep -v pileup | "+argv[3]).readlines()
+if (len(argv)==3): filelist=popen(eos+" find -f "+argv[1]+" | grep .root ").readlines()
+else: filelist=popen(eos+" find -f "+argv[1]+" | grep .root | "+argv[3]).readlines()
 
 outfile=open(argv[2],'w')
 
@@ -21,11 +21,11 @@ tfilelist = []
 for i in range(len(filelist)):
     filelist[i]=filelist[i].strip("\n")
     tfilelist.append(ROOT.TFile.Open("root://eoscms/"+filelist[i]))
-    global_variables = tfilelist[i].Get("global_variables")
-    global_variables.GetEntry(0)
-    numevents = global_variables.tot_events
-    outfile.write("root://eoscms/"+filelist[i]+"="+str(numevents)+"\n")
+    if tfilelist[i].Get("global_variables")!=None:
+        global_variables = tfilelist[i].Get("global_variables")
+        global_variables.GetEntry(0)
+        numevents = global_variables.processedEvents
+        outfile.write("root://eoscms/"+filelist[i]+"="+str(numevents)+"\n")
 
 outfile.close()
-#print "pevent file is complete eos is now closing files please suspend program."
 for file in tfilelist: file.Close()
