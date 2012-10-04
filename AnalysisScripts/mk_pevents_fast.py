e04e8e12059cebe0fd19ac0d7458e5420d7d29cd
drberry <>
2012-10-04 09:01:25 +0000

    Added ability to run on just .dat file

--- AnalysisScripts/mk_pevents_fast.py
+++ AnalysisScripts/mk_pevents_fast.py
@@ -2,20 +2,26 @@
 
 from os import popen
 from sys import argv
-import ROOT
 
-if (len(argv)<3):
-    print "Must provide eos directory to run on and an output file!\n"
-    print "\tExample:\n\tpython mk_pevents_fast.py /store/group/phys_higgs/cmshgg/reduced/V13_04_00_v1/mc/Summer12_S7_8TeV/GluGluToHToGG_M-115_8TeV-powheg-pythia6_Summer12-PU_S7_START52_V9-v1/ baseline/tmp_datafiles_baseline_oct1review.dat.pevents &"
-    print "\tpython mk_pevents_fast.py /store/group/phys_higgs/cmshgg/reduced/V13_04_00_v1/mc/Summer12_S7_8TeV/ baseline/tmp_datafiles_baseline_oct1review.dat.pevents \"grep HToGG\" &"
+if (len(argv)==1):
+    print "Must provide either a configuation dat or an eos directory and an output file!\n"
+    print "\tExamples:"
+    print "\tpython mk_pevents_fast.py baseline/datafiles_baseline_oct1review.dat"
+    print "\tpython mk_pevents_fast.py baseline/tmp_datafiles_baseline_oct1review.dat.pevents /store/group/phys_higgs/cmshgg/reduced/V13_04_00_v1/mc/Summer12_S7_8TeV/GluGluToHToGG_M-115_8TeV-powheg-pythia6_Summer12-PU_S7_START52_V9-v1/"
+    print "\tpython mk_pevents_fast.py baseline/tmp_datafiles_baseline_oct1review.dat.pevents /store/group/phys_higgs/cmshgg/reduced/V13_04_00_v1/mc/Summer12_S7_8TeV/ \"grep HToGG\""
     exit(1)
 
+import ROOT
 eos="/afs/cern.ch/project/eos/installation/0.2.5/bin/eos.select"
 
-if (len(argv)==3): filelist=popen(eos+" find -f "+argv[1]+" | grep .root ").readlines()
-else: filelist=popen(eos+" find -f "+argv[1]+" | grep .root | "+argv[3]).readlines()
+if len(argv)==2: filelist=popen("grep CaDir "+argv[1]+" | grep -v typ=0 | sed 's|.*CaDir=\\(\\S*\\).*|\\1|' | xargs -i "+eos+" find -f {} | grep .root").readlines()
+if len(argv)==3: filelist=popen(eos+" find -f "+argv[2]+" | grep .root ").readlines()
+if len(argv)==4: filelist=popen(eos+" find -f "+argv[2]+" | grep .root | "+argv[3]).readlines()
 
-outfile=open(argv[2],'w')
+if len(argv)==2:
+    if argv[1].rfind("/")==-1:outfile=open("tmp_"+argv[1]+".pevents",'w')
+    else: outfile=open(argv[1][:argv[1].rfind("/")+1]+"tmp_"+argv[1][argv[1].rfind("/")+1:]+".pevents",'w')
+else: outfile=open(argv[1],'w')
 
 tfilelist = []
 for i in range(len(filelist)):
