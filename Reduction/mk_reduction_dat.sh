9296fa5dbab5f28192eef428b7808c8e5679b496
capalmer <>
2012-10-06 08:06:48 +0000

    updating dat files for reduction

--- Reduction/mk_reduction_dat.sh
+++ Reduction/mk_reduction_dat.sh
@@ -5,7 +5,10 @@
 rm data2012_HCP/*.dat
 ./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V14_00_02/data ${storedir}/data data2012_HCP.txt
 
-rm mc_bkg_summer12_s10/*.dat
-./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V14_00_03/mc/Summer12_S10_8TeV ${storedir}/mc/Summer12_S10_8TeV mc_bkg_summer12_s10.txt
+#rm mc_bkg_summer12_s10/*.dat
+#./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V14_00_03/mc/Summer12_S10_8TeV ${storedir}/mc/Summer12_S10_8TeV mc_bkg_summer12_s10.txt
+
+rm mc_sig_summer12_s10/*.dat
+./AnalysisScripts/mk_reduction_dat.py /store/group/phys_higgs/cmshgg/processed/V14_00_03/mc/Summer12_S10_8TeV ${storedir}/mc/Summer12_S10_8TeV mc_sig_summer12_s10.txt
 
 tar zcf ${version}.tgz  AnalysisScripts/{common,reduction,aux,*.py}
--- Reduction/submit_all.sh
+++ Reduction/submit_all.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 ## submit data prompt
-./submit_reduction.sh     data2012_53_v1          DoublePhoton_Run2012C-PromptReco-v1  50
-./submit_reduction.sh     data2012_53_v1          DoublePhoton_Run2012C-PromptReco-v2  200
-./submit_reduction.sh     data2012_53_v1          Photon_Run2012A-13Jul2012-v1         40
-./submit_reduction.sh     data2012_53_v1          Photon_Run2012A-recover-06Aug2012-v1 10
-./submit_reduction.sh     data2012_53_v1          DoublePhoton_Run2012B-13Jul2012-v1         200
+./submit_reduction.sh     data2012_HCP          DoublePhoton_Run2012B-13Jul2012-v1          200
+./submit_reduction.sh     data2012_HCP          DoublePhoton_Run2012B-13Jul2012-v1_sub2     5
+./submit_reduction.sh     data2012_HCP          DoublePhoton_Run2012B-13Jul2012-v1_sub3     3
+./submit_reduction.sh     data2012_HCP          DoublePhoton_Run2012C-PromptReco-v2         250
+./submit_reduction.sh     data2012_HCP          DoublePhoton_Run2012C-PromptReco-v2_sub2    8
+./submit_reduction.sh     data2012_HCP          DoublePhoton_Run2012C-PromptReco-v2_sub4    100
+./submit_reduction.sh     data2012_HCP          Photon_Run2012A-13Jul2012-v1                30
+./submit_reduction.sh     data2012_HCP          Photon_Run2012A-13Jul2012-v1_sub2           4
+./submit_reduction.sh     data2012_HCP          Photon_Run2012A-13Jul2012-v1_sub3           1
+./submit_reduction.sh     data2012_HCP          Photon_Run2012A-recover-06Aug2012-v1        5
+./submit_reduction.sh     data2012_HCP          Photon_Run2012A-recover-06Aug2012-v1__sub1  1
+./submit_reduction.sh     data2012_HCP          Run2012C-24Aug2012-v2                       30
 
-./submit_reduction.sh     mc_bkg_summer12_s10     DYJetsToLL_M-50 20
+./submit_reduction.sh     mc_sig_summer12_s10     \* 4
 
 wait
 
