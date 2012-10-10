5c7d3d6aaf99b76dfa3a31fb8fa3ec9fb99914f4
drberry <>
2012-10-10 09:25:49 +0000

    h2gglobe_HCP2012_preapproval_red_v1

--- Reduction/submit_all.sh
+++ Reduction/submit_all.sh
@@ -2,17 +2,19 @@
 
 ## submit data prompt
 ./submit_reduction.sh     data2012_HCP          DoublePhoton_Run2012B-13Jul2012-v1          200
-./submit_reduction.sh     data2012_HCP          DoublePhoton_Run2012B-13Jul2012-v1_sub2     5
+./submit_reduction.sh     data2012_HCP          DoublePhoton_Run2012B-13Jul2012-v1_sub2     10
 ./submit_reduction.sh     data2012_HCP          DoublePhoton_Run2012B-13Jul2012-v1_sub3     10
-./submit_reduction.sh     data2012_HCP          DoublePhoton_Run2012B-13Jul2012-v1_sub4     3
+./submit_reduction.sh     data2012_HCP          DoublePhoton_Run2012B-13Jul2012-v1_sub4     5
+./submit_reduction.sh     data2012_HCP          DoublePhoton_Run2012B-13Jul2012-v1_sub5     3
 ./submit_reduction.sh     data2012_HCP          DoublePhoton_Run2012C-PromptReco-v2         250
-./submit_reduction.sh     data2012_HCP          DoublePhoton_Run2012C-PromptReco-v2_sub2    8
+./submit_reduction.sh     data2012_HCP          DoublePhoton_Run2012C-PromptReco-v2_sub2    20
 ./submit_reduction.sh     data2012_HCP          DoublePhoton_Run2012C-PromptReco-v2_sub4    100
 ./submit_reduction.sh     data2012_HCP          DoublePhoton_Run2012C-PromptReco-v2_sub5    1
-./submit_reduction.sh     data2012_HCP          Photon_Run2012A-13Jul2012-v1                30
-./submit_reduction.sh     data2012_HCP          Photon_Run2012A-13Jul2012-v1_sub2           4
+./submit_reduction.sh     data2012_HCP          DoublePhoton_Run2012C-PromptReco-v2_sub6    10
+./submit_reduction.sh     data2012_HCP          Photon_Run2012A-13Jul2012-v1                50
+./submit_reduction.sh     data2012_HCP          Photon_Run2012A-13Jul2012-v1_sub2           10
 ./submit_reduction.sh     data2012_HCP          Photon_Run2012A-13Jul2012-v1_sub3           1
-./submit_reduction.sh     data2012_HCP          Photon_Run2012A-recover-06Aug2012-v1        5
+./submit_reduction.sh     data2012_HCP          Photon_Run2012A-recover-06Aug2012-v1        10
 ./submit_reduction.sh     data2012_HCP          Photon_Run2012A-recover-06Aug2012-v1__sub1  1
 ./submit_reduction.sh     data2012_HCP          Run2012C-24Aug2012-v2                       30
 
