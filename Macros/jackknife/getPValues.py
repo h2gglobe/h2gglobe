22ce3a731310b6fe55552ddf242952b56728c831
sani <>
2012-11-20 18:36:47 +0000

    awsome script

--- Macros/jackknife/getPValues.py
+++ Macros/jackknife/getPValues.py
@@ -1,17 +1,18 @@
 #! /usr/bin/python
 
-import sys
+import sys, os
+import ROOT
 
 bestPValues = []
 workingDir=sys.argv[1]
 datacardName = sys.argv[2]
 nJobs=sys.argv[3]
+
 for i in xrange(int(nJobs)):
     r = [120+0.5*k for k in range(20)]
     maxMassPValue = (0, 9999.)
     
     for j in r:
-        print workingDir+"part"+str(i)+"/"+datacardName+"/higgsCOmbinePValie.ProfileLikelihhod.mH"+str(j)+".log"
         file = open(workingDir+"/part"+str(i)+"/"+datacardName+"/higgsCombinePValue.ProfileLikelihood.mH"+str(j)+".log")
         lines = file.readlines()
         file.close()
@@ -19,10 +20,21 @@ for i in xrange(int(nJobs)):
         for l in lines:
             if ("p-value" in l):
                 pvalue = (float(l.split()[-1]))
-                print pvalue, maxMassPValue[1]
                 if (pvalue < maxMassPValue[1]):
                     maxMassPValue = (j, pvalue)
     bestPValues.append(maxMassPValue)
+
+# Compute mu
+
+mus = []
+for i in xrange(int(nJobs)):
+    a = os.popen("combine "+workingDir+"/part"+str(i)+"/"+datacardName+".txt -m "+str(bestPValues[i][0])+" -M ChannelCompatibilityCheck --rMin=-20 --saveFitResult -s -1 -n SignalStrength")
+    lines = a.readlines()
+    for l in lines:
+        if ("Nominal" in l):
+            mus.append(float(l.split()[5]))
+    
+print mus
 
 
             
