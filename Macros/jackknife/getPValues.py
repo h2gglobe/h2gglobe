08de61d23083c101fb4da3d7fbc396621907b777
capalmer <>
2012-11-20 22:04:16 +0000

    added calls to PValuesPerCat.py and summary output file

--- Macros/jackknife/getPValues.py
+++ Macros/jackknife/getPValues.py
@@ -3,17 +3,22 @@
 import sys, os
 import ROOT
 
-bestPValues = []
-workingDir=sys.argv[1]
-datacardName = sys.argv[2]
-nJobs=sys.argv[3]
+bestPValues = {}
+mus = {}
+#workingDir=sys.argv[1]
+datacardName = sys.argv[1]
+nJobs=sys.argv[2]
+
 
 for i in xrange(int(nJobs)):
     r = [120+0.5*k for k in range(20)]
     maxMassPValue = (0, 9999.)
-    
+    cmd="python PValuesPerCat.py -s part"+str(i)+" -o -p -m 120,120.5,121,121.5,122,122.5,123,123.5,124,124.5,125,125.5,126,126.5,127,127.5,128,128.5,129,129.5,130 -d part"+str(i)+"/"+datacardName+".txt"
+    print cmd
+    output=os.system(cmd)
+
     for j in r:
-        file = open(workingDir+"/part"+str(i)+"/"+datacardName+"/higgsCombinePValue.ProfileLikelihood.mH"+str(j)+".log")
+        file = open("part"+str(i)+"/"+datacardName+"/higgsCombinePValue.ProfileLikelihood.mH"+str(j)+".log")
         lines = file.readlines()
         file.close()
     
@@ -22,19 +27,43 @@ for i in xrange(int(nJobs)):
                 pvalue = (float(l.split()[-1]))
                 if (pvalue < maxMassPValue[1]):
                     maxMassPValue = (j, pvalue)
-    bestPValues.append(maxMassPValue)
+    bestPValues[i]=maxMassPValue
 
-# Compute mu
-
-mus = []
-for i in xrange(int(nJobs)):
-    a = os.popen("combine "+workingDir+"/part"+str(i)+"/"+datacardName+".txt -m "+str(bestPValues[i][0])+" -M ChannelCompatibilityCheck --rMin=-20 --saveFitResult -s -1 -n SignalStrength")
-    lines = a.readlines()
-    for l in lines:
-        if ("Nominal" in l):
-            mus.append(float(l.split()[5]))
+    # Compute mu
+    bestcmd="python PValuesPerCat.py -s part"+str(i)+" -b -m "+str(maxMassPValue[0])+" -d part"+str(i)+"/"+datacardName+".txt"
+    print bestcmd
+    output=os.system(bestcmd)
     
-print mus
+    thismu={}
+    file = open("part"+str(i)+"/"+datacardName+"_BestFit/higgsCombineTest.ChannelCompatibilityCheck.mH"+str(maxMassPValue[0])+".log")
+    lines = file.readlines()
+    file.close()
+    
+    for line in lines:
+        if "Nominal fit" in line:
+            linelist=line.split()
+            thismu["best"]=[linelist[5],linelist[6].split("/")]
+        elif "Alternate fit" in line:
+            linelist=line.split()
+            thismu[linelist[-1]]=[linelist[4],linelist[5].split("/")]
+    #a = os.popen("combine "+workingDir+"/part"+str(i)+"/"+datacardName+".txt -m "+str(bestPValues[i][0])+" -M ChannelCompatibilityCheck --rMin=-20 --saveFitResult -s -1 -n SignalStrength")
+    #lines = a.readlines()
+    #for l in lines:
+    #    if ("Nominal" in l):
+    #        mus[maxMassPValue]=(float(l.split()[5]))
+    mus[i]=thismu 
+#print mus
+
+mukeys=mus.keys()
+mukeys.sort()
 
 
+file = open(datacardName+"_bestfits.txt","w")
+for mukey in mukeys:
+    muinfo=mus[mukey]
+    
+    file.write(str(mukey)+"\t"+str(muinfo["best"][0])+"\t"+str(bestPValues[mukey][0])+"\t"+str(bestPValues[mukey][1])+"\n")
+    bestPValues[i]=maxMassPValue
+
+file.close()
             
