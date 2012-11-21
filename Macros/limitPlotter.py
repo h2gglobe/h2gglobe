b5a2040a4e7362b22e5019c56c64f02f1e56ad76
drberry <>
2012-11-21 13:28:50 +0000

    Removed Tabs and added append option

--- Macros/limitPlotter.py
+++ Macros/limitPlotter.py
@@ -14,6 +14,7 @@ parser.add_option("-o","--outputLimits",dest="outputLimits")
 parser.add_option("-e","--expectedOnly",action="store_true")
 parser.add_option("-p","--path",dest="path",default="",type="str")
 parser.add_option("-v","--verbose",dest="verbose",action="store_true")
+parser.add_option("-a","--append",dest="append",default="",help="Append string to filename")
 parser.add_option("","--sideband",dest="sideband",default=False,action="store_true")
 parser.add_option("","--addline",action="append",type="str",help="add lines to the plot file.root:color:linestyle:legend entry", default = [])
 parser.add_option("","--show",action="store_true")
@@ -44,9 +45,9 @@ EXPmassesT = numpy.arange(110,150.1,1)
 epsilon = 0.001  # make this smaller than your smallest step size
 
 for m in OBSmassesT:
-    	#if "%.1f"%m=="%d.0"%(m+epsilon):continue	# sigh!
-	OBSmasses.append(m)
-	EXPmasses.append(m)
+        #if "%.1f"%m=="%d.0"%(m+epsilon):continue   # sigh!
+    OBSmasses.append(m)
+    EXPmasses.append(m)
 
 # Plotting Styles --------------------------------------------------------
 OFFSETLOW=0
@@ -57,7 +58,7 @@ SMFILLSTYLE=3244
 FILLCOLOR_95=ROOT.kYellow
 FILLCOLOR_68=ROOT.kGreen
 RANGEYABS=[0.0,0.6]
-RANGEYRAT=[0.0,5]
+RANGEYRAT=[0.0,4]
 RANGEMU = [-4,3.0]
 MINPV = 0.5*10E-5
 MAXPV = 1.0
@@ -71,22 +72,22 @@ if options.addline and not options.pval : sys.exit("Cannot addlines unless runni
 # ------------------------------------------------------------------------
 # SM Signal Normalizer
 if not options.doRatio:
-	ROOT.gROOT.ProcessLine(".L ../libLoopAll.so")
-	signalNormalizer = ROOT.Normalization_8TeV()
+    ROOT.gROOT.ProcessLine(".L ../libLoopAll.so")
+    signalNormalizer = ROOT.Normalization_8TeV()
 extraString = "SM"
 # ------------------------------------------------------------------------
 if options.pval:
-	 EXPmasses=[]
-	 options.doRatio=True
+     EXPmasses=[]
+     options.doRatio=True
 
 if options.Method=="MaxLikelihoodFit": 
-	options.doRatio = True
+    options.doRatio = True
 
 if not options.doRatio and options.Method != "Frequentist": 
-	ROOT.gROOT.ProcessLine(".L medianCalc.C+g")
-	from ROOT import medianCalc
-	from ROOT import FrequentistLimits
-	
+    ROOT.gROOT.ProcessLine(".L medianCalc.C+g")
+    from ROOT import medianCalc
+    from ROOT import FrequentistLimits
+    
 
 if options.bayes:
   BayesianFile =ROOT.TFile(options.bayes) 
@@ -111,8 +112,8 @@ if Method == "HybridNew" or Method == "Asymptotic" or Method == "AsymptoticNew":
 # Done with setip
 # --------------------------------------------------------------------------
 ROOT.gROOT.ProcessLine( \
-   "struct Entry{	\
-    double r;		\
+   "struct Entry{   \
+    double r;       \
    };"
 )
 from ROOT import Entry
@@ -131,7 +132,7 @@ if Method=="HybridNew":
   EXPfiles=[]
   EXPmasses = OBSmasses[:]
   for m in EXPmasses:
-    if "%.1f"%m=="%d.0"%(m+epsilon):	# sigh!
+    if "%.1f"%m=="%d.0"%(m+epsilon):    # sigh!
       EXPfiles.append(ROOT.TFile(EXPName+".mH%d.quant0.500.root"%m))
     else:
       EXPfiles.append(ROOT.TFile(EXPName+".mH%.1f.quant0.500.root"%m))
@@ -141,7 +142,7 @@ elif Method=="Asymptotic" or Method=="AsymptoticNew" or Method=="MaxLikelihoodFi
   EXPfiles=[]
   EXPmasses = OBSmasses[:]
   for m in EXPmasses:
-    if "%.1f"%m=="%d.0"%(m+epsilon) and not options.sideband:	# sigh!
+    if "%.1f"%m=="%d.0"%(m+epsilon) and not options.sideband:   # sigh!
       EXPfiles.append(ROOT.TFile(EXPName+".mH%d.root"%(m+epsilon)))
     else:
       EXPfiles.append(ROOT.TFile(EXPName+".mH%.1f.root"%m))
@@ -150,7 +151,7 @@ elif Method=="Asymptotic" or Method=="AsymptoticNew" or Method=="MaxLikelihoodFi
 else:
   EXPfiles=[]
   for m in EXPmasses:
-    if "%.1f"%m=="%d.0"%(m+epsilon) and not options.sideband:	# sigh!
+    if "%.1f"%m=="%d.0"%(m+epsilon) and not options.sideband:   # sigh!
       EXPfiles.append(ROOT.TFile(EXPName+".mH%d.root"%(m+epsilon)))
     else:
       EXPfiles.append(ROOT.TFile(EXPName+".mH%.1f.root"%m))
@@ -160,7 +161,7 @@ else:
 OBSfiles = []
 if not options.expectedOnly:
   for m in OBSmasses:
-    if "%.1f"%m=="%d.0"%(m+epsilon) and not options.sideband:	# sigh!
+    if "%.1f"%m=="%d.0"%(m+epsilon) and not options.sideband:   # sigh!
       OBSfiles.append(ROOT.TFile(OBSName+".mH%d.root"%(m+epsilon)))
     else:
       OBSfiles.append(ROOT.TFile(OBSName+".mH%.1f.root"%m))
@@ -200,225 +201,220 @@ graph95.SetLineWidth(2)
 MG = ROOT.TMultiGraph()
 
 def MakeMlfPlot(MG):
-	legend=ROOT.TLegend(0.55,0.15,0.89,0.3)
-	legend.SetFillColor(10)
-	legend.SetTextFont(42)
-	legend.SetTextSize(FONTSIZE)
-	graph68.SetLineStyle(1)
-	legend.AddEntry(graph68,"#pm 1#sigma Uncertainty","F")
+    legend=ROOT.TLegend(0.55,0.15,0.89,0.3)
+    legend.SetFillColor(10)
+    legend.SetTextFont(42)
+    legend.SetTextSize(FONTSIZE)
+    graph68.SetLineStyle(1)
+    legend.AddEntry(graph68,"#pm 1#sigma Uncertainty","F")
 
-	if options.square : c = ROOT.TCanvas("c","c",600,600)
-	else :c = ROOT.TCanvas("c","c",800,600)
+    if options.square : c = ROOT.TCanvas("c","c",600,600)
+    else :c = ROOT.TCanvas("c","c",800,600)
 
-	dhist = ROOT.TH1F("dh","dh",100,MINMH,MAXMH)
-	dhist.GetYaxis().SetTitleOffset(1.2)
-	dhist.GetXaxis().SetTitleOffset(1.2)
-	dhist.GetYaxis().SetTitleSize(0.04)
-	dhist.GetXaxis().SetTitleSize(0.04)
-	dhist.GetYaxis().SetLabelSize(0.04)
-	dhist.GetXaxis().SetLabelSize(0.04)
-	dhist.GetXaxis().SetRangeUser(MINMH,MAXMH)
-	dhist.GetYaxis().SetRangeUser(RANGEMU[0],RANGEMU[1])
-	dhist.GetXaxis().SetTitle("m_{H} (GeV)")
-	dhist.GetYaxis().SetTitle("Best fit #sigma/#sigma_{SM}")
-	dhist.Draw("AXIS")
+    dhist = ROOT.TH1F("dh","dh",100,MINMH,MAXMH)
+    dhist.GetYaxis().SetTitleOffset(1.2)
+    dhist.GetXaxis().SetTitleOffset(1.2)
+    dhist.GetYaxis().SetTitleSize(0.04)
+    dhist.GetXaxis().SetTitleSize(0.04)
+    dhist.GetYaxis().SetLabelSize(0.04)
+    dhist.GetXaxis().SetLabelSize(0.04)
+    dhist.GetXaxis().SetRangeUser(MINMH,MAXMH)
+    dhist.GetYaxis().SetRangeUser(RANGEMU[0],RANGEMU[1])
+    dhist.GetXaxis().SetTitle("m_{H} (GeV)")
+    dhist.GetYaxis().SetTitle("Best fit #sigma/#sigma_{SM}")
+    dhist.Draw("AXIS")
 
-	MG.Draw("L3")
+    MG.Draw("L3")
 
-	# ------------------------------------------------------------------------
-	# Additional Lines stored in --addline -----------------------------------
-	for lineF in options.addline:
+    # ------------------------------------------------------------------------
+    # Additional Lines stored in --addline -----------------------------------
+    for lineF in options.addline:
 
-		# Parse the string, should be file.root:color:linestyle:legend entry	
-		vals = lineF.split(":")
-		ftmp = ROOT.TFile(vals[0])
-		grext = ftmp.Get("observed")
-		grext.SetLineColor(int(vals[1]))
-		grext.SetLineStyle(int(vals[2]))
-		grext.SetLineWidth(2)
-		legend.AddEntry(grext,vals[3],"L")
-		grext.Draw("same")
-	# ------------------------------------------------------------------------
-		
+        # Parse the string, should be file.root:color:linestyle:legend entry    
+        vals = lineF.split(":")
+        ftmp = ROOT.TFile(vals[0])
+        grext = ftmp.Get("observed")
+        grext.SetLineColor(int(vals[1]))
+        grext.SetLineStyle(int(vals[2]))
+        grext.SetLineWidth(2)
+        legend.AddEntry(grext,vals[3],"L")
+        grext.Draw("same")
+    # ------------------------------------------------------------------------
+        
 
-	c.Update()
-	text = ROOT.TLatex()
-	text.SetTextColor(ROOT.kRed)
-	text.SetTextSize(FONTSIZE)
-	text.SetTextFont(42)
+    c.Update()
+    text = ROOT.TLatex()
+    text.SetTextColor(ROOT.kRed)
+    text.SetTextSize(FONTSIZE)
+    text.SetTextFont(42)
 
-	graphOne.Draw("L")
-	c.SetGrid(not options.nogrid)
-	if not options.nogrid: dhist.Draw("AXIGSAME")
+    graphOne.Draw("L")
+    c.SetGrid(not options.nogrid)
+    if not options.nogrid: dhist.Draw("AXIGSAME")
 
-	mytext= ROOT.TLatex()
-	mytext.SetTextSize(FONTSIZE)
-	mytext.SetTextFont(42)
-	mytext.SetNDC()
+    mytext= ROOT.TLatex()
+    mytext.SetTextSize(FONTSIZE)
+    mytext.SetTextFont(42)
+    mytext.SetNDC()
 
-	mytext.DrawLatex(0.18,0.24,"CMS Preliminary")
-	for t,lineT in enumerate(options.addtxt):
-		mytext.DrawLatex(0.18,0.23-(t+1)*0.04,"%s"%(lineT))
-	legend.Draw()
-	ROOT.gPad.RedrawAxis();
-	
-	if options.show:raw_input("Looks Ok?")
-	c.SaveAs("maxlhplot.pdf")
-	c.SaveAs("maxlhplot.png")
+    mytext.DrawLatex(0.18,0.24,"CMS Preliminary")
+    for t,lineT in enumerate(options.addtxt):
+        mytext.DrawLatex(0.18,0.23-(t+1)*0.04,"%s"%(lineT))
+    legend.Draw()
+    ROOT.gPad.RedrawAxis();
+    
+    if options.show:raw_input("Looks Ok?")
+    c.SaveAs("maxlhplot.pdf")
+    c.SaveAs("maxlhplot.png")
 
 #-------------------------------------------------------------------------
 def MakePvalPlot(MG):
 
-	legend=ROOT.TLegend(0.55,0.15,0.89,0.3)
-	legend.SetFillColor(10)
-	legend.SetTextFont(42)
-	legend.SetTextSize(FONTSIZE)
-	legend.AddEntry(graphObs,"Observed","L")
+    legend=ROOT.TLegend(0.55,0.15,0.89,0.3)
+    legend.SetFillColor(10)
+    legend.SetTextFont(42)
+    legend.SetTextSize(FONTSIZE)
+    legend.AddEntry(graphObs,"Observed","L")
 
-	if options.square : c = ROOT.TCanvas("c","c",600,600)
-	else :c = ROOT.TCanvas("c","c",800,600)
+    if options.square : c = ROOT.TCanvas("c","c",600,600)
+    else :c = ROOT.TCanvas("c","c",800,600)
 
-	dhist = ROOT.TH1F("dh","dh",100,MINMH,MAXMH)
-	dhist.GetYaxis().SetTitleOffset(1.5)
-	dhist.GetXaxis().SetTitleOffset(1.2)
-	dhist.GetYaxis().SetTitleSize(0.04)
-	dhist.GetXaxis().SetTitleSize(0.04)
-	dhist.GetYaxis().SetLabelSize(0.04)
-	dhist.GetXaxis().SetLabelSize(0.04)
-	dhist.GetXaxis().SetRangeUser(MINMH,MAXMH)
-	dhist.GetYaxis().SetRangeUser(MINPV,MAXPV)
-	dhist.GetXaxis().SetTitle("m_{H} (GeV)")
-	dhist.GetYaxis().SetTitle("Local p-value")
-	dhist.Draw("AXIS")
+    dhist = ROOT.TH1F("dh","dh",100,MINMH,MAXMH)
+    dhist.GetYaxis().SetTitleOffset(1.5)
+    dhist.GetXaxis().SetTitleOffset(1.2)
+    dhist.GetYaxis().SetTitleSize(0.04)
+    dhist.GetXaxis().SetTitleSize(0.04)
+    dhist.GetYaxis().SetLabelSize(0.04)
+    dhist.GetXaxis().SetLabelSize(0.04)
+    dhist.GetXaxis().SetRangeUser(MINMH,MAXMH)
+    dhist.GetYaxis().SetRangeUser(MINPV,MAXPV)
+    dhist.GetXaxis().SetTitle("m_{H} (GeV)")
+    dhist.GetYaxis().SetTitle("Local p-value")
+    dhist.Draw("AXIS")
 
-	MG.Draw("L3")
+    MG.Draw("L3")
 
-	# ------------------------------------------------------------------------
-	# Additional Lines stored in --addline -----------------------------------
-	for lineF in options.addline:
+    # ------------------------------------------------------------------------
+    # Additional Lines stored in --addline -----------------------------------
+    for lineF in options.addline:
 
-		# Parse the string, should be file.root:color:linestyle:legend entry	
-		vals = lineF.split(":")
-		ftmp = ROOT.TFile(vals[0])
-		grext = ftmp.Get("observed")
-		grext.SetLineColor(int(vals[1]))
-		grext.SetLineStyle(int(vals[2]))
-		grext.SetLineWidth(2)
-		legend.AddEntry(grext,vals[3],"L")
-		grext.Draw("same")
-	# ------------------------------------------------------------------------
-		
+        # Parse the string, should be file.root:color:linestyle:legend entry    
+        vals = lineF.split(":")
+        ftmp = ROOT.TFile(vals[0])
+        grext = ftmp.Get("observed")
+        grext.SetLineColor(int(vals[1]))
+        grext.SetLineStyle(int(vals[2]))
+        grext.SetLineWidth(2)
+        legend.AddEntry(grext,vals[3],"L")
+        grext.Draw("same")
+    # ------------------------------------------------------------------------
+        
 
-	c.Update()
-	text = ROOT.TLatex()
-	text.SetTextColor(ROOT.kRed)
-	text.SetTextSize(FONTSIZE)
-	text.SetTextFont(42)
-	
-		
-	Vals=[ROOT.RooStats.SignificanceToPValue(L) for L in Lines]
-	TLines=[ROOT.TLine(MINMH,V,MAXMH,V) for V in Vals]
+    c.Update()
+    text = ROOT.TLatex()
+    text.SetTextColor(ROOT.kRed)
+    text.SetTextSize(FONTSIZE)
+    text.SetTextFont(42)
+    
+        
+    Vals=[ROOT.RooStats.SignificanceToPValue(L) for L in Lines]
+    TLines=[ROOT.TLine(MINMH,V,MAXMH,V) for V in Vals]
 
-	for j,TL in enumerate(TLines):
-		TL.SetLineStyle(1)
-		TL.SetLineColor(2)
-		TL.SetLineWidth(1)
-		TL.Draw("same")
-		text.DrawLatex(MAXMH+0.2,Vals[j]*0.88,"%d #sigma"%Lines[j])
+    for j,TL in enumerate(TLines):
+        TL.SetLineStyle(1)
+        TL.SetLineColor(2)
+        TL.SetLineWidth(1)
+        TL.Draw("same")
+        text.DrawLatex(MAXMH+0.2,Vals[j]*0.88,"%d #sigma"%Lines[j])
 
-	c.SetGrid(not options.nogrid)
-	c.SetLogy();
-	if not options.nogrid: dhist.Draw("AXIGSAME")
+    c.SetGrid(not options.nogrid)
+    c.SetLogy();
+    if not options.nogrid: dhist.Draw("AXIGSAME")
 
-	mytext= ROOT.TLatex()
-	mytext.SetTextSize(FONTSIZE)
-	mytext.SetTextFont(42)
-	mytext.SetNDC()
+    mytext= ROOT.TLatex()
+    mytext.SetTextSize(FONTSIZE)
+    mytext.SetTextFont(42)
+    mytext.SetNDC()
 
-	mytext.DrawLatex(0.18,0.24,"CMS Preliminary")
-	for t,lineT in enumerate(options.addtxt):
-		mytext.DrawLatex(0.18,0.23-(t+1)*0.04,"%s"%(lineT))
-	legend.Draw()
-	ROOT.gPad.RedrawAxis();
-	
-	if options.show:raw_input("Looks Ok?")
-	c.SaveAs("pvaluesplot.pdf")
-	c.SaveAs("pvaluesplot.png")
+    mytext.DrawLatex(0.18,0.24,"CMS Preliminary")
+    for t,lineT in enumerate(options.addtxt):
+        mytext.DrawLatex(0.18,0.23-(t+1)*0.04,"%s"%(lineT))
+    legend.Draw()
+    ROOT.gPad.RedrawAxis();
+    
+    if options.show:raw_input("Looks Ok?")
+    c.SaveAs("pvaluesplot.pdf")
+    c.SaveAs("pvaluesplot.png")
 #-------------------------------------------------------------------------
 
 def MakeLimitPlot(MG):
 
-	leg=ROOT.TLegend(0.53,0.66,0.89,0.90)
-	leg.SetFillColor(10)
+    leg=ROOT.TLegend(0.53,0.66,0.89,0.90)
+    leg.SetFillColor(10)
 
-	# Different entries for the different methods
-	LegendEntry = ""
-	if Method == "ProfileLikelihood": LegendEntry = "PL"
-	if Method == "Bayesian": LegendEntry = "Bayesian"
-	if Method == "HybridNew": LegendEntry = "CLs"
-	if Method == "Asymptotic": LegendEntry = "CLs (Asymptotic)"
-	if Method == "AsymptoticNew": LegendEntry = "CLs (Asymptotic)"
+    # Different entries for the different methods
+    LegendEntry = ""
+    if Method == "ProfileLikelihood": LegendEntry = "PL"
+    if Method == "Bayesian": LegendEntry = "Bayesian"
+    if Method == "HybridNew": LegendEntry = "CLs"
+    if Method == "Asymptotic": LegendEntry = "CLs (Asymptotic)"
+    if Method == "AsymptoticNew": LegendEntry = "CLs (Asymptotic)"
 
-	if not options.expectedOnly: leg.AddEntry(graphObs,"Observed","L")
-	if options.bayes and not options.expectedOnly: leg.AddEntry(bayesObs,"Observed Bayesian Limit","L")
-	leg.AddEntry(graph68,"Expected #pm 1#sigma","FL")
-	leg.AddEntry(graph95,"Expected #pm 2#sigma","FL")
+    if not options.expectedOnly: leg.AddEntry(graphObs,"Observed","L")
+    if options.bayes and not options.expectedOnly: leg.AddEntry(bayesObs,"Observed Bayesian Limit","L")
+    leg.AddEntry(graph68,"Expected #pm 1#sigma","FL")
+    leg.AddEntry(graph95,"Expected #pm 2#sigma","FL")
 
-	leg.SetTextFont(42)
-	leg.SetTextSize(FONTSIZE)
+    leg.SetTextFont(42)
+    leg.SetTextSize(FONTSIZE)
 
-	if options.square : C = ROOT.TCanvas("c","c",600,600)
-	else: C = ROOT.TCanvas("c","c",700,600)
+    if options.square : C = ROOT.TCanvas("c","c",600,600)
+    else: C = ROOT.TCanvas("c","c",700,600)
 
-	C.SetGrid(not options.nogrid)
-	dummyHist = ROOT.TH1D("dummy","",1,min(OBSmasses)-OFFSETLOW,max(OBSmasses)+OFFSETHIGH)
-	dummyHist.SetTitleSize(0.04,"XY")
-	dummyHist.Draw("AXIS")
-	MG.Draw("L3")
-	if not options.nogrid: dummyHist.Draw("AXIGSAME")
+    C.SetGrid(not options.nogrid)
+    dummyHist = ROOT.TH1D("dummy","",1,min(OBSmasses)-OFFSETLOW,max(OBSmasses)+OFFSETHIGH)
+    dummyHist.SetTitleSize(0.04,"XY")
+    dummyHist.Draw("AXIS")
+    MG.Draw("L3")
+    if not options.nogrid: dummyHist.Draw("AXIGSAME")
 
-	dummyHist.GetXaxis().SetTitle("m_{H} (GeV)")
-	dummyHist.GetXaxis().SetRangeUser(min(OBSmasses)-OFFSETLOW,max(OBSmasses)+OFFSETHIGH)
-	if options.doRatio:
-	 dummyHist.GetYaxis().SetRangeUser(RANGEYRAT[0],RANGEYRAT[1])
-	 dummyHist.GetYaxis().SetNdivisions(5,int("%d"%(RANGEYRAT[1]-RANGEYRAT[0])),0)
-	 dummyHist.GetYaxis().SetTitle("\sigma(H#rightarrow #gamma #gamma)_{95%%CL} / \sigma(H#rightarrow #gamma #gamma)_{%s}"%extraString)
-	else: 
-	 dummyHist.GetYaxis().SetRangeUser(RANGEYABS[0],RANGEYABS[1])
-	 dummyHist.GetYaxis().SetNdivisions(5,int("%d"%(RANGEYABS[1]-RANGEYABS[0])),0)
-	 dummyHist.GetYaxis().SetTitle("\sigma #times BR(H#rightarrow #gamma #gamma)_{95%CL} (pb)")
+    dummyHist.GetXaxis().SetTitle("m_{H} (GeV)")
+    dummyHist.GetXaxis().SetRangeUser(min(OBSmasses)-OFFSETLOW,max(OBSmasses)+OFFSETHIGH)
+    if options.doRatio:
+     dummyHist.GetYaxis().SetRangeUser(RANGEYRAT[0],RANGEYRAT[1])
+     dummyHist.GetYaxis().SetNdivisions(5,int("%d"%(RANGEYRAT[1]-RANGEYRAT[0])),0)
+     dummyHist.GetYaxis().SetTitle("\sigma(H#rightarrow #gamma #gamma)_{95%%CL} / \sigma(H#rightarrow #gamma #gamma)_{%s}"%extraString)
+    else: 
+     dummyHist.GetYaxis().SetRangeUser(RANGEYABS[0],RANGEYABS[1])
+     dummyHist.GetYaxis().SetNdivisions(5,int("%d"%(RANGEYABS[1]-RANGEYABS[0])),0)
+     dummyHist.GetYaxis().SetTitle("\sigma #times BR(H#rightarrow #gamma #gamma)_{95%CL} (pb)")
 
-	dummyHist.GetYaxis().SetTitleOffset(1.3)
-	dummyHist.GetXaxis().SetTitleOffset(1.25)
+    dummyHist.GetYaxis().SetTitleOffset(1.3)
+    dummyHist.GetXaxis().SetTitleOffset(1.25)
 
-	MG.SetTitle("")
-	mytext = ROOT.TLatex()
-	mytext.SetTextSize(FONTSIZE)
+    MG.SetTitle("")
+    mytext = ROOT.TLatex()
+    mytext.SetTextSize(FONTSIZE)
 
-	mytext.SetNDC()
-	mytext.SetTextFont(42)
-	mytext.SetTextSize(FONTSIZE)
-	mytext.DrawLatex(0.16,0.85,"CMS Preliminary")
-	for t,lineT in enumerate(options.addtxt):
-		mytext.DrawLatex(0.16,0.84-(t+1)*(0.04),"%s"%lineT)
+    mytext.SetNDC()
+    mytext.SetTextFont(42)
+    mytext.SetTextSize(FONTSIZE)
+    mytext.DrawLatex(0.16,0.85,"CMS Preliminary")
+    for t,lineT in enumerate(options.addtxt):
+        mytext.DrawLatex(0.16,0.84-(t+1)*(0.04),"%s"%lineT)
   
-	leg.Draw()
-	ROOT.gPad.RedrawAxis();
-	if options.show:raw_input("Looks Ok?")
+    leg.Draw()
+    ROOT.gPad.RedrawAxis();
+    if options.show:raw_input("Looks Ok?")
 
-	#Make a bunch of extensions to the plots
-	if options.doRatio:
-	 C.SaveAs("limit_%s_%s_ratio.pdf"%(options.doSmooth and "smooth" or "",Method))
-	 C.SaveAs("limit_%s_%s_ratio.png"%(options.doSmooth and "smooth" or "",Method))
-	 C.SaveAs("limit_%s_%s_ratio.gif"%(options.doSmooth and "smooth" or "",Method))
-	 C.SaveAs("limit_%s_%s_ratio.eps"%(options.doSmooth and "smooth" or "",Method))
-	 C.SaveAs("limit_%s_%s_ratio.ps"%(options.doSmooth and "smooth" or "",Method))
-	else:
-	 C.SaveAs("limit_%s_%s.pdf"%(options.doSmooth and "smooth" or "",Method))
-	 C.SaveAs("limit_%s_%s.png"%(options.doSmooth and "smooth" or "",Method))
-	 C.SaveAs("limit_%s_%s.gif"%(options.doSmooth and "smooth" or "",Method))
-	 C.SaveAs("limit_%s_%s.eps"%(options.doSmooth and "smooth" or "",Method))
-	 C.SaveAs("limit_%s_%s.ps"%(options.doSmooth and "smooth" or "",Method))
+    #Make a bunch of extensions to the plots
+    outputname="limit"
+    if options.doSmooth: outputname+="_smooth"
+    outputname+="_"+Method
+    if options.doRatio: outputname+="_ratio"
+    if options.append!="": outputname+="_"+options.append
+    types=[".pdf",".png",".gif",".eps",".ps"]
+    for type in types: C.SaveAs(outputname+type)
 
 #-------------------------------------------------------------------------
 
