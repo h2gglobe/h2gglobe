5b411c5c1f8f03673a1f2426700483e069ee650d
sani <>
2012-11-21 10:51:57 +0000

    update

--- Macros/jackknife/getPValues.py
+++ Macros/jackknife/getPValues.py
@@ -5,20 +5,21 @@ import ROOT
 
 bestPValues = {}
 mus = {}
-#workingDir=sys.argv[1]
-datacardName = sys.argv[1]
-nJobs=sys.argv[2]
+workingDir=sys.argv[1]
+datacardName = sys.argv[2]
+nJobs=int(sys.argv[3])
+offset=int(sys.argv[4])
 
 
-for i in xrange(int(nJobs)):
+for i in xrange(offset, offset+nJobs):
     r = [120+0.5*k for k in range(20)]
     maxMassPValue = (0, 9999.)
-    cmd="python PValuesPerCat.py -s part"+str(i)+" -o -p -m 120,120.5,121,121.5,122,122.5,123,123.5,124,124.5,125,125.5,126,126.5,127,127.5,128,128.5,129,129.5,130 -d part"+str(i)+"/"+datacardName+".txt"
+    cmd="python ../ResultScripts/PValuesPerCat.py -s "+workingDir+"/part"+str(i)+" -o -p -m 120,120.5,121,121.5,122,122.5,123,123.5,124,124.5,125,125.5,126,126.5,127,127.5,128,128.5,129,129.5,130 -d "+workingDir+"/part"+str(i)+"/"+datacardName+".txt"
     print cmd
     output=os.system(cmd)
 
     for j in r:
-        file = open("part"+str(i)+"/"+datacardName+"/higgsCombinePValue.ProfileLikelihood.mH"+str(j)+".log")
+        file = open(workingDir+"/part"+str(i)+"/"+datacardName+"/higgsCombinePValue.ProfileLikelihood.mH"+str(j)+".log")
         lines = file.readlines()
         file.close()
     
@@ -30,12 +31,12 @@ for i in xrange(int(nJobs)):
     bestPValues[i]=maxMassPValue
 
     # Compute mu
-    bestcmd="python PValuesPerCat.py -s part"+str(i)+" -b -m "+str(maxMassPValue[0])+" -d part"+str(i)+"/"+datacardName+".txt"
+    bestcmd="python ../ResultScripts/PValuesPerCat.py -s "+workingDir+"/part"+str(i)+" -b -m "+str(maxMassPValue[0])+" -d "+workingDir+"/part"+str(i)+"/"+datacardName+".txt"
     print bestcmd
     output=os.system(bestcmd)
     
     thismu={}
-    file = open("part"+str(i)+"/"+datacardName+"_BestFit/higgsCombineTest.ChannelCompatibilityCheck.mH"+str(maxMassPValue[0])+".log")
+    file = open(workingDir+"/part"+str(i)+"/"+datacardName+"_BestFit/higgsCombineTest.ChannelCompatibilityCheck.mH"+str(maxMassPValue[0])+".log")
     lines = file.readlines()
     file.close()
     
@@ -58,7 +59,7 @@ mukeys=mus.keys()
 mukeys.sort()
 
 
-file = open(datacardName+"_bestfits.txt","w")
+file = open(workingDir+"/"+datacardName+"_bestfits.txt","a")
 for mukey in mukeys:
     muinfo=mus[mukey]
     
