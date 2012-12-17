32da98f40628f0362924c25e00b1ff5140d98fce
capalmer <>
2012-12-17 13:08:56 +0000

    adding option for ucsd

--- Macros/pileup/pileupMerger.py
+++ Macros/pileup/pileupMerger.py
@@ -12,7 +12,8 @@ import os
 from subprocess import check_call as call
 
 parser = OptionParser(usage="usage: %prog [options] EOS_source_directory\nrun with --help to get list of options")
-parser.add_option("--putBack", action="store_true", default=False, help="Put back merged file in source directory [default: %default].")
+parser.add_option("--putBack",    action="store_true", default=False, help="Put back merged file in source directory [default: %default].")
+parser.add_option("--putHadoop",  action="store_true", default=False, help="Put back merged file in source directory [default: %default].")
 
 (options, args) = parser.parse_args()
 
@@ -36,11 +37,20 @@ except:
 if not "/afs/cern.ch/project/eos/installation/pro/lib64/" in ld_path:
     os.putenv("LD_LIBRARY_PATH", "%s:%s" % ( ld_path, "/afs/cern.ch/project/eos/installation/pro/lib64/" ) )
     
-call( """%(eosLs)s%(inDir)s | awk '/root$/ { print \"root://eoscms//eos/cms%(inDir)s/\"$1  }' | sed 's/\?.*$//' > %(inDirName)s.files.txt"""
+if options.putHadoop:
+    call( """ls %(inDir)s | awk '/root$/ { print \"%(inDir)s/\"$1  }' | sed 's/\?.*$//' > %(inDirName)s.files.txt"""
+      % vars(options), shell=True)
+else: 
+    call( """%(eosLs)s%(inDir)s | awk '/root$/ { print \"root://eoscms//eos/cms%(inDir)s/\"$1  }' | sed 's/\?.*$//' > %(inDirName)s.files.txt"""
       % vars(options), shell=True)
 call( """rm -f %(inDirName)s.pileup.root %(inDirName)s.pileup.root.log""" % vars(options), shell=True)
 call( """hadd -k -T %(inDirName)s.pileup.root `cat %(inDirName)s.files.txt | paste -s` &> %(inDirName)s.pileup.root.log""" % vars(options), shell=True)
 
+if options.putHadoop:
+    cmd = """cp %(inDirName)s.pileup.root %(inDirBase)s/%(inDirName)s.pileup.root"""
+    print "Copying back root"
+    call(cmd % vars(options), shell=True)
+    
 if options.putBack:
     cmd = """cmsStage -f %(inDirName)s.pileup.root %(inDirBase)s/%(inDirName)s.pileup.root"""
     print "Copying back root"
