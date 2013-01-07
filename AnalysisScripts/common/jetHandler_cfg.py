8caeaf072db52eb0ccce7357bcc699ea75d4fbf1
capalmer <>
2013-01-07 13:02:53 +0000

    JEC for the full 2012 dataset

--- AnalysisScripts/common/jetHandler_cfg.py
+++ AnalysisScripts/common/jetHandler_cfg.py
@@ -5,16 +5,16 @@ from CMGTools.External.pujetidproducer_cfi import simple, full, cutbased
 
 ### ## JEC
 jec = cms.PSet(
-    data = cms.vstring("aux/jec2012/Summer12_V7_DATA_L1FastJet_AK5PF.txt",
-                       "aux/jec2012/Summer12_V7_DATA_L2Relative_AK5PF.txt",
-                       "aux/jec2012/Summer12_V7_DATA_L3Absolute_AK5PF.txt",
-                       "aux/jec2012/Summer12_V7_DATA_L2L3Residual_AK5PF.txt",
+    data = cms.vstring("aux/jec2012/GR_P_V42_AN3__All_L1FastJet_AK5PF.txt",
+                       "aux/jec2012/GR_P_V42_AN3__All_L2Relative_AK5PF.txt",
+                       "aux/jec2012/GR_P_V42_AN3__All_L3Absolute_AK5PF.txt",
+                       "aux/jec2012/GR_P_V42_AN3__All_L2L3Residual_AK5PF.txt",
                        ),
-    mc   = cms.vstring("aux/jec2012/Summer12_V7_MC_L1FastJet_AK5PF.txt",
-                       "aux/jec2012/Summer12_V7_MC_L2Relative_AK5PF.txt",
-                       "aux/jec2012/Summer12_V7_MC_L3Absolute_AK5PF.txt",
+    mc   = cms.vstring("aux/jec2012/START53_V15__All_L1FastJet_AK5PF.txt",
+                       "aux/jec2012/START53_V15__All_L2Relative_AK5PF.txt",
+                       "aux/jec2012/START53_V15__All_L3Absolute_AK5PF.txt",
                        ),
-    unc = cms.string("aux/jec2012/Summer12_V7_MC_Uncertainty_AK5PF.txt")
+    unc = cms.string("aux/jec2012/START53_V15__All_Uncertainty_AK5PF.txt")
     )
 
 jer = cms.PSet(
