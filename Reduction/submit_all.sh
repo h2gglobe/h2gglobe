a42ca7e6eedd4d91b28cca73167e6a743f174bed
amarini <>
2013-01-11 14:40:37 +0000

    This commit was manufactured by cvs2svn to create tag 'moriond2013_reduction_v1'.
    Sprout from master 2013-01-10 12:39:40 UTC capalmer '*** empty log message ***'
    Cherrypick from master 2013-01-11 14:40:36 UTC capalmer 'fix diphoton jets typo':
        Reduction/data_2012.txt
        Reduction/submit_all.sh

--- Reduction/submit_all.sh
+++ Reduction/submit_all.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
+./submit_reduction.sh    data_2012             Photon_Run2012A-13Jul2012-v1                    10
+./submit_reduction.sh    data_2012             Photon_Run2012A-recover-06Aug2012-v1            1
+./submit_reduction.sh    data_2012             DoublePhoton_Run2012B-13Jul2012-v1              50
+./submit_reduction.sh    data_2012             DoublePhoton_Run2012C-24Aug2012-v2              10
+./submit_reduction.sh    data_2012             DoublePhoton_Run2012C-EcalRecover_11Dec2012-v1  4
+./submit_reduction.sh    data_2012             DoublePhoton_Run2012C-PromptReco-v2             60
+./submit_reduction.sh    data_2012             DoublePhoton_Run2012C-PromptReco-v2_sub2        10
+./submit_reduction.sh    data_2012             DoublePhoton_Run2012D-PromptReco-v1             60
+./submit_reduction.sh    data_2012             DoublePhoton_Run2012D-PromptReco-v1_sub2        10
+./submit_reduction.sh    data_2012             run2012D_DE                                     80
+
+./submit_reduction.sh     mc_bkg_summer12_s10_2 DiPhotonJets_8TeV-madgraph-tarball-v2_Summer12_DR53X-PU_S10_START53_V7A-v1              40
+./submit_reduction.sh     mc_dy_summer12_s10    DYJetsToLL_M-50_TuneZ2Star_8TeV-madgraph-tarball_Summer12_DR53X-PU_S10_START53_V7A-v1   50
+
 ./submit_reduction.sh     mc_bkg_summer12_s10   DiPhotonBox_Pt-10To25_8TeV-pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1     5
 ./submit_reduction.sh     mc_bkg_summer12_s10   DiPhotonBox_Pt-250ToInf_8TeV-pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1   10
 ./submit_reduction.sh     mc_bkg_summer12_s10   DiPhotonBox_Pt-25To250_8TeV-pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1    10
-./submit_reduction.sh     mc_bkg_summer12_s10   DiPhotonJets_8TeV-madgraph_Summer12_DR53X-PU_S10_START53_V7A-v1             15
 ./submit_reduction.sh     mc_bkg_summer12_s10   GJet_Pt-20to40_doubleEMEnriched_TuneZ2star_8TeV-pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_pp  20
 ./submit_reduction.sh     mc_bkg_summer12_s10   GJet_Pt-20to40_doubleEMEnriched_TuneZ2star_8TeV-pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_pf  30
 ./submit_reduction.sh     mc_bkg_summer12_s10   GJet_Pt-20to40_doubleEMEnriched_TuneZ2star_8TeV-pythia6_Summer12_DR53X-PU_S10_START53_V7A-v1_ff  20
@@ -35,6 +48,10 @@
 
 ./submit_reduction.sh     mc_bkg_summer12_s10   TTJets_TuneZ2star_8TeV-madgraph-tauola_Summer12-PU_S7_START52_V9-v1    20
 ./submit_reduction.sh     mc_bkg_summer12_s10   TTJets_TuneZ2star_8TeV-madgraph-tauola_Summer12-PU_S8_START52_V9-v1    20
+
+
+./submit_reduction.sh     mc_sig_summer12_s10   \*     5
+
 
 
 wait
