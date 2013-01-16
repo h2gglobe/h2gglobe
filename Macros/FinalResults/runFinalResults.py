0e17b4c47edd15c76326177533fc18580260b4a0
mkenzie <>
2013-01-16 22:55:28 +0000

    Some modifications

--- Macros/FinalResults/runFinalResults.py
+++ Macros/FinalResults/runFinalResults.py
@@ -9,6 +9,8 @@
 # should be in a cmssw environment with HiggsAnalysis/CombinedLimit compiled
 
 import os
+import sys
+import numpy
 
 from optparse import OptionParser
 parser = OptionParser()
@@ -81,21 +83,62 @@ if not options.makePlots:
 
 # make plots
 else:
+  # check jobs are complete
+  job_dirs = ['Asymptotic','ExpProfileLikelihood']
+  if options.parametric: job_dirs.append('ExpProfileLikelihood_m125')
+  if options.unblind: 
+    job_dirs.append('ProfileLikelihood')
+    job_dirs.append('MaxLikelihoodFit')
+
+  failed_jobs=[]
+  for card, suffix, line in config:
+    if options.parametric:
+      for dir in job_dirs:
+        for mh in numpy.arange(options.mHlow,options.mHhigh,options.mHstep):
+          if not os.path.isfile('%s/%s/%s%5.1f.sh.done'%(suffix,dir,dir,mh)):
+            failed_jobs.append('%s/%s/%s%5.1f.sh'%(suffix,dir,dir,mh))
+    else:
+      for dir in job_dirs:
+        if not os.path.isfile('%s/%s/sub.sh.done'%(suffix,dir)):
+          failed_jobs.append('%s/%s/sub.sh'%(suffix,dir))
+
+  if len(failed_jobs)>0:
+    print 'The following jobs have failed:'
+    print '\t', failed_jobs
+    a = raw_input('Do you want to resubmit them? [y]/[n]\n')
+    if a=='y' or a=='Y' or a=='yes':
+      for job in failed_jobs:
+        if options.parametric: os.system('bsub -q 8nh -o %s/%s.log %s/%s'%(os.getcwd(),job,os.getcwd(),job))
+        else: os.system('bsub -q 8nh -o %s/%s.log %s/%s'%(os.getwcd(),job,os.getcwd(),job))
+      sys.exit()
+    else:
+      b = raw_input('There are failed jobs that haven\'t been resubmited. Do you want to continue? [y]/[n]\n')
+      if b=='n' or b=='N' or b=='no':
+        sys.exit()
+
   result_dir = os.getcwd()
   macros_dir = os.getcwd().split('Macros')[0]+'Macros'
   os.chdir(macros_dir)
   os.system("mkdir -p %s/plots"%(result_dir))
 
   for card, suffix, line in config:
+    print "-------------------------"
+    print "------ Running", suffix, "-----"
+    print "-------------------------"
+
     # pvalues
+    print "Running exppval..."
     os.system("python limitPlotter.py -M ProfileLikelihood --pval -p %s/%s/ExpProfileLikelihood -o %s/%s/exppval.root"%(result_dir,suffix,result_dir,suffix))
-    if options.parametric: os.system("python limitPlotter.py -M ProfileLikelihood --pval -p %s/%s/ExpProfileLikelihood_m125 -o %s/%s/exppval_m125.root"%(result_dir,suffix,result_dir,suffix))
-    pval_command = "python limitPlotter.py -M ProfileLikelihood --pval --addline=\"%s/%s/exppval.root:1:2:Expected 1xSM\" --addtxt=\"%s\""%(result_dir,suffix,line)
-    if options.parametric: pval_command += " --addline=\"%s/%s/exppval_m125.root:4:2:Expected 1xSM m_{H}=125"%(result_dir,suffix)
+    if options.parametric: 
+      print "Running exppval_m125..."
+      os.system("python limitPlotter.py -M ProfileLikelihood --pval -p %s/%s/ExpProfileLikelihood_m125 -o %s/%s/exppval_m125.root"%(result_dir,suffix,result_dir,suffix))
+    pval_command = "python limitPlotter.py -M ProfileLikelihood --nogrid --pval --addline=\"%s/%s/exppval.root:1:2:Expected 1xSM\" --addtxt=\"%s\""%(result_dir,suffix,line)
+    if options.parametric: pval_command += " --addline=\"%s/%s/exppval_m125.root:4:2:Expected 1xSM m_{H}=125\""%(result_dir,suffix)
     if options.unblind: pval_command += " -p %s/%s/ProfileLikelihood -o %s/%s/obspval.root"%(result_dir,suffix,result_dir,suffix)
-    else: pval_command += " -p %s/%s/ExpProfileLikelihood -e -o %s/%s/exppval.root"%(result_dir,suffix,result_dir,suffix)
+    else: pval_command += " -p %s/%s/ExpProfileLikelihood -e"%(result_dir,suffix)
+    print "Running pval plot..."
     os.system(pval_command)
-    print pval_command
+    #print '\t', pval_command
     if options.unblind:
       os.system("mv pvaluesplot.pdf %s/plots/%s_obspval.pdf"%(result_dir,suffix))
       os.system("mv pvaluesplot.png %s/plots/%s_obspval.png"%(result_dir,suffix))
@@ -106,6 +149,7 @@ else:
     # limits
     limit_command = "python limitPlotter.py -M Asymptotic -r -p %s/%s/Asymptotic --addtxt=\"%s\""%(result_dir,suffix,line)
     if not options.unblind: limit_command += " -e" 
+    print "Running limit plot..."
     os.system(limit_command)
     if options.unblind:
       os.system("mv limit_Asymptotic_ratio.pdf %s/plots/%s_obslimit.pdf"%(result_dir,suffix))
@@ -116,17 +160,18 @@ else:
     
     # best fit mu
     if options.unblind:
+      print "Running maxlh plot..."
       os.system("python limitPlotter.py -M MaxLikelihoodFit -p %s/%s/MaxLikelihoodFit --addtxt=\"%s\""%(result_dir,suffix,line))
       os.system("mv maxlhplot.pdf %s/%s_maxlh.pdf"%(result_dir,suffix)) 
       os.system("mv maxlhplot.png %s/%s_maxlh.png"%(result_dir,suffix)) 
   
   if options.runBoth and options.run2011 and options.run2012:
     # make a special pvalue plot
-    special_command = "python limitPlotter.py -M ProfileLikelihood --pval -p %s/7and8TeV/ExpProfileLikelihood -e --addline=\"%s/7TeV/exppval.root:2:2:Expected 1xSM (7TeV)\" --addline=\"%s/8TeV/exppval.root:4:2:Expected 1xSM (8TeV)\" --addline=\"%s/7and8TeV/exppval.root:1:2:Expected 1xSM (7+8TeV)\" --addtxt=\"#splitline{#sqrt{s}=7TeV L=%3.1f}{#sqrt{s}=8TeV L=%3.1f}\""%(result_dir,result_dir,result_dir,result_dir,options.lumi2011,options.lumi2012)
+    special_command = "python limitPlotter.py -M ProfileLikelihood --nogrid --pval -p %s/7and8TeV/ExpProfileLikelihood -e --addline=\"%s/7TeV/exppval.root:2:2:Expected 1xSM (7TeV)\" --addline=\"%s/8TeV/exppval.root:4:2:Expected 1xSM (8TeV)\" --addline=\"%s/7and8TeV/exppval.root:1:2:Expected 1xSM (7+8TeV)\" --addtxt=\"#splitline{#sqrt{s}=7TeV L=%3.1f}{#sqrt{s}=8TeV L=%3.1f}\""%(result_dir,result_dir,result_dir,result_dir,options.lumi2011,options.lumi2012)
     if options.unblind:
       special_command += " --addline=\"%s/7TeV/obspval.root:2:1:Observed (7TeV)\" --addline=\"%s/8TeV/obspval.root:4:1:Observed (8TeV)\" --addline=\"%s/7and8TeV/obspval.root:2:1:Observed (7+8TeV)\""%(result_dir,result_dir,result_dir,relvant)
     os.system(special_command)
-    print special_command
+    #print '\t', special_command
     if options.unblind:
       os.system("mv pvaluesplot.pdf %s/plots/obspval.pdf"%(result_dir))
       os.system("mv pvaluesplot.png %s/plots/obspval.png"%(result_dir))
