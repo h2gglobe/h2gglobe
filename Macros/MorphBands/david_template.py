28320d5c9f75a00555813306c6ec51325e2637f1
futyand <>
2013-02-18 00:21:05 +0000

    add histos

--- Macros/MorphBands/david_template.py
+++ Macros/MorphBands/david_template.py
@@ -3,8 +3,12 @@ templates = (
     'pho{pho}_sigmaEOverE_{R9}R9{syst}_cat{cat8}_DYJetsToLL',
     'pho{pho}_phoidMva_{ebee2}{syst}_cat0_DYJetsToLL',
     'pho{pho}_phoidMva_{R9}R9{syst}_cat{cat8}_DYJetsToLL',
+    'pho{pho}_phoidMva_EB{syst}_cat0_DYJetsToLL',
+    'pho{pho}_phoidMva_EE{syst}_cat0_DYJetsToLL',
+    'pho{pho}_sigmaEOverE_EB{syst}_cat0_DYJetsToLL',
+    'pho{pho}_sigmaEOverE_EE{syst}_cat0_DYJetsToLL',
     'bdtout{ebee}{syst}_cat0_DYJetsToLL',
-    'bdtout{syst}_cat{cat4}_DYJetsToLL',
+    'bdtout{syst}_cat{cat5}_DYJetsToLL',
     'bdtout_{R9t}R9{syst}_cat{cat4}_DYJetsToLL',
     )
 
@@ -14,6 +18,7 @@ variations = {
     'R9' : ('low','mid','high'),
     'cat8' : tuple(range(8)),
     'cat4' : tuple(range(4)),
+    'cat5' : tuple(range(5)),
     'ebee' : ('EB','EE','EBEE'),
     'ebee2' : ('EB','EE'),
     'R9t' : ('low','mixed','high'),
