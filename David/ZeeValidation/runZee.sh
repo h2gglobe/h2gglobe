7c0a8f8404305c90ac90d16fab83b859cfb48755
futyand <>
2013-04-04 23:27:16 +0000

    update following macro renamings

--- David/ZeeValidation/runZee.sh
+++ David/ZeeValidation/runZee.sh
@@ -1,14 +1,14 @@
 #!/bin/csh
 
-root -b -l -q 'BDT_Zee_bdtin.C(0,0)'
-root -b -l -q BDT_Zee_bdtin.C
+root -b -l -q 'diphomva_inputs.C(0,0)'
+root -b -l -q diphomva_inputs.C
 root -b -l -q bdtout_syst.C
 root -b -l -q idmva.C
 root -b -l -q idmvaInputs_EB.C
 root -b -l -q idmvaInputs_EE.C
 root -b -l -q sigmaE.C
-root -b -l -q 'BDT_Zee_bdtin_cat.C(0)'
-root -b -l -q BDT_Zee_bdtin_cat.C
+root -b -l -q 'diphomva_inputs_cat.C(0)'
+root -b -l -q diphomva_inputs_cat.C
 root -b -l -q ptRescale.C
 root -b -l -q rhoVsNvtx.C
 root -b -l -q 'zee_mass.C(0)'
--- David/ZeeValidation/runZee_interp.sh
+++ David/ZeeValidation/runZee_interp.sh
@@ -1,14 +1,14 @@
 #!/bin/csh
 
-root -b -l -q 'BDT_Zee_bdtin.C(0,0)'
-root -b -l -q BDT_Zee_bdtin.C
+root -b -l -q 'diphomva_inputs.C(0,0)'
+root -b -l -q diphomva_inputs.C
 root -b -l -q bdtout_syst_interp.C
 root -b -l -q idmva_interp.C
 root -b -l -q idmvaInputs_EB.C
 root -b -l -q idmvaInputs_EE.C
 root -b -l -q sigmaE_interp.C
-root -b -l -q 'BDT_Zee_bdtin_cat.C(0)'
-root -b -l -q BDT_Zee_bdtin_cat.C
+root -b -l -q 'diphomva_inputs_cat.C(0)'
+root -b -l -q diphomva_inputs_cat.C
 root -b -l -q ptRescale.C
 root -b -l -q rhoVsNvtx.C
 root -b -l -q 'zee_mass.C(0)'
