448d699d30ae697e74b056dcfb40dffd85e6eeb7
futyand <>
2013-04-04 20:50:10 +0000

    add variables in nvtx bins

--- Macros/MorphBands/david_template.py
+++ Macros/MorphBands/david_template.py
@@ -10,6 +10,9 @@ templates = (
     'bdtout{ebee}{syst}_cat0_DYJetsToLL',
     'bdtout{syst}_cat{cat5}_DYJetsToLL',
     'bdtout_{R9t}R9{syst}_cat{cat4}_DYJetsToLL',
+    'bdtout{syst}{vtxbin}_cat0_DYJetsToLL',
+    'idmva_EB{syst}{vtxbin}_cat0_DYJetsToLL',
+    'idmva_EE{syst}{vtxbin}_cat0_DYJetsToLL',
     )
 
 # 'syst' is a special variation and should correspond to the things to be morphed: nominal, plus, minus
@@ -23,4 +26,5 @@ variations = {
     'ebee2' : ('EB','EE'),
     'R9t' : ('low','mixed','high'),
     'syst' : ('','_up','_down'),
+    'vtxbin' : ('_nvtx0to13','_nvtx14to18','_nvtx19up'),
     }
