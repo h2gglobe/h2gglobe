37fa4c16266b81e578ec98a271a1e6a32e434b78
obondu <>
2013-04-19 12:12:08 +0000

    avoid creating unnecessary .pileup.root.pileup.root files

--- Reduction/mk_pileup.sh
+++ Reduction/mk_pileup.sh
@@ -10,7 +10,7 @@ dir=/store/group/phys_higgs/cmshgg/reduced/${version}/mc/Summer12_S10_8TeV
 
 cd pileup 
 rm *.root
-cmsLs $dir | awk '{ print $5}' | grep '/' | grep -v broken > samples.txt 
+cmsLs $dir | awk '{ print $5}' | grep '/' | grep -v broken | grep -v root > samples.txt 
 
 echo "Hadding and copying"
 ../parallel --eta --joblog parallel_pileupMerger.log --progress "python ../Macros/pileup/pileupMerger.py --putBack {1}" :::: samples.txt
