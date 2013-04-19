bfddf1844702b6f24b1381033b14e6cca9c376c6
musella <>
2013-04-19 09:19:06 +0000

    Fixed copy back to eos

--- Reduction/mk_pileup.sh
+++ Reduction/mk_pileup.sh
@@ -6,15 +6,13 @@ source "version.sh"
 
 mkdir -p pileup
 
-dir=/store/group/phys_higgs/cmshgg/reduced/${version}/Summer12_S10_8TeV
+dir=/store/group/phys_higgs/cmshgg/reduced/${version}/mc/Summer12_S10_8TeV
 
 cd pileup 
 rm *.root
 cmsLs $dir | awk '{ print $5}' | grep '/' | grep -v broken > samples.txt 
 
-echo "Hadding..."
-../parallel --eta --joblog parallel_pileupMerger.log --progress "python ../Macros/pileup/pileupMerger.py {1}" :::: samples.txt
+echo "Hadding and copying"
+../parallel --eta --joblog parallel_pileupMerger.log --progress "python ../Macros/pileup/pileupMerger.py --putBack {1}" :::: samples.txt
 
-echo "Copying to eos..."
-../parallel --eta --joblog parallel_pileupCopy.log --progress "cmsStage -f `basename {1}`.pileup.root $dir" :::: samples.txt
 
