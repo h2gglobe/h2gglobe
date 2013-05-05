cfc8f526da3fecb9a26afc40a90d6fc46a060759
cbeiraod <>
2013-05-05 21:53:02 +0000

    Updated diyPlot to estimate error using the jackknife method. Had to add the MathMore lib to the makefile

--- SpinAnalysis/test/diyPlot.cpp
+++ SpinAnalysis/test/diyPlot.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 #include <algorithm>
 #include <cmath>
 
@@ -17,16 +18,241 @@
 #include "TStyle.h"
 #include "TMath.h"
 #include "Math/DistFunc.h"
+#include "Math/QuantFuncMathMore.h"
 
 using namespace std;
 
-class  NormalQuantile {
-public:
-	NormalQuantile() {};
-	double operator() (double *x, double *p) {
-		return ROOT::Math::normal_quantile_c(x[0], p[0]);
-	};
-};
+namespace separation
+{
+  #define JK_PARAM 20
+
+  class  NormalQuantile
+  {
+  public:
+	  NormalQuantile() {};
+	  double operator() (double *x, double *p) {
+		  return ROOT::Math::normal_quantile_c(x[0], p[0]);
+	  };
+  };
+
+  template <class T>
+  std::pair<std::pair<double, double>, std::pair<double, double> > p_value(std::vector<T> H0, std::vector<T> H1)
+  {
+    for(UInt_t i = 0; i < H0.size()-2; i++)
+      for(UInt_t j = 0; j < H0.size()-1-i; j++)
+        if(H0[j] > H0[j+1])
+          std::swap(H0[j], H0[j+1]);
+    for(UInt_t i = 0; i < H1.size()-2; i++)
+      for(UInt_t j = 0; j < H1.size()-1-i; j++)
+        if(H1[j] > H1[j+1])
+          std::swap(H1[j], H1[j+1]);
+
+    UInt_t size = H0.size();
+    T H0med = (size%2)?(H0[size/2]):((H0[size/2]+H0[size/2-1])/2);
+    size = H1.size();
+    T H1med = (size%2)?(H1[size/2]):((H1[size/2]+H1[size/2-1])/2);
+
+    double H0pval = 0;
+    double H1pval = 0;
+    for(UInt_t i = 0; i < H0.size(); i++)
+      if(H0[i] > H1med)
+      {
+        H0pval = H0.size()-(i-1);
+        break;
+      }
+    for(UInt_t i = 0; i < H1.size(); i++)
+      if(H1[i] > H0med)
+      {
+        H1pval = i;
+        break;
+      }
+
+    separation::NormalQuantile quant;
+    double width = 1.0;
+
+    std::pair<std::pair<double, double>, std::pair<double, double> > retVal;
+    retVal.first.first = H0pval/H0.size();
+    retVal.first.second = quant(&retVal.first.first, &width);
+    retVal.second.first = H1pval/H1.size();
+    retVal.second.second = quant(&retVal.second.first, &width);
+
+    return retVal;
+  }
+
+  template <class T>
+  std::pair<
+            std::pair<
+                      std::pair<double, double>,
+                      std::pair<double, double>
+                      >,
+            std::pair<
+                      std::pair<double, double>,
+                      std::pair<double, double>
+                      >
+            > JackKnife(std::vector<T> H0, std::vector<T> H1, std::pair<std::pair<double, double>, std::pair<double, double> > (*func)(std::vector<T>, std::vector<T>))
+  {
+    std::pair<
+              std::pair<
+                        std::pair<double, double>,
+                        std::pair<double, double>
+                        >,
+              std::pair<
+                        std::pair<double, double>,
+                        std::pair<double, double>
+                        >
+              > retVal;
+
+    UInt_t H0_size = H0.size()/JK_PARAM;
+    UInt_t H1_size = H1.size()/JK_PARAM;
+
+    std::random_shuffle(H0.begin(),H0.end());
+    std::random_shuffle(H1.begin(),H1.end());
+
+    std::vector<double> H0pvals, H1pvals, H0sigmas, H1sigmas;
+    double H0pvalue = 0, H0sigma = 0, H1pvalue = 0, H1sigma = 0;
+
+    for(UInt_t i = 0; i < JK_PARAM; i++)
+    {
+      std::vector<T> H0_small, H1_small;
+
+      for(UInt_t j = 0; j < H0.size(); j++)
+      {
+        if(j < i*H0_size || j > (i+1)*H0_size-1)
+        {
+          H0_small.push_back(H0[j]);
+        }
+      }
+      for(UInt_t j = 0; j < H1.size(); j++)
+      {
+        if(j < i*H1_size || j > (i+1)*H1_size-1)
+        {
+          H1_small.push_back(H1[j]);
+        }
+      }
+
+      std::pair<std::pair<double, double>, std::pair<double, double> > temp = func(H0_small, H1_small);
+
+      H0pvals.push_back(temp.first.first);
+      H0pvalue += temp.first.first;
+      H0sigmas.push_back(temp.first.second);
+      H0sigma += temp.first.second;
+      H1pvals.push_back(temp.second.first);
+      H1pvalue += temp.second.first;
+      H1sigmas.push_back(temp.second.second);
+      H1sigma += temp.second.second;
+    }
+
+    UInt_t size = H0pvals.size();
+    H0pvalue /= size;
+    H1pvalue /= size;
+    H0sigma /= size;
+    H1sigma /= size;
+
+
+    double H0pvalueErr = 0, H0sigmaErr = 0, H1pvalueErr = 0, H1sigmaErr = 0;
+    for(UInt_t i = 0; i < H0pvals.size(); i++)
+    {
+      H0pvals[i] -= H0pvalue;
+      H1pvals[i] -= H1pvalue;
+      H0sigmas[i] -= H0sigma;
+      H1sigmas[i] -= H1sigma;
+
+      H0pvals[i] *= H0pvals[i];
+      H1pvals[i] *= H1pvals[i];
+      H0sigmas[i] *= H0sigmas[i];
+      H1sigmas[i] *= H1sigmas[i];
+
+      H0pvalueErr += H0pvals[i];
+      H1pvalueErr += H1pvals[i];
+      H0sigmaErr += H0sigmas[i];
+      H1sigmaErr += H1sigmas[i];
+    }
+
+    H0pvalueErr /= JK_PARAM*(JK_PARAM-1);
+    H1pvalueErr /= JK_PARAM*(JK_PARAM-1);
+    H0sigmaErr /= JK_PARAM*(JK_PARAM-1);
+    H1sigmaErr /= JK_PARAM*(JK_PARAM-1);
+
+    H0pvalueErr = sqrt(H0pvalueErr);
+    H1pvalueErr = sqrt(H1pvalueErr);
+    H0sigmaErr = sqrt(H0sigmaErr);
+    H1sigmaErr = sqrt(H1sigmaErr);
+
+    double onesigma = ROOT::Math::tdistribution_quantile_c(0.16, JK_PARAM-1);
+
+
+    retVal.first.first.first = H0pvalue;
+    retVal.second.first.first = H1pvalue;
+    retVal.first.second.first = H0sigma;
+    retVal.second.second.first = H1sigma;
+
+    retVal.first.first.second = H0pvalueErr*onesigma;
+    retVal.second.first.second = H1pvalueErr*onesigma;
+    retVal.first.second.second = H0sigmaErr*onesigma;
+    retVal.second.second.second = H1sigmaErr*onesigma;
+
+    return retVal;
+  }
+}
+
+map<string,double> calcSeparation(TTree *tree)
+{
+  map<string,double> retVal;
+
+  double q_smtoy;
+  double q_gravtoy;
+  tree->SetBranchAddress("q_smtoy",&q_smtoy);
+  tree->SetBranchAddress("q_gravtoy",&q_gravtoy);
+  std::vector<double> SMToys;
+  std::vector<double> GravToys;
+
+  for(Int_t i=0;i<tree->GetEntries();i++)
+  {
+    tree->GetEntry(i);
+    SMToys.push_back(q_smtoy);
+    GravToys.push_back(q_gravtoy);
+  }
+
+  std::pair<std::pair<double, double>, std::pair<double, double> > p_values = separation::p_value<double>(SMToys, GravToys);
+
+  retVal.insert(std::pair<string, double>("SMProb", p_values.first.first));
+  retVal.insert(std::pair<string, double>("GravProb", p_values.second.first));
+  retVal.insert(std::pair<string, double>("SMSigma", p_values.first.second));
+  retVal.insert(std::pair<string, double>("GravSigma", p_values.second.second));
+
+  separation::NormalQuantile quant;
+  TF1 *func = new TF1("my_quantile", &quant, 0, 1, 1);
+  double width = 1.0;
+
+  retVal.insert(std::pair<string, double>("SMProbErr", sqrt(retVal["SMProb"]*(1-retVal["SMProb"])/SMToys.size())));
+  retVal.insert(std::pair<string, double>("GravProbErr", sqrt(retVal["GravProb"]*(1-retVal["GravProb"])/GravToys.size())));
+  retVal.insert(std::pair<string, double>("SMSigmaErr", abs(func->Derivative(retVal["SMProb"], &width)*retVal["SMProbErr"])));
+  retVal.insert(std::pair<string, double>("GravSigmaErr", abs(func->Derivative(retVal["GravProb"], &width)*retVal["GravProbErr"])));
+
+  std::pair<
+            std::pair<
+                      std::pair<double, double>,
+                      std::pair<double, double>
+                      >,
+            std::pair<
+                      std::pair<double, double>,
+                      std::pair<double, double>
+                      >
+            > p_values_jk = separation::JackKnife<double>(SMToys, GravToys, separation::p_value<double>);
+
+  retVal.insert(std::pair<string, double>("SMProb_jk", p_values_jk.first.first.first));
+  retVal.insert(std::pair<string, double>("GravProb_jk", p_values_jk.second.first.first));
+  retVal.insert(std::pair<string, double>("SMSigma_jk", p_values_jk.first.second.first));
+  retVal.insert(std::pair<string, double>("GravSigma_jk", p_values_jk.second.second.first));
+
+  retVal.insert(std::pair<string, double>("SMProbErr_jk", p_values_jk.first.first.second));
+  retVal.insert(std::pair<string, double>("GravProbErr_jk", p_values_jk.second.first.second));
+  retVal.insert(std::pair<string, double>("SMSigmaErr_jk", p_values_jk.first.second.second));
+  retVal.insert(std::pair<string, double>("GravSigmaErr_jk", p_values_jk.second.second.second));
+
+
+  return retVal;
+}
 
 int main(int argc, char* argv[]){
 
@@ -240,7 +466,7 @@ int main(int argc, char* argv[]){
   tree->Draw("q_smtoy>>testStatSMH","q_smtoy>=-900. && q_smtoy<=900.","goff");
   tree->Draw("q_gravtoy>>testStatGRAVH","q_gravtoy>=-900. && q_gravtoy<=900.","goff");
 
-  NormalQuantile quant;
+  separation::NormalQuantile quant;
   TF1 *func = new TF1("quant", &quant, 0, 1, 1);
   double width = 1.0;
 
@@ -291,68 +517,15 @@ int main(int argc, char* argv[]){
   cout << "Prob( q > median(2) | 0 ) = "<< SMprobHist << " = " << SMsigmaHist << " sigma " << endl;
   cout << "Prob( q < median(0) | 2 ) = "<< GRAVprobHist << " = " << GRAVsigmaHist << " sigma " << endl;
 
-  //tree
-  double q_smtoy;
-  double q_gravtoy;
-  tree->SetBranchAddress("q_smtoy",&q_smtoy);
-  tree->SetBranchAddress("q_gravtoy",&q_gravtoy);
-  std::vector<double> SMToys;
-  std::vector<double> GravToys;
-
-  for(Int_t i=0;i<tree->GetEntries();i++)
-  {
-    tree->GetEntry(i);
-    SMToys.push_back(q_smtoy);
-    GravToys.push_back(q_gravtoy);
-
-    for(size_t j = SMToys.size()-1; j>0; j--)
-    {
-      if(SMToys[j-1] > SMToys[j])
-        std::swap(SMToys[j], SMToys[j-1]);
-      else
-        break;
-    }
-
-    for(size_t j = GravToys.size()-1; j>0; j--)
-    {
-      if(GravToys[j] < GravToys[j-1])
-        std::swap(GravToys[j], GravToys[j-1]);
-      else
-        break;
-    }
-  }
-
-  double SMMedian = ((SMToys.size()%2)?(SMToys[SMToys.size()/2]):((SMToys[SMToys.size()/2]+SMToys[SMToys.size()/2-1])/2));
-  double GravMedian = ((GravToys.size()%2)?(GravToys[GravToys.size()/2]):((GravToys[GravToys.size()/2]+GravToys[GravToys.size()/2-1])/2));
-  //cout << "SMMedian: " << SMMedian << "; SMMax: " << SMToys[SMToys.size()-1] << "; SMMin: " << SMToys[0] << endl;
-  //cout << "\tSMToys[999]: " << SMToys[999] << "; SMToys[1000]: " << SMToys[1000] << endl;
-  double SMProbUnbinned = 0, GravProbUnbinned = 0;
-  for(size_t i = 0; i < SMToys.size(); i++)
-  {
-    if(SMToys[i] < GravMedian)
-      SMProbUnbinned = i+1;
-  }
-  for(size_t i = 0; i < GravToys.size(); i++)
-  {
-    if(GravToys[i] < SMMedian)
-      GravProbUnbinned = i+1;
-  }
-  //cout << "SMProb: " << SMProbUnbinned << "; GRAVProb: " << GravProbUnbinned << endl;
-  SMProbUnbinned = SMToys.size() - SMProbUnbinned;
-  SMProbUnbinned /= SMToys.size();
-  GravProbUnbinned /= GravToys.size();
-
-  double SMProbUnbinnedErr = sqrt(SMProbUnbinned*(1-SMProbUnbinned)/SMToys.size());
-  double GravProbUnbinnedErr = sqrt(GravProbUnbinned*(1-GravProbUnbinned)/GravToys.size());
-
-  double SMSigmaUnbinned = ROOT::Math::normal_quantile_c(SMProbUnbinned,width);
-  double SMSigmaUnbinnedErr = abs(func->Derivative(SMProbUnbinned, &width)*SMProbUnbinnedErr);
-  double GravSigmaUnbinned = ROOT::Math::normal_quantile_c(GravProbUnbinned,width);
-  double GravSigmaUnbinnedErr = abs(func->Derivative(GravProbUnbinned, &width)*GravProbUnbinnedErr);
+  map<string,double> temp = calcSeparation(tree);
 
   cout << "UNBINNED:  -- normal_quantile_c" << endl;
-  cout << "Prob( q > median(2) | 0 ) = "<< SMProbUnbinned << "+-" << SMProbUnbinnedErr << " = " << SMSigmaUnbinned << "+-" << SMSigmaUnbinnedErr << " sigma " << endl;
-  cout << "Prob( q < median(0) | 2 ) = "<< GravProbUnbinned << "+-" << GravProbUnbinnedErr << " = " << GravSigmaUnbinned << "+-" << GravSigmaUnbinnedErr << " sigma " << endl;
+  cout << "Prob( q > median(2) | 0 ) = " << temp["SMProb"] << "+-" << temp["SMProbErr"] << " = " << temp["SMSigma"] << "+-" << temp["SMSigmaErr"] << " sigma " << endl;
+  cout << "Prob( q < median(0) | 2 ) = " << temp["GravProb"] << "+-" << temp["GravProbErr"] << " = " << temp["GravSigma"] << "+-" << temp["GravSigmaErr"] << " sigma " << endl;
+
+  cout << "UNBINNED_JK:  -- normal_quantile_c" << endl;
+  cout << "Prob( q > median(2) | 0 ) = " << temp["SMProb_jk"] << "+-" << temp["SMProbErr_jk"] << " = " << temp["SMSigma_jk"] << "+-" << temp["SMSigmaErr_jk"] << " sigma " << endl;
+  cout << "Prob( q < median(0) | 2 ) = " << temp["GravProb_jk"] << "+-" << temp["GravProbErr_jk"] << " = " << temp["GravSigma_jk"] << "+-" << temp["GravSigmaErr_jk"] << " sigma " << endl;
 
   if(nCats >= 0)
   {
@@ -369,10 +542,10 @@ int main(int argc, char* argv[]){
     tree->Branch("FittedSMProbErr", &SMprobErr);
     tree->Branch("FittedGravProbErr", &GRAVprobErr);
 
-    tree->Branch("UnbinnedSMProb", &SMProbUnbinned);
-    tree->Branch("UnbinnedGravProb", &GravProbUnbinned);
-    tree->Branch("UnbinnedSMProbErr", &SMProbUnbinnedErr);
-    tree->Branch("UnbinnedGravProbErr", &GravProbUnbinnedErr);
+    tree->Branch("UnbinnedSMProb", &temp["SMProb"]);
+    tree->Branch("UnbinnedGravProb", &temp["GravProb"]);
+    tree->Branch("UnbinnedSMProbErr", &temp["SMProbErr"]);
+    tree->Branch("UnbinnedGravProbErr", &temp["GravProbErr"]);
 
     tree->Branch("BinnedSMProb", &SMprobHist);
     tree->Branch("BinnedGravProb", &GRAVprobHist);
@@ -385,10 +558,10 @@ int main(int argc, char* argv[]){
     tree->Branch("FittedSMSigmaErr", &SMsigmaErr);
     tree->Branch("FittedGravSigmaErr", &GRAVsigmaErr);
 
-    tree->Branch("UnbinnedSMSigma", &SMSigmaUnbinned);
-    tree->Branch("UnbinnedGravSigma", &GravSigmaUnbinned);
-    tree->Branch("UnbinnedSMSigmaErr", &SMSigmaUnbinnedErr);
-    tree->Branch("UnbinnedGravSigmaErr", &GravSigmaUnbinnedErr);
+    tree->Branch("UnbinnedSMSigma", &temp["SMSigma"]);
+    tree->Branch("UnbinnedGravSigma", &temp["GravSigma"]);
+    tree->Branch("UnbinnedSMSigmaErr", &temp["SMSigmaErr"]);
+    tree->Branch("UnbinnedGravSigmaErr", &temp["GravSigmaErr"]);
 
     tree->Branch("BinnedSMSigma", &SMsigmaHist);
     tree->Branch("BinnedGravSigma", &GRAVsigmaHist);
