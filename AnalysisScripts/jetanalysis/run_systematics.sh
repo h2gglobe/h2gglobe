ae640d7968b5ede9b0dd8c0bfb766ab061a2242b
musella <>
2013-05-30 19:52:36 +0000

    Some updates to VBF stuff.

--- AnalysisScripts/jetanalysis/run_systematics.sh
+++ AnalysisScripts/jetanalysis/run_systematics.sh
@@ -2,25 +2,31 @@
 
 export DISPLAY=""
 
-tunes=""
+dir="yr3_systematics_v1"
+mkdir $dir
+
+tunes="TuneD6T TuneD6TUEOFF TuneP0 TuneP0UEOFF TuneProPT0 TuneProPT0UEOFF TuneProQ20 TuneProQ20UEOFF TuneUEOFF TuneZ2Star"
 ## tunes="TuneP0 TuneProPT0 TuneUEOFF TuneZ2Star TuneD6T TuneProQ20"
 
 for tune in $tunes; do
     rm jetanalysis/tmp_datafiles_mvavbftag_tunes.dat{,.pevents}
-    ./mk_fitter.py -i jetanalysis/datafiles_mvavbftag_tunes.dat -n 5 -l ${tune} -o tunes_v2/mavtag_${tune}/sub &&  \
-     	./submit_fitter.py -q 8nh -d tunes_v2/mavtag_${tune}
-    rm jetanalysis/tmp_datafiles_cutbasedvbftag_tunes.dat{,.pevents}
-    ./mk_fitter.py -i jetanalysis/datafiles_cutbasedvbftag_tunes.dat -n 5 -l ${tune} -o tunes_v2/cutbtag_${tune}/sub && \
-	./submit_fitter.py -q 8nh -d tunes_v2/cutbtag_${tune}
+    ./mk_fitter.py -i jetanalysis/datafiles_mvavbftag_tunes.dat -n 5 -l ${tune} -o ${dir}/mavtag_${tune}/sub &&  \
+     	./submit_fitter.py -q 8nh -d ${dir}/mavtag_${tune}
+    ### rm jetanalysis/tmp_datafiles_cutbasedvbftag_tunes.dat{,.pevents}
+    ### ./mk_fitter.py -i jetanalysis/datafiles_cutbasedvbftag_tunes.dat -n 5 -l ${tune} -o ${dir}/cutbtag_${tune}/sub && \
+    ###	./submit_fitter.py -q 8nh -d ${dir}/cutbtag_${tune}
 done
 
+## dir="yr3_systematics_v1"
+## mkdir $dir
 
+jecUncs=""
 ## jecUncs="jerCentral"
-jecUncs="nominal jecUp jecDown jerCentral jerUp jerDown"
+## jecUncs="nominal jecUp jecDown jerCentral jerUp jerDown"
 
 for syst in $jecUncs; do
-    ./mk_fitter.py -i jetanalysis/datafiles_mvavbftag_jec.dat -n 5 -l ${syst} -o jec_v3/mvatag_${syst}/sub && \
-	./submit_fitter.py -q 8nh -d jec_v3/mvatag_${syst}
-    ./mk_fitter.py -i jetanalysis/datafiles_cutbasedvbftag_jec.dat -n 5 -l ${syst} -o jec_v3/cutbtag_${syst}/sub && \
-	./submit_fitter.py -q 8nh -d jec_v3/cutbtag_${syst}
+    ./mk_fitter.py -i jetanalysis/datafiles_mvavbftag_jec.dat -n 5 -l ${syst} -o ${dir}/mvatag_${syst}/sub && \
+	./submit_fitter.py -q 8nh -d ${dir}/mvatag_${syst}
+    ./mk_fitter.py -i jetanalysis/datafiles_cutbasedvbftag_jec.dat -n 5 -l ${syst} -o ${dir}/cutbtag_${syst}/sub && \
+	./submit_fitter.py -q 8nh -d ${dir}/cutbtag_${syst}
 done
