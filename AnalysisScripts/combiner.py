e3fe6fc0662318ef27716029a3d96053ede6b5dd
musella <>
2013-06-03 12:58:11 +0000

    Added option to specify input and output files on the commandlin

--- AnalysisScripts/combiner.py
+++ AnalysisScripts/combiner.py
@@ -5,7 +5,9 @@ from sys import argv
 from python.configProducer import *
 from python.lumi import *
 import sys
-from python.runOptions import *
+from python.runOptions import parser
+parser.parser.add_option("-f","--files",dest="files",type="string",action="store",default="")
+parser.parser.add_option("-o","--output",dest="output",type="string",action="store",default="")
 (options,args)=parser.parse_args()
 if (int(options.nJobs) > 0) and (int(options.jobId) >= int(options.nJobs)):
   sys.exit("Job id's must run from 0 -> %d when splitting into %d jobs"%(int(options.nJobs)-1,int(options.nJobs)))
@@ -22,9 +24,10 @@ ROOT.gBenchmark.Start("Analysis");
 config_file="filestocombine.dat"
 if options.inputDat:
     config_file = options.inputDat
+files=[f for f.lstrip().rstrip() in options.files.split(',') if f != ""]
 
 ut = ROOT.LoopAll();
-cfg = configProducer(ut,config_file,0,int(options.nJobs),int(options.jobId))
+cfg = configProducer(ut,config_file,0,int(options.nJobs),int(options.jobId),files=files,histfile=options.output)
 #cfg = configProducer(ut,config_file,0,-1,0)
 
 if not options.dryRun:
