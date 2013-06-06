c3225534266b87242b27a7eab371da733cb96678
musella <>
2013-06-06 12:26:33 +0000

    Fixed command line input parsing

--- AnalysisScripts/combiner.py
+++ AnalysisScripts/combiner.py
@@ -6,8 +6,8 @@ from python.configProducer import *
 from python.lumi import *
 import sys
 from python.runOptions import parser
-parser.parser.add_option("-f","--files",dest="files",type="string",action="store",default="")
-parser.parser.add_option("-o","--output",dest="output",type="string",action="store",default="")
+parser.add_option("-f","--files",dest="files",type="string",action="store",default="")
+parser.add_option("-o","--output",dest="output",type="string",action="store",default="")
 (options,args)=parser.parse_args()
 if (int(options.nJobs) > 0) and (int(options.jobId) >= int(options.nJobs)):
   sys.exit("Job id's must run from 0 -> %d when splitting into %d jobs"%(int(options.nJobs)-1,int(options.nJobs)))
@@ -24,10 +24,12 @@ ROOT.gBenchmark.Start("Analysis");
 config_file="filestocombine.dat"
 if options.inputDat:
     config_file = options.inputDat
-files=[f for f.lstrip().rstrip() in options.files.split(',') if f != ""]
+fnames=[]
+if options.files != "":
+  fnames=[f.lstrip(" ").rstrip(" ") for f in options.files.split(',') if f != ""]
 
 ut = ROOT.LoopAll();
-cfg = configProducer(ut,config_file,0,int(options.nJobs),int(options.jobId),files=files,histfile=options.output)
+cfg = configProducer(ut,config_file,0,int(options.nJobs),int(options.jobId),files=fnames,histfile=options.output,mountEos=options.mountEos)
 #cfg = configProducer(ut,config_file,0,-1,0)
 
 if not options.dryRun:
