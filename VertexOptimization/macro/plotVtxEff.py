98add6275f8316d7eae0d884056dc808a6777b51
musella <>
2013-06-13 08:49:03 +0000

    better plotVtxEff

--- VertexOptimization/macro/plotVtxEff.py
+++ VertexOptimization/macro/plotVtxEff.py
@@ -1,41 +1,29 @@
 #!/bin/env python
 
-from ROOT import TFile, TF1, TCanvas, kBlack, kBlue, kRed, kGreen, kCyan, kMagenta, TGraph, TF2, TF12, kFullCircle, gROOT, TGraphAsymmErrors
+from ROOT import TFile, TF1, TCanvas, kBlack, kBlue, kRed, kGreen, kCyan, kMagenta, kWhite, TGraph, TF2, TF12, kFullCircle, gROOT, TLegend, TPaveText
 from sys import argv
-import numpy
 
-def rescaleEff(fin):
-    var = "pt"
-    num=fin.Get("%s_rv_cat0_tot" % var ).Clone()
-    den=fin.Get("%s_cat0_tot"    % var ).Clone()
+def eff(fin,name,var="pt",rebin=5):
 
-    xbins = numpy.array([0.,10.,20.,30.,40.,50.,70.,110.,250.])
-
-    num_rebin = num.Rebin(len(xbins)-1,"num_rebin",xbins)
-    den_rebin = den.Rebin(len(xbins)-1,"den_rebin",xbins)
-    
-    eff = TGraphAsymmErrors( num_rebin, den_rebin )
-    eff.SetName("efficiency")
-
-    return eff;
-    
-
-def eff(fin,name,var="pt",rebin=4):
-
-    num=fin.Get("%s_rv_cat0_tot" % var ).Clone()
-    den=fin.Get("%s_cat0_tot"    % var ).Clone()
+    num=fin.Get("%s_rv_cat0_tot" % var )
+    den=fin.Get("%s_cat0_tot"    % var )
 
     if rebin > 0:
         num.Rebin(rebin)
         den.Rebin(rebin)
-    
-    print num.Integral() / den.Integral(), den.Integral()
+
 
     eff = num.Clone(name)
     eff.Divide( num, den, 1., 1., "B" )
+
+    num.Rebin( num.GetNbinsX() )
+    den.Rebin( den.GetNbinsX() )
+    avg_eff = num.Clone()
+    avg_eff.Divide( num, den, 1., 1., "B" )
+    print "%1.3f +- %1.3f" % ( avg_eff.GetBinContent(1), avg_eff.GetBinError(1) )
     return eff
 
-def prob(fin,name,var="pt",profile=True,rebin=4):
+def prob(fin,name,var="pt",profile=True,rebin=5):
 
     prb = fin.Get("vtxprob_%s_cat0_tot" % var)
 
@@ -43,49 +31,110 @@ def prob(fin,name,var="pt",profile=True,rebin=4):
         prb.RebinX(rebin)
     
     if profile:
-        return prb.ProfileX(name)
+        return prb.ProfileX(name,1,-1,"w")
 
     return prb.Clone(name)
-    
+
 gROOT.LoadMacro("../Macros/rootglobestyle.C")
 from ROOT import setTDRStyle, gStyle
 setTDRStyle()
 gStyle.SetOptTitle(0)
 
-## file="histograms_vtxeff.root"
-file1=argv[1]
-## file2=argv[2]
 
-fin1=TFile.Open(file1)
-## fin2=TFile.Open(file2)
+file=argv[1]
+
+fin=TFile.Open(file)
+eff1 = eff(fin,"eff1",rebin=10)
+eff2 = eff(fin,"eff2","nvtx",rebin=0)
+print eff2
+
+prob1 = prob(fin,"prob1")
+prob2 = prob(fin,"prob2","nvtx",True,0)
+
+eff1.SetMarkerStyle(21);
+eff2.SetMarkerStyle(21);
+
+eff1.SetLineColor(kBlack);
+eff2.SetLineColor(kBlack);
+
+prob1.SetFillColor(4);
+prob1.SetLineColor(4);
+prob1.SetMarkerColor(4);
+
+prob2.SetFillColor(4);
+prob2.SetLineColor(4);
+prob2.SetMarkerColor(4);
+
+pt = TPaveText(51.37856,0.5654967,190.8217,0.6623778,"br")
+pt.SetFillColor(0)
+pt.SetLineColor(0)
+pt.SetTextAlign(13)
+pt.SetTextFont(42)
+pt.AddText("CMS Preliminary Simulation")
+## pt.AddText("<PU> = 19")
+pt.Draw("same")
+
+### c1 = TCanvas("vtxProbPt","vtxProbPt")
+### eff1.SetTitle("Vertex efficiency;p_{T}(#gamma #gamma) (GeV);Fraction | z_{reco} - z_{true} | < 10 mm")
+### prob1.SetTitle("Average vertex probability;p_{T}(#gamma #gamma) (GeV);Fraction | z_{reco} - z_{true} | < 10 mm")
+### eff1.GetYaxis().SetRangeUser(0.5, 1.1)
+### eff1.GetXaxis().SetTitleOffset(1.1);
+### eff1.GetYaxis().SetTitleOffset(1.2);
+### 
+### eff1.Draw("e0")
+### prob1.Draw("e3 same")
+### eff1.Draw("e0 same")
+### 
+### leg1 = TLegend(0.38,0.44,0.84,0.60)
+### leg1.SetShadowColor(kWhite), leg1.SetLineColor(kWhite), leg1.SetFillColor(kWhite), leg1.SetTextFont(60)
+### 
+### leg1.AddEntry(eff1,"","pe")
+### leg1.AddEntry(prob1,"","f")
+### leg1.Draw("same")
+### pt.Draw("same")
 
 
-fout = TFile.Open("vertex_efficiency.root","recreate")
-fout.cd()
-rescaleEff(fin1).Write("efficiency")
-fout.Close()
+### c2 = TCanvas("vtxProbNvtx","vtxProbNvtx")
+### eff2.SetTitle("True vertex eff.;N_{vtx};Fraction | z_{reco} - z_{true} | < 10 mm")
+### prob2.SetTitle("Aveage vertex prob.;N_{vtx};Fraction | z_{reco} - z_{true} | < 10 mm")
+### 
+### eff2.GetYaxis().SetRangeUser(0.5, 1.1)
+### eff2.GetXaxis().SetRangeUser(0, 35)
+### eff2.GetXaxis().SetTitleOffset(1.1);
+### eff2.GetYaxis().SetTitleOffset(1.2);
+### eff2.Draw("e0")
+### prob2.Draw("e3 same")
+### eff2.Draw("e0 same")
+### 
+### leg2 = TLegend(0.52,0.62,0.82,0.82)
+### leg2.SetShadowColor(kWhite), leg2.SetLineColor(kWhite), leg2.SetFillColor(kWhite), leg2.SetTextFont(60)
+### leg2.AddEntry(eff2,"","pe")
+### leg2.AddEntry(prob2,"","f")
+### leg2.Draw("same")
+### pt.Draw("same")
 
 
-eff1 = eff(fin1,"eff1")
-## eff2 = eff(fin2,"eff2")
+c0 = TCanvas("vtxEffPt","vtxEffPt")
+eff0 = eff1.Clone()
+eff0.SetTitle("True vertex eff.;p_{T}(#gamma #gamma) (GeV);Fraction of events")
+eff0.Draw("e1")
+leg0=TLegend(0.332215, 0.357143, 0.60906, 0.423345)
+leg0.AddEntry(eff0,"| z_{reco} - z_{true} | < 10 mm","lpf")
+leg0.SetShadowColor(kWhite), leg0.SetLineColor(kWhite), leg0.SetFillColor(kWhite), leg0.SetTextFont(60)
+leg0.Draw("same")
+pt.Draw("same")
 
-## eff2.SetLineColor(kRed)
-## eff2.SetMarkerColor(kRed)
+c3 = TCanvas("vtxEffNvtx","vtxEffNvtx")
+eff3 = eff2.Clone()
+eff3.SetTitle("True vertex eff.;N_{vtx};Fraction of events")
+eff3.Draw("e1")
+leg3=TLegend(0.434564,0.684669,0.711409,0.750871)
+leg3.SetShadowColor(kWhite), leg3.SetLineColor(kWhite), leg3.SetFillColor(kWhite), leg3.SetTextFont(60)
+leg3.AddEntry(eff3,"| z_{reco} - z_{true} | < 10 mm","lpf")
+leg3.Draw("same")
+pt.Draw("same")
 
-c = TCanvas("c","c")
-eff1.Draw()
-eff2.Draw("hist same")
 
-prob1 = prob(fin1,"prob1")
-## prob2 = prob(fin2,"prob2")
-
-### prob2.SetLineColor(kRed)
-### prob2.SetMarkerColor(kRed)
-
-c1 = TCanvas("c1","c1")
-eff1.Draw()
-prob1.Draw("hist same")
-
-### c2 = TCanvas("c2","c2")
-### eff2.Draw()
-### prob2.Draw("hist same")
+for c in c0, c1, c2, c3:
+    for fmt in "C", "png", "pdf":
+        c.SaveAs( "%s.%s" % (c.GetName(), fmt) )
