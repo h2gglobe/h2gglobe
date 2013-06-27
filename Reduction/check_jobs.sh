9b4ed01d1053ce18696b10ef13522764257c434e
Andrea Marini <andrea.marini@cern.ch>
2013-06-27 10:15:08 +0200

    fancy colors + disk space in grep

--- Reduction/check_jobs.sh
+++ Reduction/check_jobs.sh
@@ -6,5 +6,10 @@ if [[ -z $1 ]]; then
     exit 1
 fi
 
-tail -n 100 $1/*.log | egrep -B 2 '=>|cmsStage|rfcp|Red|Break' | egrep  '=>|cmsStage|rfcp|[Rr]ed|Break'
+tail -n 100 $1/*.log | egrep -B 2 '=>|cmsStage|rfcp|Red|Break|Disk quota exceeded' | egrep  '=>|cmsStage|rfcp|[Rr]ed|Break|Disk quota exceeded' | 
+		#COLOR RED
+		sed "s:Disk quota exceeded:\x1b\[01;31m&\x1b\[00m:g"  |
+		sed "s:CPU time limit exceeded:\x1b\[01;31m&\x1b\[00m:g"  | 
+		#COLOR GREEN
+		sed "s:[^/]\+\.root:\x1b\[01;32m&\x1b\[00m:g"
 
