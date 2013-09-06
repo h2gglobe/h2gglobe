0015a02c67e1661395ab9f8420cc494abfaead09
Pasquale Musella <pasquale.musella@cern.ch>
2013-09-06 22:14:24 +0200

    T2 reduction input support.

--- AnalysisScripts/python/makeFilelist.py
+++ AnalysisScripts/python/makeFilelist.py
@@ -1,29 +1,34 @@
 import commands,sys,os
 
-eos = "/afs/cern.ch/project/eos/installation/pro/bin/eos.select"
+siteHandling = {
+   "cern.ch"    : { "ls"      :  "xrd eoscms.cern.ch ls %s | awk '{ print $5 }' | sort",
+                    "prepend" :  "root://eoscms//eos/cms" },
+   "T2_CH_CSCS" : { "ls"      : "xrd cms01.lcg.cscs.ch ls %s | awk '{ print $5 }' | sort ",
+                    "prepend" : "root://cms01.lcg.cscs.ch/"  }
+   }
 
-def makeCaFiles(dir,njobs=-1,jobid=0,nf=[0],maxfiles=-1):
+
+def makeCaFiles(dir,njobs=-1,jobid=0,nf=[0],maxfiles=-1,site="cern.ch"):
 
    dir = str(dir)
    return_files = []
 
-   try:
-      ld_path = os.getenv("LD_LIBRARY_PATH")
-   except:
-      ld_path = ""
-
-   if not "/afs/cern.ch/project/eos/installation/pro/lib64/" in ld_path:
-      os.putenv("LD_LIBRARY_PATH", "%s:%s" % ( ld_path, "/afs/cern.ch/project/eos/installation/pro/lib64/" ) )
-
-   iscastor = False
+   replace = None
+   ls = None
+   prepend = None
    if dir.startswith("/castor"):
-      sc,flist = commands.getstatusoutput('nsls %s'%(dir))
+      ls = 'nsls %s'
       prepend = 'rfio://'
    else:
-      ### sc,flist = commands.getstatusoutput("cmsLs %s | awk '{ print $5 }' | xargs cmsPfn | sed 's/\?.*$//'" % (dir))
-      sc,flist = commands.getstatusoutput("%s ls /eos/cms%s | sort" % (eos,dir))
-      prepend = 'root://eoscms//eos/cms'
+      sh = siteHandling[site]
+      while type(sh) == str:
+         sh = siteHandling[sh]         
+      ls = sh["ls"]
+      prepend = sh.get("prepend",None)
+      replace = sh.get("replace",None)
       
+   sc,flist = commands.getstatusoutput(ls%dir)
+   
    if not sc:
       files = flist.split('\n')
       ifile = 0
@@ -33,29 +38,31 @@ def makeCaFiles(dir,njobs=-1,jobid=0,nf=[0],maxfiles=-1):
                break
             ifile += 1
             nf[0] += 1
-            fname = "%s%s/%s" % ( prepend, dir, f)
+            fname = f
+            if replace:
+               fname = fname.replace( *replace )
+            if prepend:
+               fname = "%s%s" % ( prepend, fname)
             if (njobs > 0) and (nf[0] % njobs != jobid):
                return_files.append((fname,False))
 	    else:
-               return_files.append((fname,True))      
+               return_files.append((fname,True))
    else:
       sys.exit("No Such Directory: %s"%(dir))
 
-   if nf[0]==0: sys.exit("No .root Files found in directory - %s"%dir)
+   if nf[0]==0:
+      sys.exit("No .root Files found in directory - %s:\n%s"%(dir,flist))
 
    return return_files
 
 
 def makeDcFiles(dir,njobs=-1,jobid=0,nf=[0],maxfiles=-1):
 
-   #dcache_prepend = 'dcap://gfe02.grid.hep.ph.ic.ac.uk/pnfs/hep.ph.ic.ac.uk/data/cms'
    dcache_prepend = 'root://xrootd.grid.hep.ph.ic.ac.uk/'
    dir = str(dir)
    return_files = []
 
    sc,flist = commands.getstatusoutput('srmls $DCACHE_SRM_ROOT/%s --count 1000'%(dir))
-
- #  nf = 0
    
    if not sc:
       files = flist.split('\n')
@@ -91,37 +98,8 @@ def unmounteos(dir):
    
 
 def makeEosFiles(dir,njobs=-1,jobid=0,nf=[0]):
-   eoslsprepend = "./eosfoo/cms/store/group/phys_higgs/cmshgg/" 
-   eosprepend = "root://eoscms.cern.ch//eos/cms/store/group/phys_higgs/cmshgg/" 
-
-   fc = commands.getstatusoutput('csh -c "eosmount ./eosfoo /store/cms/"') 
-   dir = str(dir)
-   return_files = []
-#   nf = 0
-   if os.path.isdir(eoslsprepend+dir): 
-      files = os.listdir(eoslsprepend+dir)
-      for f in files:
-         ifile = 0
-         if '.root' in f:
-            if( maxfiles > 0 and ifile >= maxfiles):
-               break
-            ifile += 1
-            nf[0] += 1
-            if (njobs > 0) and (nf[0] % njobs != jobid):
-               return_files.append((eosprepend+dir+'/'+f,False))
-	    else:
-               return_files.append((eosprepend+dir+'/'+f,True))
-   else: 
-	unmounteos("eosfoo")
-	sys.exit("No Such Directory as %s"%eoslsprepend+dir)  
-
-   if nf[0]==0: 
-	unmounteos("eosfoo")
-	sys.exit("No .root Files found in directory - %s"%eoslsprepend+dir)
+   sys.exit("makeEosFiles not supported anymore")
    
-   unmounteos("eosfoo")
-   return return_files  
-
 def makeFiles(dir,njobs=-1,jobid=0,nf=[0],maxfiles=-1):
 
    dir = str(dir)
@@ -145,3 +123,14 @@ def makeFiles(dir,njobs=-1,jobid=0,nf=[0],maxfiles=-1):
    if nf[0]==0: sys.exit("No .root Files found in directory - %s"%dir)
    
    return return_files  
+
+if __name__ == "__main__":
+   from sys import argv
+   site = "cern.ch"
+   if len(argv) == 3:
+      site = argv.pop(1)
+   flist = makeCaFiles(argv[1],site=site)
+   for f in flist:
+      print f[0]
+   
+   
