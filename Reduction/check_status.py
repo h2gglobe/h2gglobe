5dd1bccb4b02b41c221eb05620a3ddb33011ada0
Pasquale Musella <pasquale.musella@cern.ch>
2013-10-07 03:21:04 +0200

    Configuration for legacy_paper_reduction_8TeV_v4

--- Reduction/check_status.py
+++ Reduction/check_status.py
@@ -6,44 +6,42 @@ if len(sys.argv) == 1:
     usage()
     sys.exit(-1)
 
+for taskdir in sys.argv[1:]:
+    samples = glob.glob( "%s/*.dat" % taskdir )
 
-taskdir = sys.argv[1]
+    for sample in samples:
+        status = {}
+        tot = 0
+        jobs = glob.glob( "%s_[0-9]*.[a-z]*" % sample )
+        for j in jobs:
+            toks = j.split("_")
+            id,stat = toks[-1].split(".")
+            if stat in ["sub","run","fail","done"]:
+                status[id] = stat
 
-samples = glob.glob( "%s/*.dat" % taskdir )
+        groups = { "sub" : [], "run" : [], "fail" : [], "done" : [] } 
+        for i,s in status.iteritems():        
+            groups[s].append(i)
+            tot += 1
 
-for sample in samples:
-    status = {}
-    tot = 0
-    jobs = glob.glob( "%s_[0-9]*.[a-z]*" % sample )
-    for j in jobs:
-        toks = j.split("_")
-        id,stat = toks[-1].split(".")
-        if stat in ["sub","run","fail","done"]:
-            status[id] = stat
-
-    groups = { "sub" : [], "run" : [], "fail" : [], "done" : [] } 
-    for i,s in status.iteritems():        
-        groups[s].append(i)
-        tot += 1
-
-    print sample
-    name = os.path.basename(sample).rsplit(".",1)[0]
-    for g,jo in groups.iteritems():
-        if len(jo) > 0:
-            print "%d jobs %s" % (len(jo), g),
-            print "[",
-            for j in jo:
-                print j,
-            print "]"
-            if g == "fail":
-                print "find %s " % name,
-                print "-name %s_%s.root" % (name,j[0]) ,
-                for j in jo[1:]:
-                    print "-or -name %s_%s.root" % (name,j) ,
-                print " -exec rm -v \\{\\} \\; ;"
-                print "./submit_reduction.sh %s %s %d" % ( taskdir, name, tot ),
+        print sample
+        name = os.path.basename(sample).rsplit(".",1)[0]
+        for g,jo in groups.iteritems():
+            if len(jo) > 0:
+                print "%d jobs %s" % (len(jo), g),
+                print "[",
                 for j in jo:
                     print j,
-                print
-    print
+                print "]"
+                if g == "fail":
+                    print "find %s " % name,
+                    print "-name %s_%s.root" % (name,j[0]) ,
+                    for j in jo[1:]:
+                        print "-or -name %s_%s.root" % (name,j) ,
+                    print " -exec rm -v \\{\\} \\; ;"
+                    print "./submit_reduction.sh %s %s %d" % ( taskdir, name, tot ),
+                    for j in jo:
+                        print j,
+                    print
+        print
     
