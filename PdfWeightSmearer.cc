40b1d790712f89a68b64655245e41b6161a109cb
nick <n.c.k.wardle@googlemail.com>
2014-01-10 18:30:52 +0100

    Adding Pt-Y variations for VBF process. Same number of nuisances as before (should be fixed for the QCD scale in the datacard) just that now the effect on VBF is also calculated

--- PdfWeightSmearer.cc
+++ PdfWeightSmearer.cc
@@ -18,9 +18,10 @@ PdfWeightSmearer::~PdfWeightSmearer()
 }
 
 void PdfWeightSmearer::readFile(std::string uId, std::string dId ){
-  
-  kFactorSmearers_.resize(3);
 
+  kFactorSmearers_.resize(6);
+
+  // first is central/up/down for the G-fusion, then its the VBF
   TH2F* temp = (TH2F*) thePdfWeightFile_->Get("GF_cent");
   assert(temp!=0);    kFactorSmearers_[0]=(TH2F*) temp->Clone(("Hmasscent")); kFactorSmearers_[0]->SetDirectory(0);
 
@@ -35,19 +36,43 @@ void PdfWeightSmearer::readFile(std::string uId, std::string dId ){
   kFactorSmearers_[2]->Scale(C_integral/kFactorSmearers_[2]->Integral());
   kFactorSmearers_[1]->Scale(C_integral/kFactorSmearers_[1]->Integral());
 
+  temp = (TH2F*) thePdfWeightFile_->Get("VBF_cent");
+  assert(temp!=0);    kFactorSmearers_[3]=(TH2F*) temp->Clone(("Hmasscent_vbf")); kFactorSmearers_[3]->SetDirectory(0);
+
+  temp = (TH2F*) thePdfWeightFile_->Get( Form("VBF_%s",uId.c_str()) );
+  assert(temp!=0);    kFactorSmearers_[4]=(TH2F*) temp->Clone(("Hmass_up_vbf")); kFactorSmearers_[4]->SetDirectory(0);
+
+  temp = (TH2F*) thePdfWeightFile_->Get( Form("VBF_%s",dId.c_str()) );
+  assert(temp!=0);    kFactorSmearers_[5]=(TH2F*) temp->Clone(("Hmass_down_vbf")); kFactorSmearers_[5]->SetDirectory(0);
+
+  // Need to normalize to central integral!
+  C_integral = kFactorSmearers_[0]->Integral();
+  kFactorSmearers_[2]->Scale(C_integral/kFactorSmearers_[2]->Integral());
+  kFactorSmearers_[1]->Scale(C_integral/kFactorSmearers_[1]->Integral());
+
+  C_integral = kFactorSmearers_[3]->Integral();
+  kFactorSmearers_[5]->Scale(C_integral/kFactorSmearers_[5]->Integral());
+  kFactorSmearers_[4]->Scale(C_integral/kFactorSmearers_[4]->Integral());
 }
 
 bool PdfWeightSmearer::smearEvent( float & weight, const TLorentzVector & p4, const int nPu, const int sample_type, float syst_shift ) const 
 {
   if( sample_type >= 0 ) { return true; }
   int genMassPoint = std::round(norm_->GetMass(sample_type));
-  
-  if( norm_->GetProcess(sample_type) != "ggh" ) {
+
+  int process_shift=0;
+  if ( norm_->GetProcess(sample_type) == "ggh" ){
+  	process_shift =  0;
+  } else if (norm_->GetProcess(sample_type) == "vbf"){
+  	process_shift =  3;
+  } else {
     return true;
   }
+
   if( genMassPoint > 150 ) { genMassPoint=150; } // Warning: missing k-factor
   if( genMassPoint == 100 ) { genMassPoint=105; }  // Warning: missing k-factor
-  
+
+ 
   double kWeight = getWeight( p4, nPu, syst_shift );
   weight = (kWeight > 0) ? kWeight : 0;
   return true;
@@ -74,12 +99,14 @@ double PdfWeightSmearer::getPdfWeight(int genMassPoint, int id, double gPT , dou
 }
 
 
-double PdfWeightSmearer::getWeight( const TLorentzVector & p4, const int nPu, float syst_shift) const
+double PdfWeightSmearer::getWeight( const TLorentzVector & p4, const int nPu, float syst_shift, int process_shift) const
 {
   float gPT = p4.Pt();
   float gY  = fabs(p4.Rapidity());
 
   int    varId     = syst_shift > 0 ?   1 : 2;
+  varId+=process_shift; // this is a ggH or qqH signal
+
   double nominal   = getPdfWeight( 0, 0, gPT, gY );
   double variation = getPdfWeight( 0, varId, gPT, gY );
 
